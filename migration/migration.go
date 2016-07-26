b8ce4a0373896d7e39ba2e2e4a0962c515ce6dea
Aslak Knutsen <aslak@4fs.no>
2016-07-27 01:09:38 +0200

    WIP lint

--- migration/migration.go
+++ migration/migration.go
@@ -3,7 +3,6 @@ package migration
 import (
 	"github.com/almighty/almighty-core/models"
 	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
 )
 
 // Perform executes the required migration of the database on startup
