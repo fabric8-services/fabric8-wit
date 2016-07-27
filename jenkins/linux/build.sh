b06b4e950781536292460585d7f66683f5cd1491
Konrad Kleine <kkleine@redhat.com>
2016-07-27 18:00:10 +0200

    Re-use user:group of caller in jenkins automatically
    Previously I determined the username and group ID of the user that
    started the jenkins build script and created a user with the exact
    same ID inside the container.
    
    This is not needed due to the "-u" switch on the "docker run" command.
    
    This change greatly simplifies the build on a linux machine.
    
    The build now runs in source and no longer out-of source which
    helps to archive artifacts the were generated inside the container.

--- jenkins/linux/build.sh
+++ jenkins/linux/build.sh
@@ -6,38 +6,16 @@ set -x
 # Exit if a command fails
 set -e
 
-# Create user with same UID and name as the user that
-# started the container
-echo "Creating new user \"$USER\" with UID \"$USERID\""
-useradd -m ${USER} -u ${USERID}
+export GOPATH=/tmp/go
+export PATH=$PATH:${GOPATH}/bin
+export GO15VENDOREXPERIMENT=1 
 
-# Where the source is stored?
-# Take the value of the first and second argument or default
-# to /source and /build
-source_dir=${1:-/source}
-build_dir=${2:-/build}
+make deps 
 
-# Give all rights to users outside of the container
-function almighty_clean_up {
-  chown -R $USER ${build_dir} 
-}
-trap 'echo "SIGNAL received. Will clean up."; almighty_clean_up' SIGUSR1 SIGTERM SIGINT EXIT
+make generate 
 
-su $USER --command=" \
-mkdir -pv ${build_dir}/go/bin \
-&& mkdir -pv ${build_dir}/go/pkg \
-&& mkdir -pv ${build_dir}/go/src/github.com/almighty/almighty-core \
-&& export GOPATH=${build_dir}/go \
-&& export PATH=\$PATH:${build_dir}/go/bin \
-&& export GO15VENDOREXPERIMENT=1 \
-&& go env \
-&& cd ${source_dir} \
-&& cp -Rfp . ${build_dir}/go/src/github.com/almighty/almighty-core \
-&& chown -Rf $USER ${build_dir} \
-&& cd ${build_dir}/go/src/github.com/almighty/almighty-core \
-&& make deps \
-&& make generate \
-&& make \
-&& make test-unit \
-"
+make 
+
+make test-unit 
+
 
