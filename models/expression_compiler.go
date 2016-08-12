a624429c5f3f675b6d29b931433fed5f1b885341
Thomas Mäder <tmaeder@redhat.com>
2016-08-12 16:29:04 +0200

    Conditional value rendering depending on json context or regular context

--- models/expression_compiler.go
+++ models/expression_compiler.go
@@ -7,13 +7,41 @@ import (
 	"github.com/almighty/almighty-core/criteria"
 )
 
+const (
+	jsonAnnotation = "JSON"
+)
+
 // Compile takes a an expression and compiles it to a where clause for use with gorm.DB.Where()
 // Returns the number of expected parameters for the query and an array of errors if something goes wrong
 func Compile(where criteria.Expression) (whereClause string, parameterCount uint16, err []error) {
+	criteria.IteratePostOrder(where, bubbleUpJSONContext)
+
 	compiler := expressionCompiler{}
 	compiled := where.Accept(&compiler)
 
 	return compiled.(string), compiler.parameterCount, compiler.err
+}
+
+func bubbleUpJSONContext(exp criteria.Expression) bool {
+	switch t := exp.(type) {
+	case *criteria.FieldExpression:
+		if isJSONField(t.FieldName) {
+			t.SetAnnotation(jsonAnnotation, true)
+		}
+	case *criteria.EqualsExpression:
+		if t.Left().Annotation(jsonAnnotation) == true || t.Right().Annotation(jsonAnnotation) == true {
+			t.SetAnnotation(jsonAnnotation, true)
+		}
+	}
+	return true
+}
+
+func isJSONField(fieldName string) bool {
+	switch fieldName {
+	case "ID", "Name", "Type", "Version":
+		return false
+	}
+	return true
 }
 
 type expressionCompiler struct {
@@ -22,23 +50,21 @@ type expressionCompiler struct {
 }
 
 // Field implements criteria.ExpressionVisitor
-func (c *expressionCompiler) Field(f criteria.FieldExpression) interface{} {
-	switch f.FieldName {
-	case "ID", "Name", "Type", "Version":
+func (c *expressionCompiler) Field(f *criteria.FieldExpression) interface{} {
+	if !isJSONField(f.FieldName) {
 		return f.FieldName
-	default:
-		return "Fields->'" + f.FieldName + "'"
 	}
+	return "Fields->'" + f.FieldName + "'"
 }
 
 // And implements criteria.ExpressionVisitor
-func (c *expressionCompiler) And(a criteria.AndExpression) interface{} {
-	return c.binary(a.BinaryExpression, "and")
+func (c *expressionCompiler) And(a *criteria.AndExpression) interface{} {
+	return c.binary(a, "and")
 }
 
 func (c *expressionCompiler) binary(a criteria.BinaryExpression, op string) interface{} {
-	left := a.Left.Accept(c)
-	right := a.Right.Accept(c)
+	left := a.Left().Accept(c)
+	right := a.Right().Accept(c)
 	if left != nil && right != nil {
 		return "(" + left.(string) + " " + op + " " + right.(string) + ")"
 	}
@@ -47,38 +73,61 @@ func (c *expressionCompiler) binary(a criteria.BinaryExpression, op string) inte
 }
 
 // Or implements criteria.ExpressionVisitor
-func (c *expressionCompiler) Or(a criteria.OrExpression) interface{} {
-	return c.binary(a.BinaryExpression, "or")
+func (c *expressionCompiler) Or(a *criteria.OrExpression) interface{} {
+	return c.binary(a, "or")
 }
 
 // Equals implements criteria.ExpressionVisitor
-func (c *expressionCompiler) Equals(e criteria.EqualsExpression) interface{} {
-	return c.binary(e.BinaryExpression, "=")
+func (c *expressionCompiler) Equals(e *criteria.EqualsExpression) interface{} {
+	return c.binary(e, "=")
 }
 
 // Field implements criteria.ExpressionVisitor
-func (c *expressionCompiler) Parameter(v criteria.ParameterExpression) interface{} {
+func (c *expressionCompiler) Parameter(v *criteria.ParameterExpression) interface{} {
 	c.parameterCount++
 	return "?"
 }
 
-func (c *expressionCompiler) Value(v criteria.LiteralExpression) interface{} {
+func isInJSONContext(exp criteria.Expression) bool {
+	result := false
+	criteria.IterateParents(exp, func(exp criteria.Expression) bool {
+		if exp.Annotation(jsonAnnotation) == true {
+			result = true
+			return false
+		}
+		return true
+	})
+	return result
+}
+
+func (c *expressionCompiler) Literal(v *criteria.LiteralExpression) interface{} {
+	json := isInJSONContext(v)
 	switch t := v.Value.(type) {
 	case float64:
-		return strconv.FormatFloat(t, 'f', -1, 64)
+		return wrapJson(json, strconv.FormatFloat(t, 'f', -1, 64))
 	case int:
-		return strconv.FormatInt(int64(t), 10)
+		return wrapJson(json, strconv.FormatInt(int64(t), 10))
 	case int64:
-		return strconv.FormatInt(t, 10)
+		return wrapJson(json, strconv.FormatInt(t, 10))
 	case uint:
-		return strconv.FormatUint(uint64(t), 10)
+		return wrapJson(json, strconv.FormatUint(uint64(t), 10))
 	case uint64:
-		return strconv.FormatUint(t, 10)
+		return wrapJson(json, strconv.FormatUint(t, 10))
 	case string:
-		return "'\"" + t + "\"'"
+		if json {
+			return "'\"" + t + "\"'"
+		}
+		return "'" + t + "'"
 	case bool:
-		return strconv.FormatBool(t)
+		return wrapJson(json, strconv.FormatBool(t))
 	}
 	c.err = append(c.err, fmt.Errorf("unknown value type of %v: %T", v.Value, v.Value))
 	return ""
+}
+
+func wrapJson(isJSON bool, value string) string {
+	if isJSON {
+		return "'" + value + "'"
+	}
+	return value
 }
