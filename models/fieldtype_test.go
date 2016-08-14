bceef56f850ca2cc06fd1b8cf68978b6e1d400a6
Pranav <pranav@dhcp35-137.lab.eng.blr.redhat.com>
2016-08-14 12:04:27 +0530

    added KindList and updated related tests.

--- models/fieldtype_test.go
+++ models/fieldtype_test.go
@@ -1,53 +1,56 @@
 package models
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 var stString = SimpleType{KindString}
 var stInt = SimpleType{KindInteger}
 var stFloat = SimpleType{KindFloat}
 var stDuration = SimpleType{KindDuration}
 var stURL = SimpleType{KindURL}
+var stList = SimpleType{KindList}
 
 type input struct {
-	t             SimpleType
+	t             FieldType
 	value         interface{}
 	expectedValue interface{}
+	errorExpected bool
 }
 
 func TestSimpleTypeConversion(t *testing.T) {
-	// When expected value is set to nil
-	// it means `ConvertToModel` should raise an error
 	test_data := []input{
-		{stString, "hello world", "hello world"},
-		{stString, "", ""},
-		{stString, 100, nil},
-		{stString, 1.90, nil},
+		{stString, "hello world", "hello world", false},
+		{stString, "", "", false},
+		{stString, 100, nil, true},
+		{stString, 1.90, nil, true},
 
-		{stInt, 100.0, nil},
-		{stInt, 100, 100},
-		{stInt, "100", nil},
-		{stInt, true, nil},
+		{stInt, 100.0, nil, true},
+		{stInt, 100, 100, false},
+		{stInt, "100", nil, true},
+		{stInt, true, nil, true},
 
-		{stFloat, 1.1, 1.1},
-		{stFloat, 1, nil},
-		{stFloat, "a", nil},
+		{stFloat, 1.1, 1.1, false},
+		{stFloat, 1, nil, true},
+		{stFloat, "a", nil, true},
 
-		{stDuration, 0, 0},
-		{stDuration, 1.1, nil},
-		{stDuration, "duration", nil},
+		{stDuration, 0, 0, false},
+		{stDuration, 1.1, nil, true},
+		{stDuration, "duration", nil, true},
 
-		{stURL, "http://www.google.com", "http://www.google.com"},
-		{stURL, "", nil},
-		{stURL, "google", nil},
-		{stURL, "http://google.com", "http://google.com"},
+		{stURL, "http://www.google.com", "http://www.google.com", false},
+		{stURL, "", nil, true},
+		{stURL, "google", nil, true},
+		{stURL, "http://google.com", "http://google.com", false},
+
+		{stList, [4]int{1, 2, 3, 4}, [4]int{1, 2, 3, 4}, false},
+		{stList, [2]string{"1", "2"}, [2]string{"1", "2"}, false},
+		{stList, "", nil, true},
+		// {stList, []int{}, []int{}, false}, need to find out the way for empty array.
+		// because slices do not have equality operator.
 	}
 	for _, inp := range test_data {
 		retVal, err := inp.t.ConvertToModel(inp.value)
-		if retVal == inp.expectedValue {
-			fmt.Println("test pass:", inp)
+		if retVal == inp.expectedValue && (err != nil) == inp.errorExpected {
+			t.Log("test pass:", inp)
 		} else {
 			t.Error(retVal, err)
 			t.Fail()
