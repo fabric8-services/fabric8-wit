591ea5716dbf49132ee7cecf941ee43d64f2a04a
Dhriti Shikhar <dhrish20@gmail.com>
2016-08-23 19:58:04 +0530

    Adds tracker test case

--- models/tracker.go
+++ models/tracker.go
@@ -5,8 +5,6 @@ type Tracker struct {
 	ID uint64 `gorm:"primary_key"`
 	// URL of the tracker
 	URL string
-	// Credentials to access the tracker
-	Credentials string
 	// Type of the tracker (jira, github, bugzilla, trello etc.)
 	Type string
 }
