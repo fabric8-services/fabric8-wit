37964a108027e99fd99f120918ccaec49864ff47
Konrad Kleine <kkleine@redhat.com>
2016-08-23 12:21:49 +0200

    Fixes #129 List Work Item Types (WIT)
    * Fixes #129 List Work Item Types (WIT)
     * We don't have filters implemented as in the WI listing.
    
    * change WIT media type texts: work item -> work item type
    
    * Fix response type in list action of WIT
    
    * Hide removal of temporary error files
    
    * Fix typo: contant -> constant
    
    * Added github.com/stretchr/testify/assert
    * Added github.com/stretchr/testify/suite to glide
    * Added work item type tests using test suites
     * We use test suites to have a dedicated place where to run pre- and post
      test code. For example to avoid primary key clashes in the database, the
      pre-test setup deletes all keys from the database that will be created
      in the suite.
    
    * Removed `TestCreateShowWorkItemType` because it is already implemented
      more efficient using the `assert` package.
    
    * Have the test suite handle the db connection itself without using a
      global db handle.
    
    * Added a test for listing of work item types
    
    * Moved examples/create_workitemtype.json to examples/workitemtype.adoc
     * Added example about listing WITs
     * Fixed spelling errors from original document
     * Added sections and made the document renderable with asciidoctor

--- workitemtype_test.go
+++ workitemtype_test.go
@@ -3,64 +3,223 @@
 package main
 
 import (
-	"strconv"
 	"testing"
-	"time"
-	//"strings"
+
+	"fmt"
+	"net/http"
 
 	"github.com/almighty/almighty-core/app"
 	"github.com/almighty/almighty-core/app/test"
+	"github.com/almighty/almighty-core/migration"
 	"github.com/almighty/almighty-core/models"
+	"github.com/almighty/almighty-core/transaction"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/net/context"
+	"os"
 )
 
-func reandomUniqueString() string {
-	// generates random string using timestamp
-	now := time.Now().UnixNano() / 1000000
-	return strconv.FormatInt(now, 10)
+//-----------------------------------------------------------------------------
+// Test Suite setup
+//-----------------------------------------------------------------------------
+
+// The WorkItemTypeTestSuite has state the is relevant to all tests.
+// It implements these interfaces from the suite package: SetupAllSuite, SetupTestSuite, TearDownAllSuite, TearDownTestSuite
+type WorkItemTypeSuite struct {
+	suite.Suite
+	db       *gorm.DB
+	ts       *models.GormTransactionSupport
+	witRepo  *models.GormWorkItemTypeRepository
+	typeCtrl WorkitemtypeController
 }
 
-func TestCreateShowWorkItemType(t *testing.T) {
-	// test-1
-	// create a WIT (using workitem_repo)
-	// try to create anohter WIT with same name - should Fail
-	// remove the WIT (using GORM raw query)
+// The SetupSuite method will run before the tests in the suite are run.
+// It sets up a database connection for all the tests in this suite without polluting global space.
+func (s *WorkItemTypeSuite) SetupSuite() {
+	fmt.Println("--- Setting up test suite WorkItemTypeSuite ---")
 
-	ts := models.NewGormTransactionSupport(db)
-	witr := models.NewWorkItemTypeRepository(ts)
-	controller := WorkitemtypeController{ts: ts, witRepository: witr}
-	name := reandomUniqueString()
-
-	st := app.FieldType{
-		Kind: "user",
+	dbHost := os.Getenv("ALMIGHTY_DB_HOST")
+	if "" == dbHost {
+		panic("The environment variable ALMIGHTY_DB_HOST is not specified or empty.")
 	}
-	fd := app.FieldDefinition{
-		Type:     &st,
+
+	var err error
+	s.db, err = gorm.Open("postgres", fmt.Sprintf("host=%s user=postgres password=mysecretpassword sslmode=disable", dbHost))
+	if err != nil {
+		panic("failed to connect database: " + err.Error())
+	}
+
+	s.ts = models.NewGormTransactionSupport(s.db)
+	s.witRepo = models.NewWorkItemTypeRepository(s.ts)
+	s.typeCtrl = WorkitemtypeController{ts: s.ts, witRepository: s.witRepo}
+
+	// Migrate the schema
+	if err := transaction.Do(s.ts, func() error {
+		return migration.Perform(context.Background(), s.ts.TX(), s.witRepo)
+	}); err != nil {
+		panic(err.Error())
+	}
+}
+
+// The TearDownSuite method will run after all the tests in the suite have been run
+// It tears down the database connection for all the tests in this suite.
+func (s *WorkItemTypeSuite) TearDownSuite() {
+	fmt.Println("--- Tearing down test suite WorkItemTypeSuite ---")
+	if s.db != nil {
+		s.db.Close()
+	}
+}
+
+// The SetupTest method will be run before every test in the suite.
+// SetupTest ensures that non of the work item types that we will create already exist.
+func (s *WorkItemTypeSuite) SetupTest() {
+	s.T().Log("--- Running SetupTest ---")
+	// Remove all work item types from the db that will be created during these tests.
+	s.db.Where("name = ?", "animal").Delete(models.WorkItemType{})
+	s.db.Where("name = ?", "person").Delete(models.WorkItemType{})
+}
+
+// The TearDownTest method will be run after every test in the suite.
+func (s *WorkItemTypeSuite) TearDownTest() {
+	s.T().Log("--- Running TearDownTest ---")
+	s.db.Where("name = ?", "animal").Delete(models.WorkItemType{})
+	s.db.Where("name = ?", "person").Delete(models.WorkItemType{})
+}
+
+//-----------------------------------------------------------------------------
+// helper method
+//-----------------------------------------------------------------------------
+
+// createWorkItemTypeAnimal defines a work item type "animal" that consists of
+// two fields ("animal-type" and "color"). The type is mandatory but the color is not.
+func (s *WorkItemTypeSuite) createWorkItemTypeAnimal() (http.ResponseWriter, *app.WorkItemType) {
+
+	// Create an enumeration of animal names
+	typeStrings := []string{"elephant", "blue whale", "Tyrannosaurus rex"}
+
+	// Convert string slice to slice of interface{} in O(n) time.
+	typeEnum := make([]interface{}, len(typeStrings))
+	for i := range typeStrings {
+		typeEnum[i] = typeStrings[i]
+	}
+
+	// Create the type for "animal-type" field based on the enum above
+	stString := "string"
+	typeFieldDef := app.FieldDefinition{
 		Required: true,
+		Type: &app.FieldType{
+			BaseType: &stString,
+			Kind:     "enum",
+			Values:   typeEnum,
+		},
 	}
-	fields := map[string]*app.FieldDefinition{
-		"system.owner": &fd,
+
+	// Create the type for the "color" field
+	colorFieldDef := app.FieldDefinition{
+		Required: false,
+		Type: &app.FieldType{
+			Kind: "string",
+		},
 	}
+
+	// Use the goa generated code to create a work item type
 	payload := app.CreateWorkitemtypePayload{
-		ExtendedTypeName: nil,
-		Name:             name,
-		Fields:           fields,
-	}
-	t.Log("creating WIT now.")
-	_, created := test.CreateWorkitemtypeCreated(t, nil, nil, &controller, &payload)
-
-	if created.Name == "" {
-		t.Error("no Name")
-	}
-	t.Log("WIT created, Name=", created.Name)
-
-	t.Log("Fetch recently created WIT")
-	_, showWIT := test.ShowWorkitemtypeOK(t, nil, nil, &controller, name)
-
-	if showWIT == nil {
-		t.Error("Can not fetch WIT", name)
+		Fields: map[string]*app.FieldDefinition{
+			"animal_type": &typeFieldDef,
+			"color":       &colorFieldDef,
+		},
+		Name: "animal",
 	}
 
-	t.Log("Started cleanup for ", created.Name)
-	db.Table("work_item_types").Where("name=?", name).Delete(&models.WorkItemType{})
-	t.Log("Cleanup complete")
+	return test.CreateWorkitemtypeCreated(s.T(), nil, nil, &s.typeCtrl, &payload)
+}
+
+// createWorkItemTypePerson defines a work item type "person" that consists of
+// a required "name" field.
+func (s *WorkItemTypeSuite) createWorkItemTypePerson() (http.ResponseWriter, *app.WorkItemType) {
+
+	// Create the type for the "color" field
+	nameFieldDef := app.FieldDefinition{
+		Required: true,
+		Type: &app.FieldType{
+			Kind: "string",
+		},
+	}
+
+	// Use the goa generated code to create a work item type
+	payload := app.CreateWorkitemtypePayload{
+		Fields: map[string]*app.FieldDefinition{
+			"name": &nameFieldDef,
+		},
+		Name: "person",
+	}
+
+	return test.CreateWorkitemtypeCreated(s.T(), nil, nil, &s.typeCtrl, &payload)
+}
+
+//-----------------------------------------------------------------------------
+// Actual tests
+//-----------------------------------------------------------------------------
+
+// TestCreateWorkItemType tests if we can create two work item types: "animal" and "person"
+func (s *WorkItemTypeSuite) TestCreateWorkItemType() {
+	_, wit := s.createWorkItemTypeAnimal()
+	assert.NotNil(s.T(), wit)
+	assert.Equal(s.T(), "animal", wit.Name)
+
+	_, wit = s.createWorkItemTypePerson()
+	assert.NotNil(s.T(), wit)
+	assert.Equal(s.T(), "person", wit.Name)
+}
+
+// TestShowWorkItemType tests if we can fetch the work item type "animal".
+func (s *WorkItemTypeSuite) TestShowWorkItemType() {
+	// Create the work item type first and try to read it back in
+	_, wit := s.createWorkItemTypeAnimal()
+	assert.NotNil(s.T(), wit)
+
+	_, wit2 := test.ShowWorkitemtypeOK(s.T(), nil, nil, &s.typeCtrl, wit.Name)
+
+	assert.NotNil(s.T(), wit2)
+	assert.EqualValues(s.T(), wit, wit2)
+}
+
+// TestListWorkItemType tests if we can find the work item types
+// "person" and "animal" in the list of work item types
+func (s *WorkItemTypeSuite) TestListWorkItemType() {
+	// Create the work item type first and try to read it back in
+	_, witAnimal := s.createWorkItemTypeAnimal()
+	assert.NotNil(s.T(), witAnimal)
+	_, witPerson := s.createWorkItemTypePerson()
+	assert.NotNil(s.T(), witPerson)
+
+	// Fetch a single work item type
+	// Paging in the format <start>,<limit>"
+	page := "0,-1"
+	_, witCollection := test.ListWorkitemtypeOK(s.T(), nil, nil, &s.typeCtrl, &page)
+
+	assert.NotNil(s.T(), witCollection)
+	assert.Nil(s.T(), witCollection.Validate())
+
+	// Check the number of found work item types
+	assert.Condition(s.T(), func() bool {
+		return (len(witCollection) >= 2)
+	}, "At least two work item types must exist (animal and person), but only %d exist.", len(witCollection))
+
+	// Search for the work item types that must exist at minimum
+	toBeFound := 2
+	for i := 0; i < len(witCollection) && toBeFound > 0; i++ {
+		if witCollection[i].Name == "person" || witCollection[i].Name == "animal" {
+			s.T().Log("Found work item type in collection: ", witCollection[i].Name)
+			toBeFound--
+		}
+	}
+	assert.Exactly(s.T(), 0, toBeFound, "Not all required work item types (animal and person) where found.")
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestSuiteWorkItemType(t *testing.T) {
+	suite.Run(t, new(WorkItemTypeSuite))
 }
