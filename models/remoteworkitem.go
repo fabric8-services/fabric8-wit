0b9d1df4bd1395bc6ca62b21744ecab5b31ad1b0
Shoubhik <shoubhik@dhcp35-156.lab.eng.blr.redhat.com>
2016-08-24 14:53:53 +0530

    Fixed the syntax issues and added a valid test to convert into WorkItem using a simple mapper

--- models/remoteworkitem.go
+++ models/remoteworkitem.go
@@ -1,21 +1,24 @@
 package models
 
+import "strconv"
+
 // RemoteWorkItem represents a work item as it is stored in the database
 type RemoteWorkItem struct {
-	Fields map[string]interface{}	
+	Fields map[string]interface{}
 }
 
 // The intregration code for a specific remote provider must populate this.
 type WorkItemMap map[string]interface{}
 
 // Map maps RemoteWorkItem to WorkItem
-func (rwi RemoteWorkItem) MapRemote (wiMap WorkItemMap, wiType WorkItemType) WorkItem {
+func (rwi RemoteWorkItem) MapRemote(wiMap WorkItemMap, wiType *WorkItemType) WorkItem {
 	workItem := WorkItem{
-		Type: wiType.ID
-	}	
-	
+		Type:   strconv.FormatUint(wiType.ID, 10),
+		Fields: make(map[string]interface{}),
+	}
+
 	for from_key, to_key := range wiMap {
-		workItem.Fields[to_key] = remoteWorkItem.Fields[from_key]
+		workItem.Fields[to_key.(string)] = rwi.Fields[from_key]
 	}
 	return workItem
 }
