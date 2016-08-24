fac96a8a9e9c46bf533206f40d0156e73207a816
Shoubhik <shoubhik@dhcp35-156.lab.eng.blr.redhat.com>
2016-08-24 14:53:53 +0530

    Fixed the syntax issues and added a valid test to convert into WorkItem using a simple mapper

--- models/remoteworkitem_test.go
+++ models/remoteworkitem_test.go
@@ -7,23 +7,26 @@ import "testing"
 func TestMap(t *testing.T) {
 
 	// This is the Type to be converted to
-	workItemType := wellKnown["1"]  // Will remove this and add an actual Type.
+	workItemType := wellKnown["1"] // Will remove this and add an actual Type.
 
-	// remoteWorkItem is fetched from external providers
 	remoteWorkItem := RemoteWorkItem{
-        Fields : {
-            "id" : "1",
-            "title":"This is a remote issue title",
-            "description":"This is a remote issue description"
-        }
-    }   
+		Fields: map[string]interface{}{
+			"id":          "3",
+			"description": "I am a remote issue's description in Github",
+			"title":       "I am the title of the issue",
+		},
+	}
 
-    workItemMap := WorkItemMap{
-        "id":"remote_issue_id",
-        "description":"description",
-        "title":"title"
-    }
+	workItemMap := WorkItemMap{
+		"id":          "remote_issue_id",
+		"description": "description",
+		"title":       "title",
+	}
+	t.Log("Test remoteWorkItem", remoteWorkItem)
+	t.Log("Test workItemMap ", workItemMap)
+	workItem := remoteWorkItem.MapRemote(workItemMap, workItemType)
 
-    workItem,err = remoteWorkItem.MapRemote(workItemMap,workItemType)    
+	// TODO: Add code to compare the workItem fields with those of the remoteWorkItem
 
+	t.Log("Test workItem", workItem)
 }
