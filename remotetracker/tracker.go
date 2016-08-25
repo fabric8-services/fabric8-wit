8c9693395385dce649ef5d579a173dc0b9b6ddb9
Dhriti Shikhar <dhrish20@gmail.com>
2016-08-25 11:45:28 +0530

    Adds Jira integration - Fetch

--- remotetracker/tracker.go
+++ remotetracker/tracker.go
@@ -2,7 +2,14 @@ package remotetracker
 
 // Tracker represents a remote issue tracker
 // for e.g github, trello, jira, bugzilla
-type Tracker interface{
-  Fetch(query string) error
-  Import() error
+type Tracker interface {
+	Fetch(query string) error
+	Import() error
+}
+
+type Item struct {
+	ID          string
+	Title       string
+	Description string
+	State       string
 }
