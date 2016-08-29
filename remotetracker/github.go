9cba76add02f45a44916fe81bac78e864c3d31dc
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-08-29 12:46:51 +0530

    Public API for tracker scheduler & async fetching

--- remotetracker/github.go
+++ remotetracker/github.go
@@ -2,30 +2,31 @@ package remotetracker
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/google/go-github/github"
+	"github.com/jinzhu/gorm"
 )
 
-// Github represents github remote issue tracker
-type Github struct {
-	items []Item
-}
-
-func (g *Github) Fetch(url, query string) error {
+// fetch collects data from Github
+func fetchGithub(url, query string, item chan map[string]interface{}) {
 	client := github.NewClient(nil)
 	result, _, _ := client.Search.Issues(query, nil)
 	issues := result.Issues
-
-	for l, _ := range issues {
+	for l := range issues {
+		i := make(map[string]interface{})
 		id, _ := json.Marshal(issues[l].URL)
 		description, _ := json.Marshal(issues[l].Body)
 		title, _ := json.Marshal(issues[l].Title)
 		status, _ := json.Marshal(issues[l].State)
-		g.items = append(g.items, Item{ID: string(id), Title: string(title), Description: string(description), State: string(status)})
+		i = map[string]interface{}{"id": string(id), "title": string(title), "description": string(description), "state": string(status)}
+		item <- i
 	}
-	return nil
+	close(item)
 }
 
-func (g *Github) Import() error {
+// upload imports the items into database
+func uploadGithub(db *gorm.DB, item map[string]interface{}) error {
+	fmt.Println(item)
 	return nil
 }
