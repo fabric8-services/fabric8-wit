cf5e52c490cfd8ca805d1c8f4aa00c4ed7eeefbd
Konrad Kleine <kkleine@redhat.com>
2016-08-31 16:26:42 +0200

    Fixes #158 Add created/updated/deleted times to DB models

--- models/workitemtype.go
+++ models/workitemtype.go
@@ -2,6 +2,7 @@ package models
 
 // WorkItemType represents a work item type as it is stored in the db
 type WorkItemType struct {
+	Lifecycle
 	// internal id of this work item type
 	ID uint64
 	// the name of this work item type. Does not have to be unique.
