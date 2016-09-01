0fbf9d44589a685c30c80dc4eb1e11aac803b1ae
Dhriti Shikhar <dhrish20@gmail.com>
2016-09-01 12:35:17 +0530

    Implements soft delete

--- models/tracker.go
+++ models/tracker.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 // Tracker represents tracker configuration
 type Tracker struct {
+	gorm.Model
 	ID uint64 `gorm:"primary_key"`
 	// URL of the tracker
 	URL string
