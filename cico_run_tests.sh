882be9887858b9d3a5e54397a7172b970ba0fe50
Konrad Kleine <kwk@users.noreply.github.com>
2016-09-05 14:26:01 +0200

    Simplify CI test script and also run integration tests
    This will simplify the CI test script by avoiding `&&` concatenations through `set -e`. 
    
    Also, we setup an environment for integration tests and tear it down when we exit the script. While the environment is live we run the integration tests.

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
+# Output command before executing
+set -x
+
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
 
@@ -11,8 +17,17 @@ yum -y install \
 service docker start
 
 # lets test
-make docker-start && \
-  make docker-deps && \ 
-  make docker-generate && \
-  make docker-build && \
-  make docker-test-unit
+make docker-start
+make docker-deps
+make docker-generate
+make docker-build
+make docker-test-unit
+
+function cleanup {
+  make integration-test-env-tear-down
+}
+trap cleanup EXIT
+
+make integration-test-env-prepare
+make docker-test-integration
+
