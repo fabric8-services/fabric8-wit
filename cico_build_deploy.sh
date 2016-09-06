aacea790b15a17480a07bf71720614c7ccd528c8
Konrad Kleine <kwk@users.noreply.github.com>
2016-09-06 09:48:22 +0200

    Simplify CI test and deploy script and also run integration tests (#186)
    * Simplify CI test and deploy scripts and also run integration tests
    
    This will simplify the CI test and deploy scripts by avoiding `&&` concatenations through `set -e`. 
    
    Also, we setup an environment for integration tests and tear it down when we exit the script.
    While this environment is live we run the integration tests.
    
    * Use centos/postgresql-95-centos7 instead of postgres:9.5
    * Don't publish the DB port to the host. No need to do that.
    * Simplify the cico_build_deploy.sh and include integration tests
    
    Fixes #23 Setup openshift environment for almighty

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Output command before executing
+set -x
+
+# Exit on error
+set -e
+
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
 
@@ -8,25 +14,25 @@ yum -y install docker make git
 sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --log-driver=journald --insecure-registry registry.ci.centos.org:5000"' /etc/sysconfig/docker
 service docker start
 
-# lets test
-make docker-start && \
-make docker-deps && \
-make docker-generate && \
-make docker-build && \
+# Let's test
+make docker-start
+make docker-deps
+make docker-generate
+make docker-build
 make docker-test-unit
-if [ $? -eq 0 ]; then
-  echo 'CICO: app tests OK'
-  make docker-image-deploy && \
-  docker tag almighty-core-deploy registry.ci.centos.org:5000/almighty/almighty-core:latest && \ 
-  docker push registry.ci.centos.org:5000/almighty/almighty-core:latest
-  if [ $? -eq 0 ]; then
-    echo 'CICO: Image pushed, ready to update deployed app'
-    exit 0
-  else
-    echo 'CICO: Image push to registry failed'
-    exit 2
-  fi
-else
-  echo 'CICO: app tests Failed'
-  exit 1
-fi
+make integration-test-env-prepare
+function cleanup {
+  EXIT_CODE=$?
+  make integration-test-env-tear-down
+  echo 'CICO: Exiting with $EXIT_CODE'
+}
+trap cleanup EXIT
+make docker-test-integration
+echo 'CICO: app tests OK'
+
+# Let's deploy
+make docker-image-deploy
+docker tag almighty-core-deploy registry.ci.centos.org:5000/almighty/almighty-core:latest 
+docker push registry.ci.centos.org:5000/almighty/almighty-core:latest
+echo 'CICO: Image pushed, ready to update deployed app'
+
--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Output command before executing
+set -x
+
+# Exit on error
+set -e
+
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
 
@@ -10,9 +16,19 @@ yum -y install \
   git 
 service docker start
 
-# lets test
-make docker-start && \
-  make docker-deps && \ 
-  make docker-generate && \
-  make docker-build && \
-  make docker-test-unit
+# Let's test
+make docker-start
+make docker-deps
+make docker-generate
+make docker-build
+make docker-test-unit
+
+make integration-test-env-prepare
+
+function cleanup {
+  make integration-test-env-tear-down
+}
+trap cleanup EXIT
+
+make docker-test-integration
+
