ff7bd083736cd0dc12b2d5dc298174aea57ec5fe
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-09-07 20:34:28 +0530

    Avoid duplicate tracker items while importing
    fixes #193

--- remoteworkitem/trackeritem_repository.go
+++ remoteworkitem/trackeritem_repository.go
@@ -5,7 +5,14 @@ import "github.com/jinzhu/gorm"
 // upload imports the items into database
 func upload(db *gorm.DB, tqID int, item map[string]string) error {
 	ti := TrackerItem{Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
-	//db.Where("remote_item_id = ?", item["id"]).Find(&user)
-	err := db.Create(&ti).Error
+	db.Where("remote_item_id = ?", item["id"]).Find(&ti)
+	var err error
+	if ti.ID == 0 {
+		ti1 := TrackerItem{Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
+		err = db.Create(&ti1).Error
+	} else {
+		ti2 := TrackerItem{ID: ti.ID, Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
+		err = db.Save(&ti2).Error
+	}
 	return err
 }
--- remoteworkitem/trackeritem_test.go
+++ remoteworkitem/trackeritem_test.go
@@ -8,11 +8,23 @@ import (
 
 func TestUpload(t *testing.T) {
 	resource.Require(t, resource.Database)
-	t := Tracker{URL: "https://api.github.com/", Type: "github"}
-	db.Create(&t)
-	tq := TrackerQuery{Query: "some random query", Schedule: "0 0 0 * * *", t.ID}
+	db.Exec(`DELETE FROM "tracker_items"`)
+	tr := Tracker{URL: "https://api.github.com/", Type: "github"}
+	db.Create(&tr)
+	tq := TrackerQuery{Query: "some random query", Schedule: "0 0 0 * * *", TrackerID: tr.ID}
 	db.Create(&tq)
-	//ti := TrackerItem{Item: "{}", RemoteItemID: "https://github.com/golang/go/issues/124", BatchID: "1", TrackerID: tq.ID}
-	i := map[string]string{"item": ""}
-	upload(db, tq.ID)
+	db.Delete(&tq)
+	db.Delete(&tr)
+	i := map[string]string{"content": "some text", "id": "https://github.com/golang/go/issues/124", "batch_id": "10"}
+
+	// create
+	err := upload(db, int(tq.ID), i)
+	if err != nil {
+		t.Error("Create error:", err)
+	}
+	// update
+	err = upload(db, int(tq.ID), i)
+	if err != nil {
+		t.Error("Update error:", err)
+	}
 }
