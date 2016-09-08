b68b2e920a287dd2f03bda98b1ab7ab66b454eb9
Shoubhik Bose <sbose78@gmail.com>
2016-09-08 10:35:23 +0530

    Added methods to flatten a multi-level map
    Why:
    So that static maps can be declared to tell the system how to map a remote
    work item to an ALM work item.
    
    How:
    Added a DFS algorithm to walk through nested maps and created a dot delimited key of
    the path traversed.
    
    What:
    - Added the DFS method which looks for nested maps and uses a stack to traverse the map(s).
    - Updated the map keys which are used for mapping from remote to local WI.
    - Added tests to check if the flattenning + mapping works, with the Assignee as an optional field for GH and Jira.
    
    Fixes #214

--- remoteworkitem/remoteworkitem_test.go
+++ remoteworkitem/remoteworkitem_test.go
@@ -1,8 +1,7 @@
-// +build unit
-
 package remoteworkitem
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,8 +10,30 @@ import (
 	"github.com/almighty/almighty-core/test"
 )
 
+const (
+	GithubIssueWithAssignee    = "http://api.github.com/repos/almighty/almighty-core/issues/132"
+	GithubIssueWithoutAssignee = "https://api.github.com/repos/sbose78/mysql-transfer/issues/1"
+	JiraIssueWithAssignee      = "http://jira.atlassian.com/rest/api/latest/issue/JRA-9"
+	JiraIssueWithoutAssignee   = "http://jira.atlassian.com/rest/api/latest/issue/JRA-10"
+)
+
 func provideRemoteGithubData() ([]byte, error) {
-	url := "https://api.github.com/repos/almighty/almighty-core/issues/131"
+	url := GithubIssueWithAssignee
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return responseData, nil
+}
+
+func provideRemoteJiraData() ([]byte, error) {
+	url := JiraIssueWithAssignee
 	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -72,9 +93,95 @@ func TestGitHubIssueMapping(t *testing.T) {
 	}
 
 	for _, localWorkItemKey := range workItemMap {
-		t.Log("Mapped ", localWorkItemKey)
-		if workItem.Fields[localWorkItemKey] == nil {
+		t.Log("Mapping ", localWorkItemKey)
+		_, ok := workItem.Fields[localWorkItemKey]
+		if ok == false {
 			t.Error(fmt.Sprintf("%s not mapped", localWorkItemKey))
+		} else {
+			t.Log("Mapped to ", workItem.Fields[localWorkItemKey])
+		}
+	}
+}
+
+func TestJiraIssueMapping(t *testing.T) {
+
+	content, err := test.LoadTestData("jira_issue_mapping.json", provideRemoteJiraData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	workItemMap := WorkItemKeyMaps[ProviderJira]
+	remoteWorkItem := RemoteWorkItem{ID: "xyz", Content: []byte(content)}
+
+	remoteWorkItemImpl := RemoteWorkItemImplRegistry[ProviderJira]
+	ji, err := remoteWorkItemImpl(remoteWorkItem)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	workItem, err := Map(ji, workItemMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, localWorkItemKey := range workItemMap {
+		t.Log("Mapping ", localWorkItemKey)
+		_, ok := workItem.Fields[localWorkItemKey]
+		if ok == false {
+			t.Error(fmt.Sprintf("%s not mapped", localWorkItemKey))
+		} else {
+			t.Log("Mapped to ", workItem.Fields[localWorkItemKey])
+		}
+	}
+}
+
+func TestFlattenGithubResponseMap(t *testing.T) {
+	testString, err := test.LoadTestData("github_issue_mapping.json", provideRemoteGithubData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var nestedMap map[string]interface{}
+	err = json.Unmarshal(testString, &nestedMap)
+
+	if err != nil {
+		t.Error("Incorrect dataset ", testString)
+	}
+
+	OneLevelMap := Flatten(nestedMap)
+
+	githubKeyMap := WorkItemKeyMaps[ProviderGithub]
+
+	// Verifying if the new map is usable.
+	for k := range githubKeyMap {
+		_, ok := OneLevelMap[string(k)]
+		if ok == false && string(k) != GithubAssignee {
+			// GithubAssignee is an optional field so that being absent is fine
+			t.Error("Could not access the following key from the flattened map ", k)
+		}
+	}
+}
+
+func TestFlattenJiraResponseMap(t *testing.T) {
+	testString, err := test.LoadTestData("jira_issue_mapping.json", provideRemoteJiraData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var nestedMap map[string]interface{}
+	err = json.Unmarshal(testString, &nestedMap)
+
+	if err != nil {
+		t.Error("Incorrect dataset ", testString)
+	}
+
+	OneLevelMap := Flatten(nestedMap)
+	jiraKeyMap := WorkItemKeyMaps[ProviderJira]
+
+	// Verifying if the newly converted map is usable.
+	for k := range jiraKeyMap {
+		_, ok := OneLevelMap[string(k)]
+		if ok == false && string(k) != JiraAssignee {
+			// JiraAssignee is an optional field so that being absent is fine.
+			t.Error("Could not access the following key from the flattened map ", k)
 		}
 	}
 }
