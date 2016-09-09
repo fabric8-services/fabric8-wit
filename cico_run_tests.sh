9cf3b366ab11bc71fa65aab23ab7adac1674c324
Konrad Kleine <kkleine@redhat.com>
2016-09-09 12:58:34 +0200

    Only source those env vars we really need

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -10,8 +10,7 @@ set -e
 # that might interest this worker.
 if [ -e "jenkins-env" ]; then
   cat jenkins-env \
-    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprb)" \
-    | grep -v "ghprbPullLongDescription" \
+    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)" \
     | sed 's/^/export /g' \
     > ~/.jenkins-env
   source ~/.jenkins-env
