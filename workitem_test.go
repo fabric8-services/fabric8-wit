e44485cf94f79c9a944fcc9f1305d8e50dcdd880
Konrad Kleine <kkleine@redhat.com>
2016-09-12 13:42:10 +0200

    Improve coverage of parseInts, parseLimit and NewWorkItemController

--- workitem_test.go
+++ workitem_test.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/almighty/almighty-core/resource"
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
 )
 
 var db *gorm.DB
@@ -155,4 +156,48 @@ func TestListByFields(t *testing.T) {
 	}
 
 	test.DeleteWorkitemOK(t, nil, nil, &controller, wi.ID)
+}
+
+func TestNewWorkitemController(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+	assert.Panics(t, func() {
+		NewWorkitemController(nil, nil, nil)
+	})
+}
+
+func TestParseInts(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+
+	integers, err := parseInts(nil)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, []int{}, integers)
+
+	str := "1, 2, foo"
+	_, err = parseInts(&str)
+	assert.NotNil(t, err)
+}
+
+func TestParseLimit(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+
+	// Test parsing error in parseInts
+	str := "1000, foo"
+	integers, length, err := parseLimit(&str)
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, length)
+	assert.Nil(t, integers)
+
+	// Test length = 1
+	str = "1000"
+	integers, length, err = parseLimit(&str)
+	assert.Nil(t, err)
+	assert.Equal(t, 1000, length)
+	assert.Nil(t, integers)
+
+	// Test empty string
+	str = ""
+	integers, length, err = parseLimit(&str)
+	assert.Nil(t, err)
+	assert.Equal(t, 100, length)
+	assert.Nil(t, integers)
 }
