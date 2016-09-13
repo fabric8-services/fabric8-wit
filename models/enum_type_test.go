b1b7ba3dc0bc57f936261eeeff2e522e3a6d1efa
Konrad Kleine <kkleine@redhat.com>
2016-09-13 14:59:39 +0200

    Improve coverage of enum_type.go's contains() function

--- models/enum_type_test.go
+++ models/enum_type_test.go
@@ -39,3 +39,18 @@ func TestEnumType_Equal(t *testing.T) {
 	}
 	assert.False(t, a.Equal(d))
 }
+
+func TestEnumTypeContains(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+
+	haystack := []interface{}{1,2,3,4}
+
+	// Check for existence
+	needle := interface{}(3)
+	assert.True(t, contains(haystack, needle))
+
+	// Check for absence
+	needle = interface{}(42)
+	assert.False(t, contains(haystack, needle))
+
+}
