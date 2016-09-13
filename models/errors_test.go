8f07aa7fbe8f23ed86dd6bafa6fe81d8e09d8f39
Konrad Kleine <kkleine@redhat.com>
2016-09-13 15:04:50 +0200

    Introduce error template strings and re-use in test

--- models/errors_test.go
+++ models/errors_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/almighty/almighty-core/resource"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"fmt"
 )
 
 func TestSimpleError_Error(t *testing.T) {
@@ -15,11 +16,11 @@ func TestSimpleError_Error(t *testing.T) {
 func TestBadParameterError_Error(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
 	e := BadParameterError{parameter: "foo", value: "bar"}
-	assert.Equal(t, "Bad value for parameter 'foo': 'bar'", e.Error())
+	assert.Equal(t, fmt.Sprintf(stBadParameterErrorMsg, e.parameter, e.value), e.Error())
 }
 
 func TestNotFoundError_Error(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
 	e := NotFoundError{entity: "foo", ID: "bar"}
-	assert.Equal(t, "foo with id 'bar' not found", e.Error())
+	assert.Equal(t, fmt.Sprintf(stNotFoundErrorMsg, e.entity, e.ID), e.Error())
 }
