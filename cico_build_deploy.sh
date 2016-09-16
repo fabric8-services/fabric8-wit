5c04a5ba536c437dc51e040b4246af1f71e5981a
Konrad Kleine <kwk@users.noreply.github.com>
2016-09-16 13:23:58 +0200

    Fixup: Upload coverage to codecov.io (#245)
    This will upload coverage for master and PRs well.
    For now we use a hard-coded token until almighty/almighty-jobs#17 is done.

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -6,11 +6,21 @@ set -x
 # Exit on error
 set -e
 
+# Source environment variables of the jenkins slave
+# that might interest this worker.
+if [ -e "jenkins-env" ]; then
+  cat jenkins-env \
+    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)" \
+    | sed 's/^/export /g' \
+    > ~/.jenkins-env
+  source ~/.jenkins-env
+fi
+
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
 
 # Get all the deps in
-yum -y install docker make git 
+yum -y install docker make git curl 
 sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --log-driver=journald --insecure-registry registry.ci.centos.org:5000"' /etc/sysconfig/docker
 service docker start
 
@@ -32,6 +42,10 @@ echo 'CICO: app tests OK'
 
 # Output coverage
 make docker-coverage-all
+
+# Upload coverage to codecov.io
+cp tmp/coverage.mode* coverage.txt
+bash <(curl -s https://codecov.io/bash) -X search -f coverage.txt -t ad12dad7-ebdc-47bc-a016-8c05fa7356bc
 
 # Let's deploy
 make docker-image-deploy
