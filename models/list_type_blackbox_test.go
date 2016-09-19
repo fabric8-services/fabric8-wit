302f8c0ba9d92156d0e6ba010cab61da0c0d86b9
Konrad Kleine <kkleine@redhat.com>
2016-09-19 10:22:24 +0200

    Add models/list_type_blackbox_test.go

---
+++ models/list_type_blackbox_test.go
@@ -0,0 +1,35 @@
+package models_test
+
+import (
+	"testing"
+
+	. "github.com/almighty/almighty-core/models"
+	"github.com/almighty/almighty-core/resource"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListType_Equal(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+
+	a := ListType{
+		SimpleType:    SimpleType{Kind: KindList},
+		ComponentType: SimpleType{Kind: KindString},
+	}
+
+	// Test type incompatibility
+	assert.False(t, a.Equal(DummyEqualer{}))
+
+	// Test simple type difference
+	b := ListType{
+		SimpleType:    SimpleType{Kind: KindString},
+		ComponentType: SimpleType{Kind: KindString},
+	}
+	assert.False(t, a.Equal(b))
+
+	// Test component type difference
+	c := ListType{
+		SimpleType:    SimpleType{Kind: KindList},
+		ComponentType: SimpleType{Kind: KindInteger},
+	}
+	assert.False(t, a.Equal(c))
+}
