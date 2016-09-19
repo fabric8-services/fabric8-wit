1b4269ac809150e356776056f29142bd5e7f1ed0
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-09-19 10:01:57 +0530

    avoid additional struct

--- remoteworkitem/trackeritem_repository.go
+++ remoteworkitem/trackeritem_repository.go
@@ -7,12 +7,12 @@ func upload(db *gorm.DB, tqID int, item map[string]string) error {
 	ti := TrackerItem{Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
 	db.Where("remote_item_id = ?", item["id"]).Find(&ti)
 	var err error
+	ti1 := TrackerItem{Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
 	if ti.ID == 0 {
-		ti1 := TrackerItem{Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
 		err = db.Create(&ti1).Error
 	} else {
-		ti2 := TrackerItem{ID: ti.ID, Item: item["content"], RemoteItemID: item["id"], BatchID: item["batch_id"], TrackerQueryID: uint64(tqID)}
-		err = db.Save(&ti2).Error
+		ti1.ID = ti.ID
+		err = db.Save(&ti1).Error
 	}
 	return err
 }
