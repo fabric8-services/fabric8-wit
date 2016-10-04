7960ccb219a2e6419b1ce06f3db02e9f88357b36
Aslak Knutsen <aslak@4fs.no>
2016-10-04 11:47:01 -0700

    Fix comment lint warning

--- main.go
+++ main.go
@@ -29,9 +29,9 @@ import (
 var (
 	// Commit current build commit set by build script
 	Commit = "0"
-	// Build time set by build script in ISO 8601 (UTC) format: YYYY-MM-DDThh:mm:ssTZD (see https://www.w3.org/TR/NOTE-datetime for details)
+	// BuildTime set by build script in ISO 8601 (UTC) format: YYYY-MM-DDThh:mm:ssTZD (see https://www.w3.org/TR/NOTE-datetime for details)
 	BuildTime = "0"
-	// Start time in ISO 8601 (UTC) format
+	// StartTime in ISO 8601 (UTC) format
 	StartTime = time.Now().UTC().Format("2006-01-02T15:04:05Z")
 )
 
