66a260b63458b350660f02bf0b8af779c4bc3e29
Shoubhik Bose <sbose78@gmail.com>
2016-10-05 17:43:34 +0530

    Expose auth token keypair & github keys as env variables
    Why?
    Sensitive information used in production namely github secrets, encryption keys needs to be removed from the code. Having done that, it should not impair the average developer environment. There should be room for supplying the information in the form of env variables/config data , and in case none of that is done, there should be defaults to fall back upon.
    
    What?
    Added viper configuration for the same along with the default keys.
    
    Fixes #337

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -70,6 +70,10 @@ const (
 	varPopulateCommonTypes          = "populate.commontypes"
 	varHTTPAddress                  = "http.address"
 	varDeveloperModeEnabled         = "developer.mode.enabled"
+	varGithubSecret                 = "github.secret"
+	varGithubClientID               = "github.client.id"
+	varTokenPublicKey               = "token.publickey"
+	varTokenPrivateKey              = "token.privatekey"
 )
 
 func setConfigDefaults() {
@@ -101,6 +105,12 @@ func setConfigDefaults() {
 	viper.SetDefault(varDeveloperModeEnabled, false)
 
 	viper.SetDefault(varPopulateCommonTypes, true)
+
+	// Auth-related defaults
+	viper.SetDefault(varTokenPublicKey, defaultTokenPublicKey)
+	viper.SetDefault(varTokenPrivateKey, defaultTokenPrivateKey)
+	viper.SetDefault(varGithubClientID, defaultGithubClientID)
+	viper.SetDefault(varGithubSecret, defaultGithubSecret)
 }
 
 // GetPostgresHost returns the postgres host as set via default, config file, or environment variable
@@ -174,3 +184,74 @@ func GetHTTPAddress() string {
 func IsPostgresDeveloperModeEnabled() bool {
 	return viper.GetBool(varDeveloperModeEnabled)
 }
+
+// GetTokenPrivateKey returns the private key (as set via config file or environment variable)
+// that is used to sign the authentication token.
+func GetTokenPrivateKey() []byte {
+	return []byte(viper.GetString(varTokenPrivateKey))
+}
+
+// GetTokenPublicKey returns the public key (as set via config file or environment variable)
+// that is used to decrypt the authentication token.
+func GetTokenPublicKey() []byte {
+	return []byte(viper.GetString(varTokenPublicKey))
+}
+
+// GetGithubSecret returns the Github secret(as set via config file or environment variable)
+// that is used to make authorized Github API Calls.
+func GetGithubSecret() string {
+	return viper.GetString(varGithubSecret)
+}
+
+// GetGithubClientID returns the Github Client ID(as set via config file or environment variable)
+// that is used to make authorized Github API Calls.
+func GetGithubClientID() string {
+	return viper.GetString(varGithubClientID)
+}
+
+// Auth-related defaults
+
+// RSAPrivateKey for signing JWT Tokens
+// ssh-keygen -f alm_rsa
+var defaultTokenPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEAnwrjH5iTSErw9xUptp6QSFoUfpHUXZ+PaslYSUrpLjw1q27O
+DSFwmhV4+dAaTMO5chFv/kM36H3ZOyA146nwxBobS723okFaIkshRrf6qgtD6coT
+HlVUSBTAcwKEjNn4C9jtEpyOl+eSgxhMzRH3bwTIFlLlVMiZf7XVE7P3yuOCpqkk
+2rdYVSpQWQWKU+ZRywJkYcLwjEYjc70AoNpjO5QnY+Exx98E30iEdPHZpsfNhsjh
+9Z7IX5TrMYgz7zBTw8+niO/uq3RBaHyIhDbvenbR9Q59d88lbnEeHKgSMe2RQpFR
+3rxFRkc/64Rn/bMuL/ptNowPqh1P+9GjYzWmPwIDAQABAoIBAQCBCl5ZpnvprhRx
+BVTA/Upnyd7TCxNZmzrME+10Gjmz79pD7DV25ejsu/taBYUxP6TZbliF3pggJOv6
+UxomTB4znlMDUz0JgyjUpkyril7xVQ6XRAPbGrS1f1Def+54MepWAn3oGeqASb3Q
+bAj0Yl12UFTf+AZmkhQpUKk/wUeN718EIY4GRHHQ6ykMSqCKvdnVbMyb9sIzbSTl
+v+l1nQFnB/neyJq6P0Q7cxlhVj03IhYj/AxveNlKqZd2Ih3m/CJo0Abtwhx+qHZp
+cCBrYj7VelEaGARTmfoIVoGxFGKZNCcNzn7R2ic7safxXqeEnxugsAYX/UmMoq1b
+vMYLcaLRAoGBAMqMbbgejbD8Cy6wa5yg7XquqOP5gPdIYYS88TkQTp+razDqKPIU
+hPKetnTDJ7PZleOLE6eJ+dQJ8gl6D/dtOsl4lVRy/BU74dk0fYMiEfiJMYEYuAU0
+MCramo3HAeySTP8pxSLFYqJVhcTpL9+NQgbpJBUlx5bLDlJPl7auY077AoGBAMkD
+UpJRIv/0gYSz5btVheEyDzcqzOMZUVsngabH7aoQ49VjKrfLzJ9WznzJS5gZF58P
+vB7RLuIA8m8Y4FUwxOr4w9WOevzlFh0gyzgNY4gCwrzEryOZqYYqCN+8QLWfq/hL
++gYFYpEW5pJ/lAy2i8kPanC3DyoqiZCsUmlg6JKNAoGBAIdCkf6zgKGhHwKV07cs
+DIqx2p0rQEFid6UB3ADkb+zWt2VZ6fAHXeT7shJ1RK0o75ydgomObWR5I8XKWqE7
+s1dZjDdx9f9kFuVK1Upd1SxoycNRM4peGJB1nWJydEl8RajcRwZ6U+zeOc+OfWbH
+WUFuLadlrEx5212CQ2k+OZlDAoGAdsH2w6kZ83xCFOOv41ioqx5HLQGlYLpxfVg+
+2gkeWa523HglIcdPEghYIBNRDQAuG3RRYSeW+kEy+f4Jc2tHu8bS9FWkRcsWoIji
+ZzBJ0G5JHPtaub6sEC6/ZWe0F1nJYP2KLop57FxKRt0G2+fxeA0ahpMwa2oMMiQM
+4GM3pHUCgYEAj2ZjjsF2MXYA6kuPUG1vyY9pvj1n4fyEEoV/zxY1k56UKboVOtYr
+BA/cKaLPqUF+08Tz/9MPBw51UH4GYfppA/x0ktc8998984FeIpfIFX6I2U9yUnoQ
+OCCAgsB8g8yTB4qntAYyfofEoDiseKrngQT5DSdxd51A/jw7B8WyBK8=
+-----END RSA PRIVATE KEY-----`
+
+// RSAPublicKey for verifying JWT Tokens
+// openssl rsa -in alm_rsa -pubout -out alm_rsa.pub
+var defaultTokenPublicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnwrjH5iTSErw9xUptp6Q
+SFoUfpHUXZ+PaslYSUrpLjw1q27ODSFwmhV4+dAaTMO5chFv/kM36H3ZOyA146nw
+xBobS723okFaIkshRrf6qgtD6coTHlVUSBTAcwKEjNn4C9jtEpyOl+eSgxhMzRH3
+bwTIFlLlVMiZf7XVE7P3yuOCpqkk2rdYVSpQWQWKU+ZRywJkYcLwjEYjc70AoNpj
+O5QnY+Exx98E30iEdPHZpsfNhsjh9Z7IX5TrMYgz7zBTw8+niO/uq3RBaHyIhDbv
+enbR9Q59d88lbnEeHKgSMe2RQpFR3rxFRkc/64Rn/bMuL/ptNowPqh1P+9GjYzWm
+PwIDAQAB
+-----END PUBLIC KEY-----`
+
+var defaultGithubClientID = "875da0d2113ba0a6951d"
+var defaultGithubSecret = "2fe6736e90a9283036a37059d75ac0c82f4f5288"
