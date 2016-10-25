cb3b1f719e5f47b220ad3554351160b33a72a529
Konrad Kleine <kkleine@redhat.com>
2016-10-25 18:23:13 +0200

    Remove dot-imports of goa design and design/apidsl packages
    To be clear upfront: I'm NOT removing the dot-imports simply because
    there's some warning about good Go practices on imports!
    
    I found that no editor could autocomplete or jump to the declaration of
    any of the words in the modified files. Being sick of jumping to the
    online reference of goa, I decided to take the bumpy road and live with
    a DSL that looks a little bit less okay that with the dot-import.
    
    The benefit is, that I get autocompletion and can jump to the definition
    of all the GOA symbols now.
    
    With migration to JSON API ahead of us we're in desperate need of
    control over all files in the design folder.

--- design/media_types.go
+++ design/media_types.go
@@ -1,199 +1,200 @@
 package design
 
 import (
-	. "github.com/goadesign/goa/design"
-	. "github.com/goadesign/goa/design/apidsl"
+	d "github.com/goadesign/goa/design"
+	a "github.com/goadesign/goa/design/apidsl"
 )
 
 // ALMStatus defines the status of the current running ALM instance
-var ALMStatus = MediaType("application/vnd.status+json", func() {
-	Description("The status of the current running instance")
-	Attributes(func() {
-		Attribute("commit", String, "Commit SHA this build is based on")
-		Attribute("buildTime", String, "The time when built")
-		Attribute("startTime", String, "The time when started")
-		Attribute("error", String, "The error if any")
-		Required("commit", "buildTime", "startTime")
+var ALMStatus = a.MediaType("application/vnd.status+json", func() {
+	a.Description("The status of the current running instance")
+	a.Attributes(func() {
+		a.Attribute("commit", d.String, "Commit SHA this build is based on")
+		a.Attribute("buildTime", d.String, "The time when built")
+		a.Attribute("startTime", d.String, "The time when started")
+		a.Attribute("error", d.String, "The error if any")
+		a.Required("commit", "buildTime", "startTime")
 	})
-	View("default", func() {
-		Attribute("commit")
-		Attribute("buildTime")
-		Attribute("startTime")
-		Attribute("error")
+	a.View("default", func() {
+		a.Attribute("commit")
+		a.Attribute("buildTime")
+		a.Attribute("startTime")
+		a.Attribute("error")
 	})
 })
 
 // AuthToken represents an authentication JWT Token
-var AuthToken = MediaType("application/vnd.authtoken+json", func() {
-	TypeName("AuthToken")
-	Description("JWT Token")
-	Attributes(func() {
-		Attribute("token", String, "JWT Token")
-		Required("token")
+var AuthToken = a.MediaType("application/vnd.authtoken+json", func() {
+	a.TypeName("AuthToken")
+	a.Description("JWT Token")
+	a.Attributes(func() {
+		a.Attribute("token", d.String, "JWT Token")
+		a.Required("token")
 	})
-	View("default", func() {
-		Attribute("token")
+	a.View("default", func() {
+		a.Attribute("token")
 	})
 })
 
 // workItem is the media type for work items
-var workItem = MediaType("application/vnd.workitem+json", func() {
-	TypeName("WorkItem")
-	Description("A work item hold field values according to a given field type")
-	Attribute("id", String, "unique id per installation")
-	Attribute("version", Integer, "Version for optimistic concurrency control")
-	Attribute("type", String, "Name of the type of this work item")
-	Attribute("fields", HashOf(String, Any), "The field values, according to the field type")
+var workItem = a.MediaType("application/vnd.workitem+json", func() {
+	a.TypeName("WorkItem")
+	a.Description("A work item hold field values according to a given field type")
+	a.Attribute("id", d.String, "unique id per installation")
+	a.Attribute("version", d.Integer, "Version for optimistic concurrency control")
+	a.Attribute("type", d.String, "Name of the type of this work item")
+	a.Attribute("fields", a.HashOf(d.String, d.Any), "The field values, according to the field type")
 
-	Required("id")
-	Required("version")
-	Required("type")
-	Required("fields")
+	a.Required("id")
+	a.Required("version")
+	a.Required("type")
+	a.Required("fields")
 
-	View("default", func() {
-		Attribute("id")
-		Attribute("version")
-		Attribute("type")
-		Attribute("fields")
+	a.View("default", func() {
+		a.Attribute("id")
+		a.Attribute("version")
+		a.Attribute("type")
+		a.Attribute("fields")
 	})
 })
 
-var pagingLinks = Type("pagingLinks", func() {
-	Attribute("prev", String)
-	Attribute("next", String)
-	Attribute("first", String)
-	Attribute("last", String)
+var pagingLinks = a.Type("pagingLinks", func() {
+	a.Attribute("prev", d.String)
+	a.Attribute("next", d.String)
+	a.Attribute("first", d.String)
+	a.Attribute("last", d.String)
 })
 
-var meta = Type("workItemListResponseMeta", func() {
-	Attribute("totalCount", Number)
+var meta = a.Type("workItemListResponseMeta", func() {
+	a.Attribute("totalCount", d.Number)
 
-	Required("totalCount")
+	a.Required("totalCount")
 })
 
 // workItemListResponse contains paged results for listing work items and paging links
-var workItemListResponse = MediaType("application/vnd.workitemlist+json", func() {
-	TypeName("WorkItemListResponse")
-	Description("Holds the paginated response to a work item list request")
-	Attribute("links", pagingLinks)
-	Attribute("meta", meta)
-	Attribute("data", CollectionOf(workItem))
+var workItemListResponse = a.MediaType("application/vnd.workitemlist+json", func() {
+	a.TypeName("WorkItemListResponse")
+	a.Description("Holds the paginated response to a work item list request")
+	a.Attribute("links", pagingLinks)
+	a.Attribute("meta", meta)
+	a.Attribute("data", a.CollectionOf(workItem))
 
-	Required("links")
-	Required("meta")
-	Required("data")
+	a.Required("links")
+	a.Required("meta")
+	a.Required("data")
 
-	View("default", func() {
-		Attribute("links", func() {
-			Attribute("prev", String)
-			Attribute("next", String)
-			Attribute("first", String)
-			Attribute("last", String)
+	a.View("default", func() {
+		a.Attribute("links", func() {
+			a.Attribute("prev", d.String)
+			a.Attribute("next", d.String)
+			a.Attribute("first", d.String)
+			a.Attribute("last", d.String)
 		})
-		Attribute("meta", func() {
-			Attribute("totalCount", Number)
+		a.Attribute("meta", func() {
+			a.Attribute("totalCount", d.Number)
 		})
-		Attribute("data")
+		a.Attribute("data")
 	})
 })
 
 // fieldDefinition defines the possible values for a field in a work item type
-var fieldDefinition = Type("fieldDefinition", func() {
-	Description("A fieldDescription aggregates a fieldType and additional field metadata")
-	Attribute("required", Boolean)
-	Attribute("type", fieldType)
+var fieldDefinition = a.Type("fieldDefinition", func() {
+	a.Description("A fieldDescription aggregates a fieldType and additional field metadata")
+	a.Attribute("required", d.Boolean)
+	a.Attribute("type", fieldType)
 
-	Required("required")
-	Required("type")
+	a.Required("required")
+	a.Required("type")
 
-	View("default", func() {
-		Attribute("kind")
+	a.View("default", func() {
+		a.Attribute("kind")
 	})
 })
 
 // fieldType is the datatype of a single field in a work item tepy
-var fieldType = Type("fieldType", func() {
-	Description("A fieldType describes the values a particular field can hold")
-	Attribute("kind", String, "The constant indicating the kind of type, for example 'string' or 'enum' or 'instant'")
-	Attribute("componentType", String, "The kind of type of the individual elements for a list type. Required for list types. Must be a simple type, not  enum or list")
-	Attribute("baseType", String, "The kind of type of the enumeration values for an enum type. Required for enum types. Must be a simple type, not  enum or list")
-	Attribute("values", ArrayOf(Any), "The possible values for an enum type. The values must be of a type convertible to the base type")
+var fieldType = a.Type("fieldType", func() {
+	a.Description("A fieldType describes the values a particular field can hold")
+	a.Attribute("kind", d.String, "The constant indicating the kind of type, for example 'string' or 'enum' or 'instant'")
+	a.Attribute("componentType", d.String, "The kind of type of the individual elements for a list type. Required for list types. Must be a simple type, not  enum or list")
+	a.Attribute("baseType", d.String, "The kind of type of the enumeration values for an enum type. Required for enum types. Must be a simple type, not  enum or list")
+	a.Attribute("values", a.ArrayOf(d.Any), "The possible values for an enum type. The values must be of a type convertible to the base type")
 
-	Required("kind")
+	a.Required("kind")
 })
 
 // workItemType is the media type representing a work item type.
-var workItemType = MediaType("application/vnd.workitemtype+json", func() {
-	TypeName("WorkItemType")
-	Description("A work item type describes the values a work item type instance can hold.")
-	Attribute("version", Integer, "Version for optimistic concurrency control")
-	Attribute("name", String, "User Readable Name of this item type")
-	Attribute("fields", HashOf(String, fieldDefinition), "Definitions of fields in this work item type")
+var workItemType = a.MediaType("application/vnd.workitemtype+json", func() {
+	a.TypeName("WorkItemType")
+	a.Description("A work item type describes the values a work item type instance can hold.")
+	a.Attribute("version", d.Integer, "Version for optimistic concurrency control")
+	a.Attribute("name", d.String, "User Readable Name of this item type")
+	a.Attribute("fields", a.HashOf(d.String, fieldDefinition), "Definitions of fields in this work item type")
 
-	Required("version")
-	Required("name")
-	Required("fields")
+	a.Required("version")
+	a.Required("name")
+	a.Required("fields")
 
-	View("default", func() {
-		Attribute("version")
-		Attribute("name")
-		Attribute("fields")
+	a.View("default", func() {
+		a.Attribute("version")
+		a.Attribute("name")
+		a.Attribute("fields")
 	})
-	View("link", func() {
-		Attribute("name")
+	a.View("link", func() {
+		a.Attribute("name")
 	})
 
 })
 
 // Tracker configuration
-var Tracker = MediaType("application/vnd.tracker+json", func() {
-	TypeName("Tracker")
-	Description("Tracker configuration")
-	Attribute("id", String, "unique id per tracker")
-	Attribute("url", String, "URL of the tracker")
-	Attribute("type", String, "Type of the tracker")
+var Tracker = a.MediaType("application/vnd.tracker+json", func() {
+	a.TypeName("Tracker")
+	a.Description("Tracker configuration")
+	a.Attribute("id", d.String, "unique id per tracker")
+	a.Attribute("url", d.String, "URL of the tracker")
+	a.Attribute("type", d.String, "Type of the tracker")
 
-	Required("id")
-	Required("url")
-	Required("type")
+	a.Required("id")
+	a.Required("url")
+	a.Required("type")
 
-	View("default", func() {
-		Attribute("id")
-		Attribute("url")
-		Attribute("type")
+	a.View("default", func() {
+		a.Attribute("id")
+		a.Attribute("url")
+		a.Attribute("type")
 	})
 })
 
 // TrackerQuery represents the search query with schedule
-var TrackerQuery = MediaType("application/vnd.trackerquery+json", func() {
-	TypeName("TrackerQuery")
-	Description("Tracker query with schedule")
-	Attribute("id", String, "unique id per installation")
-	Attribute("query", String, "Search query")
-	Attribute("schedule", String, "Schedule for fetch and import")
-	Attribute("trackerID", String, "Tracker ID")
+var TrackerQuery = a.MediaType("application/vnd.trackerquery+json", func() {
+	a.TypeName("TrackerQuery")
+	a.Description("Tracker query with schedule")
+	a.Attribute("id", d.String, "unique id per installation")
+	a.Attribute("query", d.String, "Search query")
+	a.Attribute("schedule", d.String, "Schedule for fetch and import")
+	a.Attribute("trackerID", d.String, "Tracker ID")
 
-	Required("id")
-	Required("query")
-	Required("schedule")
-	Required("trackerID")
+	a.Required("id")
+	a.Required("query")
+	a.Required("schedule")
+	a.Required("trackerID")
 
-	View("default", func() {
-		Attribute("id")
-		Attribute("query")
-		Attribute("schedule")
-		Attribute("trackerID")
+	a.View("default", func() {
+		a.Attribute("id")
+		a.Attribute("query")
+		a.Attribute("schedule")
+		a.Attribute("trackerID")
 	})
 })
 
-var User = MediaType("application/vnd.user+json", func() {
-	TypeName("User")
-	Description("ALM User")
-	Attribute("fullName", String, "The users full name")
-	Attribute("imageURL", String, "The avatar image for the user")
+// User represents a user object (TODO: add better description)
+var User = a.MediaType("application/vnd.user+json", func() {
+	a.TypeName("User")
+	a.Description("ALM User")
+	a.Attribute("fullName", d.String, "The users full name")
+	a.Attribute("imageURL", d.String, "The avatar image for the user")
 
-	View("default", func() {
-		Attribute("fullName")
-		Attribute("imageURL")
+	a.View("default", func() {
+		a.Attribute("fullName")
+		a.Attribute("imageURL")
 	})
 })
