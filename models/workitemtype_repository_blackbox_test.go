f30eb35e9bf1a01bacb06154e2d92f7891e2da62
Thomas Mäder <tmader@redhat.com>
2016-11-01 16:28:13 +0100

    Don't rely on transaction behaviour for setup

--- models/workitemtype_repository_blackbox_test.go
+++ models/workitemtype_repository_blackbox_test.go
@@ -1,7 +1,8 @@
 package models_test
 
 import (
-	"context"
+	"golang.org/x/net/context"
+
 	"testing"
 
 	"github.com/almighty/almighty-core/app"
@@ -11,43 +12,38 @@ import (
 )
 
 func TestCreateLoadWIT(t *testing.T) {
-	doWithTransaction(t, func(ts *models.GormTransactionSupport) {
-		repo := models.NewWorkItemTypeRepository(ts)
-		wit, err := repo.Load(context.Background(), "foo.bar")
-		assert.IsType(t, models.NotFoundError{}, err)
-		assert.Nil(t, wit)
-		wit, err = repo.Create(context.Background(), nil, "foo.bar", map[string]app.FieldDefinition{
-			"foo": app.FieldDefinition{
-				Required: true,
-				Type:     &app.FieldType{Kind: string(models.KindFloat)},
-			},
-		})
-		assert.Nil(t, err)
-		assert.NotNil(t, wit)
-
-		wit3, err := repo.Create(context.Background(), nil, "foo.bar", map[string]app.FieldDefinition{})
-		assert.IsType(t, models.BadParameterError{}, err)
-		assert.Nil(t, wit3)
-
-		wit2, err := repo.Load(context.Background(), "foo.bar")
-		assert.Nil(t, err)
-		assert.NotNil(t, wit2)
-		field := wit2.Fields["foo"]
-		assert.NotNil(t, field)
-		assert.Equal(t, string(models.KindFloat), field.Type.Kind)
-		assert.Equal(t, true, field.Required)
-		assert.Nil(t, field.Type.ComponentType)
-		assert.Nil(t, field.Type.BaseType)
-		assert.Nil(t, field.Type.Values)
-	})
-}
-
-func doWithTransaction(t *testing.T, todo func(ts *models.GormTransactionSupport)) {
 	resource.Require(t, resource.Database)
-	ts := models.NewGormTransactionSupport(db)
-	if err := ts.Begin(); err != nil {
-		panic(err.Error())
+
+	repo := models.NewWorkItemTypeRepository(db)
+	db2 := db.Unscoped().Delete(models.WorkItemType{Name: "foo.bar"})
+
+	if db2.Error != nil {
+		t.Fatalf("Could not setup test %s", db2.Error.Error())
+		return
 	}
-	defer ts.Rollback()
-	todo(ts)
+
+	wit, err := repo.Create(context.Background(), nil, "foo.bar", map[string]app.FieldDefinition{
+		"foo": app.FieldDefinition{
+			Required: true,
+			Type:     &app.FieldType{Kind: string(models.KindFloat)},
+		},
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, wit)
+	defer db.Unscoped().Delete(models.WorkItemType{Name: "foo.bar"})
+
+	wit3, err := repo.Create(context.Background(), nil, "foo.bar", map[string]app.FieldDefinition{})
+	assert.IsType(t, models.BadParameterError{}, err)
+	assert.Nil(t, wit3)
+
+	wit2, err := repo.Load(context.Background(), "foo.bar")
+	assert.Nil(t, err)
+	assert.NotNil(t, wit2)
+	field := wit2.Fields["foo"]
+	assert.NotNil(t, field)
+	assert.Equal(t, string(models.KindFloat), field.Type.Kind)
+	assert.Equal(t, true, field.Required)
+	assert.Nil(t, field.Type.ComponentType)
+	assert.Nil(t, field.Type.BaseType)
+	assert.Nil(t, field.Type.Values)
 }
