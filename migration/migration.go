19b685739012bfd6459d765447006309756a552d
Alexey Kazakov <alkazako@redhat.com>
2016-11-22 17:25:16 -0800

    Use WIT cache instead of fetching same WITs many times
    This cache is just a simple synchronized unexported global var.
    
    Fixes #242

--- migration/migration.go
+++ migration/migration.go
@@ -341,6 +341,7 @@ func PopulateCommonTypes(ctx context.Context, db *gorm.DB, witr *workitem.GormWo
 	if err := createOrUpdatePlannerItemExtension(workitem.SystemBug, ctx, witr, db); err != nil {
 		return errs.WithStack(err)
 	}
+	witr.ClearCache() // Clear the WIT cache after updating existing WITs
 	return nil
 }
 
