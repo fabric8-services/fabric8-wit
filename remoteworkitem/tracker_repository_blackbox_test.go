90158fd8e82674152428d46419c18c4b1cd1a0ed
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-12-08 21:56:19 +0530

    Special ID:0 handling to tracker & tracker query (#556)
    Support Delete, Save & Load
    
    Related to #555

---
+++ remoteworkitem/tracker_repository_blackbox_test.go
@@ -0,0 +1,77 @@
+package remoteworkitem_test
+
+import (
+	"testing"
+
+	"github.com/almighty/almighty-core/application"
+	"github.com/almighty/almighty-core/gormsupport"
+	"github.com/almighty/almighty-core/remoteworkitem"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/net/context"
+)
+
+type trackerRepoBlackBoxTest struct {
+	gormsupport.DBTestSuite
+	repo application.TrackerRepository
+}
+
+func TestRunTrackerRepoBlackBoxTest(t *testing.T) {
+	suite.Run(t, &trackerRepoBlackBoxTest{DBTestSuite: gormsupport.NewDBTestSuite("../config.yaml")})
+}
+
+func (s *trackerRepoBlackBoxTest) SetupTest() {
+	s.repo = remoteworkitem.NewTrackerRepository(s.DB)
+}
+
+func (s *trackerRepoBlackBoxTest) TestFailDeleteZeroID() {
+	defer gormsupport.DeleteCreatedEntities(s.DB)()
+
+	// Create at least 1 item to avoid RowsEffectedCheck
+	_, err := s.repo.Create(
+		context.Background(),
+		"http://api.github.com",
+		remoteworkitem.ProviderGithub)
+
+	if err != nil {
+		s.T().Error("Could not create tracker", err)
+	}
+
+	err = s.repo.Delete(context.Background(), "0")
+	require.IsType(s.T(), remoteworkitem.NotFoundError{}, err)
+}
+
+func (s *trackerRepoBlackBoxTest) TestFailSaveZeroID() {
+	defer gormsupport.DeleteCreatedEntities(s.DB)()
+
+	// Create at least 1 item to avoid RowsEffectedCheck
+	tr, err := s.repo.Create(
+		context.Background(),
+		"http://api.github.com",
+		remoteworkitem.ProviderGithub)
+
+	if err != nil {
+		s.T().Error("Could not create tracker", err)
+	}
+	tr.ID = "0"
+
+	_, err = s.repo.Save(context.Background(), *tr)
+	require.IsType(s.T(), remoteworkitem.NotFoundError{}, err)
+}
+
+func (s *trackerRepoBlackBoxTest) TestFaiLoadZeroID() {
+	defer gormsupport.DeleteCreatedEntities(s.DB)()
+
+	// Create at least 1 item to avoid RowsEffectedCheck
+	_, err := s.repo.Create(
+		context.Background(),
+		"http://api.github.com",
+		remoteworkitem.ProviderGithub)
+
+	if err != nil {
+		s.T().Error("Could not create tracker", err)
+	}
+
+	_, err = s.repo.Load(context.Background(), "0")
+	require.IsType(s.T(), remoteworkitem.NotFoundError{}, err)
+}
