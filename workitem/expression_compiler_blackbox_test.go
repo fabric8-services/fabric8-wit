320a30e34efab4e8e7c795fa658e1de4a544006a
Aslak Knutsen <aslak@4fs.no>
2016-12-19 00:18:24 +0100

    fixup! Add filter[assignee] support on WorkItem Endpoint

--- workitem/expression_compiler_blackbox_test.go
+++ workitem/expression_compiler_blackbox_test.go
@@ -14,7 +14,7 @@ import (
 func TestField(t *testing.T) {
 	t.Parallel()
 	resource.Require(t, resource.UnitTest)
-	expect(t, Equals(Field("foo"), Literal(23)), "(Fields@>'{\"foo\" : ?}')", []interface{}{"23"})
+	expect(t, Equals(Field("foo"), Literal(23)), "(Fields@>'{\"foo\" : 23}')", []interface{}{})
 	expect(t, Equals(Field("Type"), Literal("abcd")), "(Type = ?)", []interface{}{"abcd"})
 }
 
@@ -23,8 +23,8 @@ func TestAndOr(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
 	expect(t, Or(Literal(true), Literal(false)), "(? or ?)", []interface{}{true, false})
 
-	expect(t, And(Equals(Field("foo"), Literal("abcd")), Equals(Literal(true), Literal(false))), "((Fields@>'{\"foo\" : ?}') and (? = ?))", []interface{}{"\"abcd\"", true, false})
-	expect(t, Or(Equals(Field("foo"), Literal("abcd")), Equals(Literal(true), Literal(false))), "((Fields@>'{\"foo\" : ?}') or (? = ?))", []interface{}{"\"abcd\"", true, false})
+	expect(t, And(Equals(Field("foo"), Literal("abcd")), Equals(Literal(true), Literal(false))), "((Fields@>'{\"foo\" : \"abcd\"}') and (? = ?))", []interface{}{true, false})
+	expect(t, Or(Equals(Field("foo"), Literal("abcd")), Equals(Literal(true), Literal(false))), "((Fields@>'{\"foo\" : \"abcd\"}') or (? = ?))", []interface{}{true, false})
 }
 
 func expect(t *testing.T, expr Expression, expectedClause string, expectedParameters []interface{}) {
