3d79b902f2a3b91a1fae0467b7afde067b1a2692
Pranav Gore <pranavgore09@gmail.com>
2016-12-27 10:51:49 +0530

    Fixes #468
    Moved tracker related design stuff to design/trackers.go
    Make Create API jsonapi compatible
    Update existing Test Cases to use new create payload.

--- tracker_test.go
+++ tracker_test.go
@@ -98,8 +98,9 @@ func TestTrackerListItemsNotNil(t *testing.T) {
 		URL:  "http://issues.jboss.com",
 		Type: "jira",
 	})
-	test.CreateTrackerCreated(t, nil, nil, &controller, &payload)
-	test.CreateTrackerCreated(t, nil, nil, &controller, &payload)
+	_, item1 := test.CreateTrackerCreated(t, nil, nil, &controller, &payload)
+
+	_, item2 := test.CreateTrackerCreated(t, nil, nil, &controller, &payload)
 
 	_, list := test.ListTrackerOK(t, nil, nil, &controller, nil, nil)
 
