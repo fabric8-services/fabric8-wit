dbc4dcb3df701f012df38eebe0829f613324d7d4
Konrad Kleine <kkleine@redhat.com>
2017-01-02 16:53:46 +0100

    Check for errors after db.Create

--- remoteworkitem/trackeritem_repository_test.go
+++ remoteworkitem/trackeritem_repository_test.go
@@ -19,11 +19,13 @@ func TestConvertNewWorkItem(t *testing.T) {
 
 	// Setting up the dependent tracker query and tracker data in the Database
 	tr := Tracker{URL: "https://api.github.com/", Type: ProviderGithub}
-	db.Create(&tr)
+	db = db.Create(&tr)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tr)
 
 	tq := TrackerQuery{Query: "some random query", Schedule: "0 0 0 * * *", TrackerID: tr.ID}
-	db.Create(&tq)
+	db = db.Create(&tq)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tq)
 
 	t.Log("Created Tracker Query and Tracker")
@@ -56,11 +58,13 @@ func TestConvertExistingWorkItem(t *testing.T) {
 
 	// Setting up the dependent tracker query and tracker data in the Database
 	tr := Tracker{URL: "https://api.github.com/", Type: ProviderGithub}
-	db.Create(&tr)
+	db = db.Create(&tr)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tr)
 
 	tq := TrackerQuery{Query: "some random query", Schedule: "0 0 0 * * *", TrackerID: tr.ID}
-	db.Create(&tq)
+	db = db.Create(&tq)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tq)
 
 	t.Log("Created Tracker Query and Tracker")
@@ -114,11 +118,13 @@ func TestConvertGithubIssue(t *testing.T) {
 	t.Log("Scenario 3 : Mapping and persisting a Github issue")
 
 	tr := Tracker{URL: "https://api.github.com/", Type: ProviderGithub}
-	db.Create(&tr)
+	db = db.Create(&tr)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tr)
 
 	tq := TrackerQuery{Query: "some random query", Schedule: "0 0 0 * * *", TrackerID: tr.ID}
-	db.Create(&tq)
+	db = db.Create(&tq)
+	require.Nil(t, db.Error)
 	defer db.Delete(&tq)
 
 	models.Transactional(db, func(tx *gorm.DB) error {
