1820af03762d37a3e8b1df47a780f57eb298116a
Konrad Kleine <kkleine@redhat.com>
2017-01-09 10:22:27 +0100

    Ensure system.bug and others are created before running the remoteworkitem tests

--- remoteworkitem/scheduler_test.go
+++ remoteworkitem/scheduler_test.go
@@ -6,10 +6,14 @@ import (
 	"testing"
 
 	"github.com/almighty/almighty-core/configuration"
+	"github.com/almighty/almighty-core/migration"
+	"github.com/almighty/almighty-core/models"
 	"github.com/almighty/almighty-core/resource"
+	"github.com/almighty/almighty-core/workitem"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
 )
 
 var db *gorm.DB
@@ -27,6 +31,15 @@ func TestMain(m *testing.M) {
 			panic("Failed to connect database: " + err.Error())
 		}
 		defer db.Close()
+
+		// Make sure the database is populated with the correct types (e.g. system.bug etc.)
+		if configuration.GetPopulateCommonTypes() {
+			if err := models.Transactional(db, func(tx *gorm.DB) error {
+				return migration.PopulateCommonTypes(context.Background(), tx, workitem.NewWorkItemTypeRepository(tx))
+			}); err != nil {
+				panic(err.Error())
+			}
+		}
 	}
 	os.Exit(m.Run())
 }
