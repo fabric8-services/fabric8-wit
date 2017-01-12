353e97e44a0103edd1a92d734bfd2cd54702bf77
Konrad Kleine <kkleine@redhat.com>
2017-01-12 07:48:49 +0100

    Really only test github.com/almighty/almighty-core

--- .make/test.mk
+++ .make/test.mk
@@ -137,7 +137,7 @@ test-integration: prebuild-check clean-coverage-integration migrate-database $(C
 test-integration-no-coverage: prebuild-check migrate-database $(SOURCES)
 	$(call log-info,"Running test: $@")
 	$(eval TEST_PACKAGES:=$(shell go list ./... | grep -v $(ALL_PKGS_EXCLUDE_PATTERN)))
-	ALMIGHTY_RESOURCE_DATABASE=1 ALMIGHTY_RESOURCE_UNIT_TEST=0 go test -v $(TEST_PACKAGES) github.com/almighty/almighty-core
+	ALMIGHTY_RESOURCE_DATABASE=1 ALMIGHTY_RESOURCE_UNIT_TEST=0 go test -v github.com/almighty/almighty-core
 
 # Add one by one until it breaks on CI
 # github.com/almighty/almighty-core/account
