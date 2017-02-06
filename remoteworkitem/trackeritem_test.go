b648c378e132cd1eef20bee828462e7899e23dc0
Hector Fernandez <hfernand@redhat.com>
2017-02-06 20:42:24 +0100

    Aligned types with the print argument calls (#729)
    Aligned types with the print argument calls

--- remoteworkitem/trackeritem_test.go
+++ remoteworkitem/trackeritem_test.go
@@ -28,7 +28,7 @@ func TestUpload(t *testing.T) {
 		t.Errorf("Content not saved: %s", i.Content)
 	}
 	if ti1.TrackerID != tr.ID {
-		t.Errorf("Tracker ID not saved: %s", tr.ID)
+		t.Errorf("Tracker ID not saved: %d", tr.ID)
 	}
 
 	i = TrackerItemContent{Content: []byte("some text 2"), ID: "https://github.com/golang/go/issues/124"}
@@ -43,7 +43,7 @@ func TestUpload(t *testing.T) {
 		t.Errorf("Content not saved: %s", i.Content)
 	}
 	if ti2.TrackerID != tr.ID {
-		t.Errorf("Tracker ID not saved: %s", tq.ID)
+		t.Errorf("Tracker ID not saved: %d", tq.ID)
 	}
 	var count int
 	db.Model(&TrackerItem{}).Where("remote_item_id = ?", i.ID).Count(&count)
