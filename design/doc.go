028a1a65396515855ea8d26131d19eec7aecdac7
Hector Fernandez <hectorj@gmail.com>
2017-02-18 18:17:50 +0100

    Added doc files to improve the documentation

---
+++ design/doc.go
@@ -0,0 +1,4 @@
+// Package design contains the generic API machinery code of our adder API generated
+// using goa framework. This generated API accepts HTTP GET requests from multiple
+// clients.
+package design
