ad9c47765bbb6f1b47994ed58756848c2911bc2d
Xavier Coulon <xcoulon@redhat.com>
2017-02-23 11:22:53 +0100

    Applying review comments
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- migration/sql-files/029-identities-foreign-key.sql
+++ migration/sql-files/029-identities-foreign-key.sql
@@ -1,2 +1,2 @@
 -- Refactor Identities: add a foreign key constraint
-alter table identities add constraint identies_user_id_users_id_foreign foreign key (user_id) REFERENCES users (id);
+alter table identities add constraint identities_user_id_users_id_fk foreign key (user_id) REFERENCES users (id);
