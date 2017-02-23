a7f2263645b3786fecedd89563a3678d7a493150
Hector Fernandez <hectorj@gmail.com>
2017-02-23 13:12:11 +0100

    Adapted to use DELETE CASCADE

--- migration/sql-files/029-add-space-id-wilt.sql
+++ migration/sql-files/029-add-space-id-wilt.sql
@@ -1,6 +1,6 @@
 -- Alter the tables
-ALTER TABLE work_item_link_types ADD space_id uuid;
-ALTER TABLE work_item_link_types ADD FOREIGN KEY (space_id) REFERENCES spaces(id);
+ALTER TABLE work_item_link_types ADD space_id uuid NOT NULL;
+ALTER TABLE work_item_link_types ADD FOREIGN KEY (space_id) REFERENCES spaces(id) ON DELETE CASCADE;
 
 -- Create indexes
 CREATE INDEX ix_space_id ON work_item_link_types USING btree (space_id);
