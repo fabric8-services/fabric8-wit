f07373f746038bca2c2eb92ec4b48853e961eb5e
Pranav Gore <pranavgore09@gmail.com>
2017-02-25 14:27:55 +0530

    Added unit tests for path's value() and scan().

--- path/path_test.go
+++ path/path_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/almighty/almighty-core/path"
 	"github.com/almighty/almighty-core/resource"
 	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,7 +27,7 @@ func TestThis(t *testing.T) {
 	grandParent := uuid.NewV4()
 	immediateParent := uuid.NewV4()
 	lp := path.LtreePath{greatGrandParent, grandParent, immediateParent}
-	require.Equal(t, immediateParent, lp.This())
+	assert.Equal(t, immediateParent, lp.This())
 
 	lp2 := path.LtreePath{}
 	require.Equal(t, uuid.Nil, lp2.This())
@@ -40,7 +41,7 @@ func TestConvert(t *testing.T) {
 	lp := path.LtreePath{grandParent, immediateParent}
 	expected := fmt.Sprintf("%s.%s", grandParent, immediateParent)
 	expected = strings.Replace(expected, "-", "_", -1)
-	require.Equal(t, expected, lp.Convert())
+	assert.Equal(t, expected, lp.Convert())
 
 	lp2 := path.LtreePath{}
 	require.Empty(t, lp2.Convert())
@@ -65,10 +66,10 @@ func TestRoot(t *testing.T) {
 	grandParent := uuid.NewV4()
 	immediateParent := uuid.NewV4()
 	lp := path.LtreePath{grandParent, immediateParent}
-	require.Equal(t, path.LtreePath{grandParent}, lp.Root())
+	assert.Equal(t, path.LtreePath{grandParent}, lp.Root())
 
 	lp2 := path.LtreePath{}
-	require.Equal(t, path.LtreePath{uuid.Nil}, lp2.Root())
+	assert.Equal(t, path.LtreePath{uuid.Nil}, lp2.Root())
 }
 
 func TestParent(t *testing.T) {
@@ -81,4 +82,38 @@ func TestParent(t *testing.T) {
 
 	lp2 := path.LtreePath{}
 	require.Equal(t, path.LtreePath{uuid.Nil}, lp2.Parent())
+}
+
+func TestValuerImplementation(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+	t.Parallel()
+
+	grandParent := uuid.NewV4()
+	immediateParent := uuid.NewV4()
+	lp := path.LtreePath{grandParent, immediateParent}
+	expected := fmt.Sprintf("%s.%s", grandParent, immediateParent)
+	expected = strings.Replace(expected, "-", "_", -1)
+	v, err := lp.Value()
+	require.Nil(t, err)
+	assert.Equal(t, expected, v)
+}
+
+func TestScannerImplementation(t *testing.T) {
+	grandParent := uuid.NewV4()
+	immediateParent := uuid.NewV4()
+	lp := path.LtreePath{grandParent, immediateParent}
+	v, err := lp.Value()
+	require.Nil(t, err)
+
+	lp2 := path.LtreePath{}
+	err2 := lp2.Scan([]byte(v.(string)))
+	require.Nil(t, err2)
+	require.Len(t, lp2, 2)
+	assert.Equal(t, lp2, lp)
+	assert.Equal(t, lp[0], lp2[0])
+
+	lp3 := path.LtreePath{}
+	err3 := lp2.Scan(nil)
+	require.Nil(t, err3)
+	assert.Len(t, lp3, 0)
 }
