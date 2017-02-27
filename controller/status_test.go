c4c21c7b9a7b8c3154e5881445deb154a1e033e4
Hector Fernandez <hfernand@redhat.com>
2017-02-27 15:51:38 +0100

    Moved the controllers to the controller pkg (#773)
    We now use the new goa command `controller` to generate the code inside a specific folder (https://github.com/goadesign/goa/pull/1049).
    
    related to: https://github.com/almighty/almighty-core/issues/723 in particular to this comment https://github.com/almighty/almighty-core/issues/723#issuecomment-278126385
    
    That leaves our root folder much cleaner.
    
    * Moved the controllers to the controllers pkg
    * Adapted the tests to the new package and modified the path to the config file
    * Use singular packages name and moved new controllers
    * Add the pwd to locate the config file
    * Added code to find config.yaml inside the controller tests
    * Moved iteration_whitebox_test to controller pkg

---
+++ controller/status_test.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"testing"
+
+	"time"
+
+	"github.com/almighty/almighty-core/app/test"
+	"github.com/almighty/almighty-core/resource"
+	"github.com/goadesign/goa"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShowStatusOK(t *testing.T) {
+	resource.Require(t, resource.Database)
+	controller := StatusController{db: DB}
+	_, res := test.ShowStatusOK(t, nil, nil, &controller)
+
+	if res.Commit != "0" {
+		t.Error("Commit not found")
+	}
+	if res.StartTime != StartTime {
+		t.Error("StartTime is not correct")
+	}
+	_, err := time.Parse("2006-01-02T15:04:05Z", res.StartTime)
+	if err != nil {
+		t.Error("Incorrect layout of StartTime: ", err.Error())
+	}
+}
+
+func TestNewStatusController(t *testing.T) {
+	t.Parallel()
+	resource.Require(t, resource.UnitTest)
+	svc := goa.New("TestNewStatusControllerService")
+	assert.NotNil(t, NewStatusController(svc, nil))
+}
