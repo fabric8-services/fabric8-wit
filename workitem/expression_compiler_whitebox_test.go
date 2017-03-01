1b6c6e4b3189084e280aca99de6ecb2e9f2b6cb1
Konrad Kleine <kkleine@redhat.com>
2017-03-01 15:58:59 +0100

    Added tests

--- workitem/expression_compiler_whitebox_test.go
+++ workitem/expression_compiler_whitebox_test.go
@@ -3,9 +3,7 @@ package workitem
 import (
 	"testing"
 
-	. "github.com/almighty/almighty-core/criteria"
 	"github.com/almighty/almighty-core/resource"
-	. "github.com/almighty/almighty-core/workitem"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,7 +11,7 @@ func TestUnderscore(t *testing.T) {
 	t.Parallel()
 	resource.Require(t, resource.UnitTest)
 
-	assert.Equal(t, unserscore("TypeID"), "type_id")
-	assert.Equal(t, unserscore("type_ID"), "type_id")
-	assert.Equal(t, unserscore("type_id"), "type_id")
+	assert.Equal(t, "type_id", underscore("TypeID"))
+	assert.Equal(t, "type_id", underscore("type_ID"))
+	assert.Equal(t, "type_id", underscore("type_id"))
 }
