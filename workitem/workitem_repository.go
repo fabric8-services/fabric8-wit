2ef6be1ec200f0bb063b7f098c0221a9512cbc3c
Dhriti Shikhar <dhrish20@gmail.com>
2017-03-08 23:43:29 +0530

    [1] Fixes compile errors

--- workitem/workitem_repository.go
+++ workitem/workitem_repository.go
@@ -114,7 +114,7 @@ func (r *GormWorkItemRepository) LoadTopWorkitem(ctx context.Context) (*app.Work
 	if err != nil {
 		return nil, errors.NewInternalError(err.Error())
 	}
-	return convertWorkItemModelToApp(wiType, &res)
+	return convertWorkItemModelToApp(goa.ContextRequest(ctx), wiType, &res)
 }
 
 // LoadBottomWorkitem returns bottom work item of the list. Bottom most workitem has the lowest order.
@@ -132,7 +132,7 @@ func (r *GormWorkItemRepository) LoadBottomWorkitem(ctx context.Context) (*app.W
 	if err != nil {
 		return nil, errors.NewInternalError(err.Error())
 	}
-	return convertWorkItemModelToApp(wiType, &res)
+	return convertWorkItemModelToApp(goa.ContextRequest(ctx), wiType, &res)
 }
 
 // LoadHighestOrder returns the highest order
@@ -357,7 +357,7 @@ func (r *GormWorkItemRepository) Reorder(ctx context.Context, direction Directio
 	if err != nil {
 		return nil, err
 	}
-	return convertWorkItemModelToApp(wiType, &res)
+	return convertWorkItemModelToApp(goa.ContextRequest(ctx), wiType, &res)
 }
 
 // Save updates the given work item in storage. Version must be the same as the one int the stored version
@@ -444,8 +444,6 @@ func (r *GormWorkItemRepository) Create(ctx context.Context, spaceID uuid.UUID, 
 	pos = pos + orderValue
 	fields[SystemOrder] = pos
 	wi := WorkItem{
-		Type:           typeID,
-		Fields:         Fields{},
 		Type:           typeID,
 		Fields:         Fields{},
 		ExecutionOrder: pos,
