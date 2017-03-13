983626b572b9a4708652aa8f3792798d8be09d8a
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-03-13 09:07:44 +0530

    update tests to common service

--- controller/work-item-children-blackbox_test.go
+++ controller/work-item-children-blackbox_test.go
@@ -1,6 +1,7 @@
 package controller_test
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,7 +12,6 @@ import (
 	"github.com/almighty/almighty-core/account"
 	"github.com/almighty/almighty-core/app"
 	"github.com/almighty/almighty-core/app/test"
-	"github.com/almighty/almighty-core/application"
 	. "github.com/almighty/almighty-core/controller"
 	"github.com/almighty/almighty-core/gormapplication"
 	"github.com/almighty/almighty-core/gormsupport/cleaner"
@@ -19,7 +19,6 @@ import (
 	"github.com/almighty/almighty-core/migration"
 	"github.com/almighty/almighty-core/models"
 	"github.com/almighty/almighty-core/resource"
-	"github.com/almighty/almighty-core/space"
 	testsupport "github.com/almighty/almighty-core/test"
 	almtoken "github.com/almighty/almighty-core/token"
 	"github.com/almighty/almighty-core/workitem"
@@ -45,7 +44,7 @@ type workItemChildSuite struct {
 	workItemCtrl             *WorkitemController
 	workItemRelsLinksCtrl    *WorkItemRelationshipsLinksController
 	spaceCtrl                *SpaceController
-	workItemSvc              *goa.Service
+	svc                      *goa.Service
 	typeCtrl                 *WorkitemtypeController
 	// These IDs can safely be used by all tests
 	bug1ID               uint64
@@ -69,7 +68,7 @@ func (s *workItemChildSuite) SetupSuite() {
 	s.clean = cleaner.DeleteCreatedEntities(s.DB)
 
 	// Make sure the database is populated with the correct types (e.g. bug etc.)
-	if err := models.Transactional(DB, func(tx *gorm.DB) error {
+	if err := models.Transactional(s.DB, func(tx *gorm.DB) error {
 		return migration.PopulateCommonTypes(context.Background(), tx, workitem.NewWorkItemTypeRepository(tx))
 	}); err != nil {
 		panic(err.Error())
@@ -82,60 +81,57 @@ func (s *workItemChildSuite) SetupSuite() {
 	priv, err := almtoken.ParsePrivateKey([]byte(almtoken.RSAPrivateKey))
 	require.Nil(s.T(), err)
 
-	svc := testsupport.ServiceAsUser("WorkItemLink-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc := testsupport.ServiceAsUser("WorkItemLink-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemLinkCtrl = NewWorkItemLinkController(svc, s.db)
 	require.NotNil(s.T(), s.workItemLinkCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemChildren-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemChildren-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemChildrenCtrl = NewWorkItemChildrenController(svc, s.db)
 	require.NotNil(s.T(), s.workItemChildrenCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemLinkType-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemLinkType-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemLinkTypeCtrl = NewWorkItemLinkTypeController(svc, s.db)
 	require.NotNil(s.T(), s.workItemLinkTypeCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemLinkCategory-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemLinkCategory-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemLinkCategoryCtrl = NewWorkItemLinkCategoryController(svc, s.db)
 	require.NotNil(s.T(), s.workItemLinkCategoryCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemType-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemType-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.typeCtrl = NewWorkitemtypeController(svc, s.db)
 	require.NotNil(s.T(), s.typeCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemLink-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemLink-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemLinkCtrl = NewWorkItemLinkController(svc, s.db)
 	require.NotNil(s.T(), s.workItemLinkCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemRelationshipsLinks-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemRelationshipsLinks-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemRelsLinksCtrl = NewWorkItemRelationshipsLinksController(svc, s.db)
 	require.NotNil(s.T(), s.workItemRelsLinksCtrl)
 
-	svc = testsupport.ServiceAsUser("WorkItemChildren-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("WorkItemChildren-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), svc)
 	s.workItemChildrenCtrl = NewWorkItemChildrenController(svc, s.db)
 	require.NotNil(s.T(), s.workItemChildrenCtrl)
 
-	svc = testsupport.ServiceAsUser("TestWorkItem-Service", almtoken.NewManagerWithPrivateKey(priv), testsupport.TestIdentity)
+	svc = testsupport.ServiceAsUser("TestWorkItem-Service", almtoken.NewManagerWithPrivateKey(priv), testIdentity)
 	require.NotNil(s.T(), svc)
-	s.workItemSvc = svc
+	s.svc = svc
 	s.workItemCtrl = NewWorkitemController(svc, s.db)
 	require.NotNil(s.T(), s.workItemCtrl)
-}
 
-func (s *workItemChildSuite) securedControllers(identity account.Identity) (*goa.Service, *WorkitemController, *WorkItemCommentsController, *CommentsController) {
-	priv, _ := almtoken.ParsePrivateKey([]byte(almtoken.RSAPrivateKey))
-	svc := testsupport.ServiceAsUser("Comment-Service", almtoken.NewManagerWithPrivateKey(priv), identity)
-	workitemCtrl := NewWorkitemController(svc, s.db)
-	workitemCommentsCtrl := NewWorkItemCommentsController(svc, s.db)
-	commentsCtrl := NewCommentsController(svc, s.db)
-	return svc, workitemCtrl, workitemCommentsCtrl, commentsCtrl
+	svc = testsupport.ServiceAsUser("Space-Service", almtoken.NewManagerWithPrivateKey(priv), testIdentity)
+	require.NotNil(s.T(), svc)
+	s.spaceCtrl = NewSpaceController(svc, s.db)
+	require.NotNil(s.T(), s.spaceCtrl)
+
 }
 
 // The SetupTest method will be run before every test in the suite.
@@ -144,23 +140,15 @@ func (s *workItemChildSuite) securedControllers(identity account.Identity) (*goa
 func (s *workItemChildSuite) SetupTest() {
 	var err error
 
-	application.Transactional(s.db, func(app application.Application) error {
-		newSpace := &space.Space{
-			Name: "Test Space 1" + uuid.NewV4().String(),
-		}
-		p, err := app.Spaces().Create(context.Background(), newSpace)
-		if err != nil {
-			s.T().Error(err)
-			return err
-		}
-		s.userSpaceID = p.ID
-		return nil
-	})
+	// Create a work item link space
+	createSpacePayload := CreateSpacePayload("test-space"+uuid.NewV4().String(), "description")
+	_, space := test.CreateSpaceCreated(s.T(), s.svc.Context, s.svc, s.spaceCtrl, createSpacePayload)
+	s.userSpaceID = *space.Data.ID
+	s.T().Logf("Created link space with ID: %s\n", *space.Data.ID)
 
 	// Create 3 work items (bug1, bug2, and feature1)
 	bug1Payload := CreateWorkItem(s.userSpaceID, workitem.SystemBug, "bug1")
-	userSvc, workitemCtrl, _, _ := s.securedControllers(s.testIdentity)
-	_, bug1 := test.CreateWorkitemCreated(s.T(), userSvc.Context, userSvc, workitemCtrl, bug1Payload)
+	_, bug1 := test.CreateWorkitemCreated(s.T(), s.svc.Context, s.svc, s.workItemCtrl, bug1Payload)
 	require.NotNil(s.T(), bug1)
 
 	s.bug1ID, err = strconv.ParseUint(*bug1.Data.ID, 10, 64)
@@ -168,7 +156,7 @@ func (s *workItemChildSuite) SetupTest() {
 	s.T().Logf("Created bug1 with ID: %s\n", *bug1.Data.ID)
 
 	bug2Payload := CreateWorkItem(s.userSpaceID, workitem.SystemBug, "bug2")
-	_, bug2 := test.CreateWorkitemCreated(s.T(), userSvc.Context, userSvc, workitemCtrl, bug2Payload)
+	_, bug2 := test.CreateWorkitemCreated(s.T(), s.svc.Context, s.svc, s.workItemCtrl, bug2Payload)
 	require.NotNil(s.T(), bug2)
 
 	s.bug2ID, err = strconv.ParseUint(*bug2.Data.ID, 10, 64)
@@ -176,7 +164,7 @@ func (s *workItemChildSuite) SetupTest() {
 	s.T().Logf("Created bug2 with ID: %s\n", *bug2.Data.ID)
 
 	bug3Payload := CreateWorkItem(s.userSpaceID, workitem.SystemBug, "bug3")
-	_, bug3 := test.CreateWorkitemCreated(s.T(), userSvc.Context, userSvc, workitemCtrl, bug3Payload)
+	_, bug3 := test.CreateWorkitemCreated(s.T(), s.svc.Context, s.svc, s.workItemCtrl, bug3Payload)
 	require.NotNil(s.T(), bug3)
 
 	s.bug3ID, err = strconv.ParseUint(*bug3.Data.ID, 10, 64)
@@ -185,14 +173,14 @@ func (s *workItemChildSuite) SetupTest() {
 
 	// Create a work item link category
 	createLinkCategoryPayload := CreateWorkItemLinkCategory("test-user" + uuid.NewV4().String())
-	_, workItemLinkCategory := test.CreateWorkItemLinkCategoryCreated(s.T(), userSvc.Context, userSvc, s.workItemLinkCategoryCtrl, createLinkCategoryPayload)
+	_, workItemLinkCategory := test.CreateWorkItemLinkCategoryCreated(s.T(), s.svc.Context, s.svc, s.workItemLinkCategoryCtrl, createLinkCategoryPayload)
 	require.NotNil(s.T(), workItemLinkCategory)
 	s.userLinkCategoryID = *workItemLinkCategory.Data.ID
 	s.T().Logf("Created link category with ID: %s\n", *workItemLinkCategory.Data.ID)
 
 	// Create work item link type payload
 	createLinkTypePayload := createParentChildWorkItemLinkType("test-bug-blocker", workitem.SystemBug, workitem.SystemBug, s.userLinkCategoryID, s.userSpaceID)
-	_, workItemLinkType := test.CreateWorkItemLinkTypeCreated(s.T(), userSvc.Context, userSvc, s.workItemLinkTypeCtrl, createLinkTypePayload)
+	_, workItemLinkType := test.CreateWorkItemLinkTypeCreated(s.T(), s.svc.Context, s.svc, s.workItemLinkTypeCtrl, createLinkTypePayload)
 	require.NotNil(s.T(), workItemLinkType)
 	s.bugBlockerLinkTypeID = *workItemLinkType.Data.ID
 	s.T().Logf("Created link type with ID: %s\n", *workItemLinkType.Data.ID)
@@ -247,17 +235,17 @@ func TestSuiteWorkItemChildren(t *testing.T) {
 }
 
 func (s *workItemChildSuite) TestListChildren() {
-	resource.Require(s.T(), resource.Database)
 	createPayload := CreateWorkItemLink(s.bug1ID, s.bug2ID, s.bugBlockerLinkTypeID)
-	_, workItemLink := test.CreateWorkItemLinkCreated(s.T(), nil, nil, s.workItemLinkCtrl, createPayload)
+	_, workItemLink := test.CreateWorkItemLinkCreated(s.T(), s.svc.Context, s.svc, s.workItemLinkCtrl, createPayload)
 	require.NotNil(s.T(), workItemLink)
 
 	createPayload2 := CreateWorkItemLink(s.bug1ID, s.bug3ID, s.bugBlockerLinkTypeID)
-	_, workItemLink2 := test.CreateWorkItemLinkCreated(s.T(), nil, nil, s.workItemLinkCtrl, createPayload2)
+	_, workItemLink2 := test.CreateWorkItemLinkCreated(s.T(), s.svc.Context, s.svc, s.workItemLinkCtrl, createPayload2)
 	require.NotNil(s.T(), workItemLink2)
 
 	workItemID1 := strconv.FormatUint(s.bug1ID, 10)
-	_, workItemList := test.ListWorkItemChildrenOK(s.T(), nil, nil, s.workItemChildrenCtrl, workItemID1)
+	_, workItemList := test.ListWorkItemChildrenOK(s.T(), s.svc.Context, s.svc, s.workItemChildrenCtrl, workItemID1)
+	fmt.Printf("\nworkItemList.Data: %#v\n", workItemList.Data)
 	assert.Equal(s.T(), 2, len(workItemList.Data))
 	var count int
 	for _, v := range workItemList.Data {
