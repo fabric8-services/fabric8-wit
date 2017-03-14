11dcfd54adba24c6c45e7e2ce41b9fa4cdd60823
Dhriti Shikhar <dhrish20@gmail.com>
2017-03-14 17:41:37 +0530

    * Removes space_id from categories

--- migration/sql-files/043-categories.sql
+++ migration/sql-files/043-categories.sql
@@ -3,7 +3,6 @@ CREATE TABLE categories (
 	updated_at timestamp with time zone,
 	deleted_at timestamp with time zone,
 	id uuid primary key DEFAULT uuid_generate_v4() NOT NULL,
-	space_id uuid,
 	name text
 );
 
