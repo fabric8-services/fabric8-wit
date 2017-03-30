9620981ba284f6f91f282ee3d4bc80cbad2ed6c9
Alexey Kazakov <alkazako@redhat.com>
2017-03-30 08:37:36 -0700

    Fix https scheme for sso requests (#1081)

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -476,7 +476,7 @@ func (c *ConfigurationData) openIDConnectPath(suffix string) string {
 
 func (c *ConfigurationData) getKeycloakURL(req *goa.RequestData, path string) (string, error) {
 	scheme := "http"
-	if req.TLS != nil { // isHTTPS
+	if req.URL != nil && req.URL.Scheme == "https" { // isHTTPS
 		scheme = "https"
 	}
 	newHost, err := rest.ReplaceDomainPrefix(req.Host, c.GetKeycloakDomainPrefix())
@@ -558,7 +558,7 @@ ZwIDAQAB
 	defaultKeycloakTesUser2Secret = "testuser2"
 
 	// Keycloak vars to be used in dev mode. Can be overridden by setting up keycloak.url & keycloak.realm
-	devModeKeycloakURL   = "http://sso.prod-preview.openshift.io"
+	devModeKeycloakURL   = "https://sso.prod-preview.openshift.io"
 	devModeKeycloakRealm = "fabric8-test"
 
 	defaultOpenshiftTenantMasterURL = "https://tsrv.devshift.net:8443"
