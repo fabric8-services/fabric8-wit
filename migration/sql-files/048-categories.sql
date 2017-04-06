3a8a72d74da97b0873804474cf0022d3cbb9c3ae
Dhriti Shikhar <dhrish20@gmail.com>
2017-04-06 11:15:59 +0530

    * Creates index for categories table * Adds NOT NULL constraint to category name in categories table and category_id and workitemtype_id in workitemtype_categories table * Adds foreign key to category_id

--- migration/sql-files/048-categories.sql
+++ migration/sql-files/048-categories.sql
@@ -3,14 +3,17 @@ CREATE TABLE categories (
 	updated_at timestamp with time zone,
 	deleted_at timestamp with time zone,
 	id uuid primary key DEFAULT uuid_generate_v4() NOT NULL,
-	name text
+	name text NOT NULL
 );
+
+CREATE INDEX categories_id_index ON categories (id);
 
 CREATE TABLE workitemtype_categories (
 	created_at timestamp with time zone,
 	updated_at timestamp with time zone,
 	deleted_at timestamp with time zone,
-	category_id uuid,
-	workitemtype_id uuid
+	category_id uuid NOT NULL,
+	workitemtype_id uuid NOT NULL
 );
 
+ALTER TABLE workitemtype_categories ADD CONSTRAINT category_id_categories_id_fk FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE;
