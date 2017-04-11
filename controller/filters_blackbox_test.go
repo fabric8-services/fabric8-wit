ac86a7202af4d8509fc14bc61f7e9c38f81d2da3
Dhriti Shikhar <dhrish20@gmail.com>
2017-04-11 13:03:27 +0530

    Fixes test-cases related to WorkItemTypes

--- controller/filters_blackbox_test.go
+++ controller/filters_blackbox_test.go
@@ -37,6 +37,6 @@ func (rest *TestFiltersREST) TestListFiltersOK() {
 	// when
 	res, filters := test.ListFilterOK(rest.T(), svc.Context, svc, ctrl, nil)
 	// then
-	assert.Equal(rest.T(), 5, len(filters.Data))
+	assert.Equal(rest.T(), 6, len(filters.Data))
 	assertResponseHeaders(rest.T(), res)
 }
