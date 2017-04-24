eea7ae6fef29f684c9c69ea60b3436800078e7df
Xavier Coulon <xcoulon@redhat.com>
2017-04-24 12:29:33 +0200

    fix migration tests
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- migration/sql-test-files/055-assign-root-area-if-missing.sql
+++ migration/sql-test-files/055-assign-root-area-if-missing.sql
@@ -1,3 +1,4 @@
-insert into spaces (id, name) values ('00000000-1111-2222-3333-000000000000', 'test');
-insert into areas (id, name, space_id) values ('00000000-1111-2222-3333-444444444444', 'test area', '00000000-1111-2222-3333-000000000000');
-insert into work_items (id, space_id, type, fields) values (12345, '00000000-1111-2222-3333-000000000000', '26787039-b68f-4e28-8814-c2f93be1ef4e', '{"system.title":"Title"}'::json);
+insert into spaces (id, name) values ('11111111-2222-0000-0000-000000000000', 'test');
+insert into areas (id, name, path, space_id) values ('11111111-3333-0000-0000-000000000000', 'test area', '', '11111111-2222-0000-0000-000000000000');
+insert into work_item_types (id, name, space_id) values ('11111111-4444-0000-0000-000000000000', 'Test WIT','11111111-2222-0000-0000-000000000000');
+insert into work_items (id, space_id, type, fields) values (12345, '11111111-2222-0000-0000-000000000000', '11111111-4444-0000-0000-000000000000', '{"system.title":"Title"}'::json);
--- migration/sql-test-files/056-assign-root-iteration-if-missing.sql
+++ migration/sql-test-files/056-assign-root-iteration-if-missing.sql
@@ -1,3 +1,4 @@
-insert into spaces (id, name) values ('00000000-1111-2222-3333-000000000000', 'test');
-insert into iterations (id, name, space_id) values ('00000000-1111-2222-3333-444444444444', 'test area', '00000000-1111-2222-3333-000000000000');
-insert into work_items (id, space_id, type, fields) values (12346, '00000000-1111-2222-3333-000000000000', '26787039-b68f-4e28-8814-c2f93be1ef4e', '{"system.title":"Title"}'::json);
+insert into spaces (id, name) values ('11111111-2222-bbbb-0000-000000000000', 'test');
+insert into iterations (id, name, path, space_id) values ('11111111-3333-bbbb-0000-000000000000', 'test area', '', '11111111-2222-bbbb-0000-000000000000');
+insert into work_item_types (id, name, space_id) values ('11111111-4444-bbbb-0000-000000000000', 'Test WIT','11111111-2222-bbbb-0000-000000000000');
+insert into work_items (id, space_id, type, fields) values (12346, '11111111-2222-bbbb-0000-000000000000', '11111111-4444-bbbb-0000-000000000000', '{"system.title":"Title"}'::json);
