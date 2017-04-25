1fc56d52cb47c822bfe8ef047444b0706e20374a
Shoubhik Bose <sbose78@gmail.com>
2017-04-25 18:07:08 +0530

    remove duplicated paging code

--- controller/search.go
+++ controller/search.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/almighty/almighty-core/account"
 	"github.com/almighty/almighty-core/app"
@@ -147,7 +146,7 @@ func (c *SearchController) Users(ctx *app.UsersSearchContext) error {
 	var count int
 	var err error
 
-	offset, limit := paginationOptions(ctx.PageOffset, ctx.PageLimit)
+	offset, limit := computePagingLimts(ctx.PageOffset, ctx.PageLimit)
 
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		result, count, err = appl.Identities().Search(ctx, q, offset, limit)
@@ -183,112 +182,12 @@ func (c *SearchController) Users(ctx *app.UsersSearchContext) error {
 		})
 	}
 
-	pagingLinks := paginationLinks(
-		buildAbsoluteURL(ctx.RequestData),
-		"q="+ctx.Q,
-		offset,
-		limit,
-		count,
-		len(result))
-
 	response := app.SearchResponseUsers{
 		Data:  users,
-		Links: &pagingLinks,
+		Links: &app.PagingLinks{},
 		Meta:  map[string]interface{}{"total-count": count},
 	}
+	setPagingLinks(response.Links, buildAbsoluteURL(ctx.RequestData), len(result), offset, limit, count, "q="+q)
 
 	return ctx.OK(&response)
-}
-
-func paginationLinks(path, args string, offset, limit, count, length int) app.PagingLinks {
-	links := app.PagingLinks{}
-
-	// prev link
-	if offset > 0 && count > 0 {
-		var prevStart int
-		// we do have a prev link
-		if offset <= count {
-			prevStart = offset - limit
-		} else {
-			// the first range that intersects the end of the useful range
-			prevStart = offset - (((offset-count)/limit)+1)*limit
-		}
-		realLimit := limit
-		if prevStart < 0 {
-			// need to cut the range to start at 0
-			realLimit = limit + prevStart
-			prevStart = 0
-		}
-		prev := fmt.Sprintf("%s?%s&page[offset]=%d&page[limit]=%d", path, args, prevStart, realLimit)
-		links.Prev = &prev
-	}
-
-	// next link
-	nextStart := offset + length
-	if nextStart < count {
-		// we have a next link
-		next := fmt.Sprintf("%s?%s&page[offset]=%d&page[limit]=%d", path, args, nextStart, limit)
-		links.Next = &next
-	}
-
-	// first link
-	var firstEnd int
-	if offset > 0 {
-		firstEnd = offset % limit // this is where the second page starts
-	} else {
-		// offset == 0, first == current
-		firstEnd = limit
-	}
-	first := fmt.Sprintf("%s?%s&page[offset]=%d&page[limit]=%d", path, args, 0, firstEnd)
-	links.First = &first
-
-	// last link
-	var lastStart int
-	if offset < count {
-		// advance some pages until touching the end of the range
-		lastStart = offset + (((count - offset - 1) / limit) * limit)
-	} else {
-		// retreat at least one page until covering the range
-		lastStart = offset - ((((offset - count) / limit) + 1) * limit)
-	}
-	realLimit := limit
-	if lastStart < 0 {
-		// need to cut the range to start at 0
-		realLimit = limit + lastStart
-		lastStart = 0
-	}
-	last := fmt.Sprintf("%s?%s&page[offset]=%d&page[limit]=%d", path, args, lastStart, realLimit)
-	links.Last = &last
-
-	return links
-}
-
-// paginationOptions parses and defaults offset/limit numbers
-func paginationOptions(pageOffset *string, pageLimit *int) (int, int) {
-	var offset int
-	var limit int
-
-	if pageOffset == nil {
-		offset = 0
-	} else {
-		offsetValue, err := strconv.Atoi(*pageOffset)
-		if err != nil {
-			offset = 0
-		} else {
-			offset = offsetValue
-		}
-	}
-
-	if pageLimit == nil {
-		limit = 100
-	} else {
-		limit = *pageLimit
-	}
-	if offset < 0 {
-		offset = 0
-	}
-	if limit > 100 {
-		limit = 100
-	}
-	return offset, limit
 }
