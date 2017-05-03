d26c6bdcbb0013b05f2f6d6e8e65ad3ef79ac0f9
Hector Fernandez <hectorj@gmail.com>
2017-05-03 18:51:15 +0200

    Use our own log request function to add identity id and convert to ms the time unit

--- main.go
+++ main.go
@@ -147,7 +147,7 @@ func main() {
 
 	// Mount middleware
 	service.Use(middleware.RequestID())
-	service.Use(middleware.LogRequest(configuration.IsPostgresDeveloperModeEnabled()))
+	service.Use(log.LogRequest(configuration.IsPostgresDeveloperModeEnabled()))
 	service.Use(gzip.Middleware(9))
 	service.Use(jsonapi.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
