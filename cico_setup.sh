64bcea82304e221ab2dbc02e5f8e5b269fc001fc
Konrad Kleine <kwk@users.noreply.github.com>
2017-05-26 11:58:47 +0200

    Avoid manual sed on env vars
    Once https://github.com/almighty/almighty-jobs/pull/186 is approved and merged, this change should work.

--- cico_setup.sh
+++ cico_setup.sh
@@ -12,7 +12,6 @@ function load_jenkins_vars() {
   if [ -e "jenkins-env" ]; then
     cat jenkins-env \
       | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
-      | sed 's/^/export /g' \
       > ~/.jenkins-env
     source ~/.jenkins-env
   fi
