e2aa60d1ca58f8582520c633a94702418112cb62
Dhriti Shikhar <dhrish20@gmail.com>
2017-06-12 11:02:07 +0530

    Removes category id Foreign Key

--- migration/sql-files/062-categories.sql
+++ migration/sql-files/062-categories.sql
@@ -21,4 +21,3 @@ CREATE TABLE work_item_type_categories (
 CREATE INDEX work_item_type_categories_id_idx ON work_item_type_categories (id);
 CREATE UNIQUE INDEX work_item_type_categories_idx ON work_item_type_categories (category_id, work_item_type_id) WHERE deleted_at IS NULL;
 ALTER TABLE work_item_type_categories ADD CONSTRAINT work_item_type_id_work_item_types_id_fk FOREIGN KEY (work_item_type_id) REFERENCES work_item_types (id) ON DELETE CASCADE;
-ALTER TABLE work_item_type_categories ADD CONSTRAINT category_id_categories_id_fk FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE;
