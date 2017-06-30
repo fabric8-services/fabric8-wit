68889f3cb8037a49402f196cef50aab5af439a88
Hector Fernandez <hfernand@redhat.com>
2017-06-30 14:36:11 +0200

    Changed almighty prefixes of the env vars

--- .make/test.mk
+++ .make/test.mk
@@ -162,7 +162,7 @@ test-integration-no-coverage: prebuild-check migrate-database $(SOURCES)
 test-integration-benchmark: prebuild-check migrate-database $(SOURCES)
 	$(call log-info,"Running benchmarks: $@")
 	$(eval TEST_PACKAGES:=$(shell go list ./... | grep -v $(ALL_PKGS_EXCLUDE_PATTERN)))
-	ALMIGHTY_DEVELOPER_MODE_ENABLED=1 ALMIGHTY_LOG_LEVEL=error ALMIGHTY_RESOURCE_DATABASE=1 ALMIGHTY_RESOURCE_UNIT_TEST=0 go test -run=^$$ -bench=. -cpu 1,2,4 -test.benchmem $(GO_TEST_VERBOSITY_FLAG) $(TEST_PACKAGES)
+	F8_DEVELOPER_MODE_ENABLED=1 F8_LOG_LEVEL=error F8_RESOURCE_DATABASE=1 F8_RESOURCE_UNIT_TEST=0 go test -run=^$$ -bench=. -cpu 1,2,4 -test.benchmem $(GO_TEST_VERBOSITY_FLAG) $(TEST_PACKAGES)
 
 .PHONY: test-remote
 ## Runs the remote tests and produces coverage files for each package.
