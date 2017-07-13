bd39d046dae7f17bc500373c6bf2174ed41c11cd
Xavier Coulon <xcoulon@redhat.com>
2017-07-13 13:33:20 +0200

    Remove FK contraint for now
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- migration/sql-files/068-spaces_identities_fk.sql
+++ migration/sql-files/068-spaces_identities_fk.sql
@@ -1,5 +1,3 @@
--- add a foreign key from spaces.owner_id to identities.id
-alter table spaces add constraint spaces_owner_id_identities_id_fk foreign key (owner_id) REFERENCES identities (id);
 -- add index on spaces.name
 create index idx_spaces_name on spaces (lower(name));
 -- add index on identities.username
