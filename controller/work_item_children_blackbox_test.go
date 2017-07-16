0647db716e7d6997992969e4f67fb82419e6dc3e
Aslak Knutsen <aslak@4fs.no>
2017-07-16 15:31:06 +0200

    Stabilize testsuite (#1503)
    * Clean up test data
    * Reuse DBSuite from parent suite to reuse DB clean
      Avoids calling setupTest/tearDownTest
    
    Fixes #1499

--- controller/work_item_children_blackbox_test.go
+++ controller/work_item_children_blackbox_test.go
@@ -63,6 +63,18 @@ func (s *workItemChildSuite) SetupSuite() {
 	s.DBTestSuite.PopulateDBTestSuite(ctx)
 
 	s.db = gormapplication.NewGormDB(s.DB)
+}
+
+const (
+	hasChildren   bool = true
+	hasNoChildren bool = false
+)
+
+// The SetupTest method will be run before every test in the suite.
+// SetupTest ensures that none of the work item links that we will create already exist.
+// It will also make sure that some resources that we rely on do exists.
+func (s *workItemChildSuite) SetupTest() {
+	s.clean = cleaner.DeleteCreatedEntities(s.DB)
 
 	testIdentity, err := testsupport.CreateTestIdentity(s.DB, "workItemChildSuite user", "test provider")
 	require.Nil(s.T(), err)
@@ -112,26 +124,8 @@ func (s *workItemChildSuite) SetupSuite() {
 	s.spaceCtrl = NewSpaceController(svc, s.db, s.Configuration, &DummyResourceManager{})
 	require.NotNil(s.T(), s.spaceCtrl)
 
-}
-
-const (
-	hasChildren   bool = true
-	hasNoChildren bool = false
-)
-
-// The SetupTest method will be run before every test in the suite.
-// SetupTest ensures that none of the work item links that we will create already exist.
-// It will also make sure that some resources that we rely on do exists.
-func (s *workItemChildSuite) SetupTest() {
-	s.clean = cleaner.DeleteCreatedEntities(s.DB)
-	var err error
-
 	// Create a test user identity
-	priv, err := almtoken.ParsePrivateKey([]byte(almtoken.RSAPrivateKey))
-	require.Nil(s.T(), err)
-	testIdentity, err := testsupport.CreateTestIdentity(s.DB, "test user", "test provider")
-	require.Nil(s.T(), err)
-	s.svc = testsupport.ServiceAsUser("TestWorkItem-Service", almtoken.NewManagerWithPrivateKey(priv), *testIdentity)
+	s.svc = testsupport.ServiceAsUser("TestWorkItem-Service", almtoken.NewManagerWithPrivateKey(priv), s.testIdentity)
 	require.NotNil(s.T(), s.svc)
 
 	// Create a work item link space
