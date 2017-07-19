b06f0716cc495fdee0ec5aece6e7daa7cd610a22
DhritiShikhar <dshikhar@redhat.com>
2017-07-19 16:31:47 +0530

    Adds comments in migration script

--- migration/sql-files/069-limit-execution-order-to-space.sql
+++ migration/sql-files/069-limit-execution-order-to-space.sql
@@ -1,5 +1,7 @@
+-- limit_execution_order_to_space() function limits execution order of workitems to space
+-- Fetch space from spaces. For workitems which belong to that space, set their execution order sequentially -> 1000, 2000, 3000 and so on.
+-- Fetch next space from spaces, for each workitem which belongs to that space, again set their execution_order sequentially -> 1000, 2000, 3000...
 CREATE OR REPLACE FUNCTION limit_execution_order_to_space() RETURNS void as $$
--- limit_execution_order_to_space() function limits order to space
 	DECLARE
 		i integer;
 		r RECORD;
@@ -8,12 +10,13 @@ CREATE OR REPLACE FUNCTION limit_execution_order_to_space() RETURNS void as $$
 		spaces_cursor CURSOR FOR SELECT id from spaces;
 	BEGIN
 		open spaces_cursor;
-			FOR a in FETCH ALL FROM spaces_cursor
+			FOR a in FETCH ALL FROM spaces_cursor -- fetch space from spaces
 				LOOP
 					i:=1000;
 					open workitems_cursor;
-						FOR r in FETCH ALL FROM workitems_cursor
+						FOR r in FETCH ALL FROM workitems_cursor -- fetch workitem from work_items
 							LOOP
+								-- for workitems belonging to that space, set their execution order sequentially 1000, 2000, 3000
 								UPDATE work_items set execution_order=i where id=r.id AND space_id=a.id;
 								i = i+1000;
 							END LOOP;
