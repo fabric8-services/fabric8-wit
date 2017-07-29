446a02038b32224d28f69f3463b9d51221199c7a
Hector Fernandez <hfernand@redhat.com>
2017-07-29 19:29:27 +0200

    Added code to extract the token context before the goa log request middleware

---
+++ goamiddleware/jwt_token_context.go
@@ -0,0 +1,150 @@
+package goamiddleware
+
+import (
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"context"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fabric8-services/fabric8-wit/log"
+	"github.com/goadesign/goa"
+	goajwt "github.com/goadesign/goa/middleware/security/jwt"
+)
+
+// TokenContext is a new goa middleware that aims to extract the token from the
+// Authorization header when possible. If the Authorization header is missing in the request,
+// no error is returned. However, if the Authorization header contains a
+// token, it is validated and added to the context. In case the token is invalid
+// then this middleware will throw warning logs.
+func TokenContext(validationKeys interface{}, validationFunc goa.Middleware, scheme *goa.JWTSecurity) goa.Middleware {
+	var rsaKeys []*rsa.PublicKey
+	var hmacKeys [][]byte
+
+	rsaKeys, ecdsaKeys, hmacKeys := partitionKeys(validationKeys)
+
+	return func(nextHandler goa.Handler) goa.Handler {
+		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			// TODO: implement the QUERY string handler too
+			if scheme.In != goa.LocHeader {
+				log.Error(ctx, nil, fmt.Sprintf("whoops, security scheme with location (in) %q not supported", scheme.In))
+				return fmt.Errorf("whoops, security scheme with location (in) %q not supported", scheme.In)
+			}
+			val := req.Header.Get(scheme.Name)
+			if val != "" && strings.HasPrefix(strings.ToLower(val), "bearer ") {
+				log.Debug(ctx, nil, "found header 'Authorization: Bearer JWT-token...'")
+				incomingToken := strings.Split(val, " ")[1]
+				log.Debug(ctx, nil, "extracted the incoming token %v ", incomingToken)
+
+				var (
+					token     *jwt.Token
+					err       error
+					validated = false
+				)
+
+				if len(rsaKeys) > 0 {
+					token, err = validateRSAKeys(rsaKeys, "RS", incomingToken)
+					validated = err == nil
+				}
+
+				if !validated && len(ecdsaKeys) > 0 {
+					token, err = validateECDSAKeys(ecdsaKeys, "ES", incomingToken)
+					validated = err == nil
+				}
+
+				if !validated && len(hmacKeys) > 0 {
+					token, err = validateHMACKeys(hmacKeys, "HS", incomingToken)
+				}
+
+				if err != nil {
+					log.Warn(ctx, nil, "JWT validation failed %v", err)
+					return nil
+				}
+
+				ctx = goajwt.WithJWT(ctx, token)
+			}
+
+			return nextHandler(ctx, rw, req)
+		}
+	}
+}
+
+// partitionKeys sorts keys by their type.
+func partitionKeys(k interface{}) ([]*rsa.PublicKey, []*ecdsa.PublicKey, [][]byte) {
+	var (
+		rsaKeys   []*rsa.PublicKey
+		ecdsaKeys []*ecdsa.PublicKey
+		hmacKeys  [][]byte
+	)
+
+	switch typed := k.(type) {
+	case []byte:
+		hmacKeys = append(hmacKeys, typed)
+	case [][]byte:
+		hmacKeys = typed
+	case string:
+		hmacKeys = append(hmacKeys, []byte(typed))
+	case []string:
+		for _, s := range typed {
+			hmacKeys = append(hmacKeys, []byte(s))
+		}
+	case *rsa.PublicKey:
+		rsaKeys = append(rsaKeys, typed)
+	case []*rsa.PublicKey:
+		rsaKeys = typed
+	case *ecdsa.PublicKey:
+		ecdsaKeys = append(ecdsaKeys, typed)
+	case []*ecdsa.PublicKey:
+		ecdsaKeys = typed
+	}
+
+	return rsaKeys, ecdsaKeys, hmacKeys
+}
+
+func validateRSAKeys(rsaKeys []*rsa.PublicKey, algo, incomingToken string) (token *jwt.Token, err error) {
+	for _, pubkey := range rsaKeys {
+		token, err = jwt.Parse(incomingToken, func(token *jwt.Token) (interface{}, error) {
+			if !strings.HasPrefix(token.Method.Alg(), algo) {
+				return nil, goajwt.ErrJWTError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			}
+			return pubkey, nil
+		})
+		if err == nil {
+			return
+		}
+	}
+	return
+}
+
+func validateECDSAKeys(ecdsaKeys []*ecdsa.PublicKey, algo, incomingToken string) (token *jwt.Token, err error) {
+	for _, pubkey := range ecdsaKeys {
+		token, err = jwt.Parse(incomingToken, func(token *jwt.Token) (interface{}, error) {
+			if !strings.HasPrefix(token.Method.Alg(), algo) {
+				return nil, goajwt.ErrJWTError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			}
+			return pubkey, nil
+		})
+		if err == nil {
+			return
+		}
+	}
+	return
+}
+
+func validateHMACKeys(hmacKeys [][]byte, algo, incomingToken string) (token *jwt.Token, err error) {
+	for _, key := range hmacKeys {
+		token, err = jwt.Parse(incomingToken, func(token *jwt.Token) (interface{}, error) {
+			if !strings.HasPrefix(token.Method.Alg(), algo) {
+				return nil, goajwt.ErrJWTError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			}
+			return key, nil
+		})
+		if err == nil {
+			return
+		}
+	}
+	return
+}
