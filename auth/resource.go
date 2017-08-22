fc82b308373355e5972682755d7848e9c60bae89
Alexey Kazakov <alkazako@redhat.com>
2017-08-21 17:35:57 -0700

    fix import

--- auth/resource.go
+++ auth/resource.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/almighty/almighty-core/rest"
 	"github.com/fabric8-services/fabric8-wit/errors"
 	"github.com/fabric8-services/fabric8-wit/log"
+	"github.com/fabric8-services/fabric8-wit/rest"
 	"github.com/goadesign/goa"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	errs "github.com/pkg/errors"
