83ed2743c4ea73085f7d27aafec0c149947a8a0a
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-09-06 17:17:04 +0530

    improve test cases

--- migration/sql-test-files/073-label-empty-name.sql
+++ migration/sql-test-files/073-label-empty-name.sql
@@ -1,10 +1,6 @@
 -- empty label name
 -- this should fail
-INSERT INTO
-	labels(created_at, updated_at, id, color, space_id)
-VALUES
-   (
-	now(), now(), 'c0c3b254-c780-4a21-b00d-241a29e6be51', '#000000', '5c12842c-61ce-4481-b33d-163d09a732c4'
-   );
-
+delete from labels;
+INSERT INTO labels(text_color, background_color, space_id) 
+	VALUES ('#fff9db', '#2f4c56', '5c12842c-61ce-4481-b33d-163d09a732c4');
 
--- migration/sql-test-files/073-label-same-name.sql
+++ migration/sql-test-files/073-label-same-name.sql
@@ -1,17 +1,13 @@
+delete from labels;
 INSERT INTO
-	labels(created_at, updated_at, name, id, color, space_id)
+	labels(name, text_color, background_color, space_id)
 VALUES
    (
-	now(), now(), 'c0c3b254-c780-4a21-b00d-241a29e6be51', 'easy-fix', '#000000', '5c12842c-61ce-4481-b33d-163d09a732c4'
+	'easy-fix', '#fff9db', '#2f4c56', '2e0698d8-753e-4cef-bb7c-f027634824a2'
    );
-
--- same label name inside a space
--- this should fail
 INSERT INTO
-	labels(created_at, updated_at, name, id, color, space_id)
+	labels(name, text_color, background_color, space_id)
 VALUES
    (
-	now(), now(), 'c0c3b254-c780-4a21-b00d-241a29e6be51', 'easy-fix', '#000000', '5c12842c-61ce-4481-b33d-163d09a732c4'
+	'easy-fix', '#fff9db', '#2f4c56', '2e0698d8-753e-4cef-bb7c-f027634824a2'
    );
-
-
