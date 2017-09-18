0f50de03d24fc2c4a8a5c2ef1cc412b4f5f1741f
DhritiShikhar <dshikhar@redhat.com>
2017-09-18 17:03:18 +0530

    Removes UserActive field in test fixture

--- test/testfixture/make_functions.go
+++ test/testfixture/make_functions.go
@@ -133,11 +133,9 @@ func makeIterations(fxt *TestFixture) error {
 	iterationRepo := iteration.NewIterationRepository(fxt.db)
 	for i := range fxt.Iterations {
 		desc := "Some description"
-		f := false
 		fxt.Iterations[i] = &iteration.Iteration{
 			Name:        testsupport.CreateRandomValidTestName("iteration "),
 			Description: &desc,
-			UserActive:  f,
 		}
 		if !fxt.isolatedCreation {
 			fxt.Iterations[i].SpaceID = fxt.Spaces[0].ID
