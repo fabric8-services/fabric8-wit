e917fa61801fd697b82d307531e833f4e363ad7c
Konrad Kleine <kwk@users.noreply.github.com>
2017-09-19 12:04:08 +0200

    simplify-dbbench-suite (#1647)

--- codebase/codebase_bench_test.go
+++ codebase/codebase_bench_test.go
@@ -3,12 +3,8 @@ package codebase_test
 import (
 	"testing"
 
-	"golang.org/x/net/context"
-
 	"github.com/fabric8-services/fabric8-wit/codebase"
-	"github.com/fabric8-services/fabric8-wit/gormsupport/cleaner"
 	gormbench "github.com/fabric8-services/fabric8-wit/gormtestsupport/benchmark"
-	"github.com/fabric8-services/fabric8-wit/migration"
 	"github.com/fabric8-services/fabric8-wit/space"
 	testsupport "github.com/fabric8-services/fabric8-wit/test"
 	uuid "github.com/satori/go.uuid"
@@ -16,31 +12,16 @@ import (
 
 type BenchCodebaseRepository struct {
 	gormbench.DBBenchSuite
-	clean func()
-	repo  codebase.Repository
-	ctx   context.Context
+	repo codebase.Repository
 }
 
 func BenchmarkRunCodebaseRepository(b *testing.B) {
 	testsupport.Run(b, &BenchCodebaseRepository{DBBenchSuite: gormbench.NewDBBenchSuite("../config.yaml")})
 }
 
-// SetupSuite overrides the DBTestSuite's function but calls it before doing anything else
-// The SetupSuite method will run before the tests in the suite are run.
-// It sets up a database connection for all the tests in this suite without polluting global space.
-func (s *BenchCodebaseRepository) SetupSuite() {
-	s.DBBenchSuite.SetupSuite()
-	s.ctx = migration.NewMigrationContext(context.Background())
-	s.DBBenchSuite.PopulateDBBenchSuite(s.ctx)
-}
-
 func (s *BenchCodebaseRepository) SetupBenchmark() {
-	s.clean = cleaner.DeleteCreatedEntities(s.DB)
+	s.DBBenchSuite.SetupBenchmark()
 	s.repo = codebase.NewCodebaseRepository(s.DB)
-}
-
-func (s *BenchCodebaseRepository) TearDownBenchmark() {
-	s.clean()
 }
 
 func newCodebase(spaceID uuid.UUID, stackID, lastUsedWorkspace, repotype, url string) *codebase.Codebase {
@@ -54,7 +35,7 @@ func newCodebase(spaceID uuid.UUID, stackID, lastUsedWorkspace, repotype, url st
 }
 
 func (s *BenchCodebaseRepository) createCodebase(c *codebase.Codebase) {
-	err := s.repo.Create(s.ctx, c)
+	err := s.repo.Create(s.Ctx, c)
 	if err != nil {
 		s.B().Fail()
 	}
@@ -79,7 +60,7 @@ func (s *BenchCodebaseRepository) BenchmarkListCodebases() {
 	s.B().ResetTimer()
 	s.B().ReportAllocs()
 	for n := 0; n < s.B().N; n++ {
-		if codebases, _, err := s.repo.List(s.ctx, space.SystemSpace, &offset, &limit); err != nil || (err == nil && len(codebases) == 0) {
+		if codebases, _, err := s.repo.List(s.Ctx, space.SystemSpace, &offset, &limit); err != nil || (err == nil && len(codebases) == 0) {
 			s.B().Fail()
 		}
 	}
@@ -93,7 +74,7 @@ func (s *BenchCodebaseRepository) BenchmarkLoadCodebase() {
 	s.B().ResetTimer()
 	s.B().ReportAllocs()
 	for n := 0; n < s.B().N; n++ {
-		if loadedCodebase, err := s.repo.Load(s.ctx, codebaseTest.ID); err != nil || (err == nil && loadedCodebase == nil) {
+		if loadedCodebase, err := s.repo.Load(s.Ctx, codebaseTest.ID); err != nil || (err == nil && loadedCodebase == nil) {
 			s.B().Fail()
 		}
 	}
