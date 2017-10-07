cf777184030112eaa18212efe0b950c3b3822e21
Alexey Kazakov <alkazako@redhat.com>
2017-10-07 10:28:57 -0700

    Headers in log

--- rest/proxy/proxy.go
+++ rest/proxy/proxy.go
@@ -118,6 +118,7 @@ func newDirector(ctx context.Context, originalRequestData *goa.RequestData, targ
 			"target_req_url":   targetReqString,
 			"target":           target,
 			"target_string":    target.String(),
+			"headers":          req.Header,
 		}, "Routing %s to %s", originalReqString, targetReqString)
 	}
 }
