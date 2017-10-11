739c654e0d9393b3dd7d22e2585c559958597ce4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-10-11 17:10:21 +0530

    comment about migration

--- migration/sql-files/077-assignee-and-label-empty-value.sql
+++ migration/sql-files/077-assignee-and-label-empty-value.sql
@@ -1,3 +1,4 @@
+-- Remove 'system.assignees' from the fields if the value is 'null' or '[]'
 update work_items set fields=fields - 'system.assignees' where Fields->>'system.assignees' is null;
 update work_items set fields=fields - 'system.assignees' where Fields->>'system.assignees'='[]';
 update work_items set fields=fields - 'system.labels' where Fields->>'system.labels' is null;
