19a932ec72f2db42e35ce32ceba13fdb1d2d0157
Konrad Kleine <kkleine@redhat.com>
2017-11-30 23:35:49 +0100

    Redesign the work item type group and structure TestWICountsWithIterationListBySpace

--- controller/work_item_type_group.go
+++ controller/work_item_type_group.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/fabric8-services/fabric8-wit/app"
 	"github.com/fabric8-services/fabric8-wit/application"
+	"github.com/fabric8-services/fabric8-wit/errors"
 	"github.com/fabric8-services/fabric8-wit/jsonapi"
-	"github.com/fabric8-services/fabric8-wit/workitem/typegroup"
+	"github.com/fabric8-services/fabric8-wit/rest"
+	"github.com/fabric8-services/fabric8-wit/space"
+	"github.com/fabric8-services/fabric8-wit/workitem"
 	"github.com/goadesign/goa"
 )
 
@@ -26,6 +29,26 @@ func NewWorkItemTypeGroupController(service *goa.Service, db application.DB) *Wo
 	}
 }
 
+// Show runs the list action.
+func (c *WorkItemTypeGroupController) Show(ctx *app.ShowWorkItemTypeGroupContext) error {
+	err := application.Transactional(c.db, func(appl application.Application) error {
+		return appl.Spaces().CheckExists(ctx, ctx.SpaceTemplateID.String())
+	})
+	if err != nil {
+		return jsonapi.JSONErrorResponse(ctx, err)
+	}
+	// TODO(kwk): Replace with loading from DB once type groups are persistently
+	// stored in there.
+	for _, group := range workitem.TypeGroups() {
+		if group.ID == ctx.GroupID {
+			return ctx.OK(&app.WorkItemTypeGroupSingle{
+				Data: ConvertTypeGroup(ctx.Request, group),
+			})
+		}
+	}
+	return jsonapi.JSONErrorResponse(ctx, errors.NewNotFoundError("type group", ctx.GroupID.String()))
+}
+
 // List runs the list action.
 func (c *WorkItemTypeGroupController) List(ctx *app.ListWorkItemTypeGroupContext) error {
 	err := application.Transactional(c.db, func(appl application.Application) error {
@@ -34,29 +57,70 @@ func (c *WorkItemTypeGroupController) List(ctx *app.ListWorkItemTypeGroupContext
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
-	res := &app.WorkItemTypeGroupSigleSingle{}
-	res.Data = &app.WorkItemTypeGroupData{
-		Attributes: &app.WorkItemTypeGroupAttributes{
-			Hierarchy: []*app.WorkItemTypeGroup{
-				ConvertTypeGroup(ctx.Request, typegroup.Portfolio0),
-				ConvertTypeGroup(ctx.Request, typegroup.Portfolio1),
-				ConvertTypeGroup(ctx.Request, typegroup.Requirements0),
-				ConvertTypeGroup(ctx.Request, typegroup.Execution0),
-			},
+	typeGroups := workitem.TypeGroups()
+	res := &app.WorkItemTypeGroupList{
+		Data: make([]*app.WorkItemTypeGroupData, len(typeGroups)),
+		Links: &app.WorkItemTypeGroupLinks{
+			Self: rest.AbsoluteURL(ctx.Request, app.SpaceTemplateHref(space.SystemSpace)) + "/" + APIWorkItemTypeGroups,
 		},
-		Type: APIWorkItemTypeGroups,
+	}
+	for i, group := range typeGroups {
+		res.Data[i] = ConvertTypeGroup(ctx.Request, group)
 	}
 	return ctx.OK(res)
 }
 
 // ConvertTypeGroup converts WorkitemTypeGroup model to a response resource
 // object for jsonapi.org specification
-func ConvertTypeGroup(request *http.Request, tg typegroup.WorkItemTypeGroup) *app.WorkItemTypeGroup {
-	return &app.WorkItemTypeGroup{
-		Group:         tg.Group,
-		Level:         tg.Level,
-		Name:          tg.Name,
-		WitCollection: tg.WorkItemTypeCollection,
-		Icon:          tg.Icon,
+func ConvertTypeGroup(request *http.Request, tg workitem.WorkItemTypeGroup) *app.WorkItemTypeGroupData {
+
+	workitemtypes := "workitemtypes"
+	// TODO(kwk): Replace system space once we have space templates
+	// defaultWorkItemTypeRelatedURL := rest.AbsoluteURL(request, app.WorkitemtypeHref(space.SystemSpace, tg.DefaultType))
+	workItemTypeGroupRelatedURL := rest.AbsoluteURL(request, app.WorkItemTypeGroupHref(space.SystemSpace, tg.ID))
+	defaultIDStr := tg.DefaultType.String()
+	createdAt := tg.CreatedAt.UTC()
+	updatedAt := tg.UpdatedAt.UTC()
+
+	res := &app.WorkItemTypeGroupData{
+		ID:   &tg.ID,
+		Type: APIWorkItemTypeGroups,
+		Links: &app.GenericLinks{
+			Related: &workItemTypeGroupRelatedURL,
+		},
+		Attributes: &app.WorkItemTypeGroupAttributes{
+			Bucket:    tg.Bucket.String(),
+			Name:      tg.Name,
+			Icon:      tg.Icon,
+			CreatedAt: &createdAt,
+			UpdatedAt: &updatedAt,
+		},
+		Relationships: &app.WorkItemTypeGroupRelationships{
+			DefaultType: &app.RelationGeneric{
+				Data: &app.GenericData{
+					ID:   &defaultIDStr,
+					Type: &workitemtypes,
+					// Links: &app.GenericLinks{
+					// 	Related: &defaultWorkItemTypeRelatedURL,
+					// },
+				},
+			},
+			TypeList: &app.RelationGenericList{
+				Data: make([]*app.GenericData, len(tg.TypeList)),
+			},
+		},
 	}
+
+	for i, witID := range tg.TypeList {
+		// witRelatedURL := rest.AbsoluteURL(request, app.WorkitemtypeHref(space.SystemSpace, witID))
+		idStr := witID.String()
+		res.Relationships.TypeList.Data[i] = &app.GenericData{
+			ID:   &idStr,
+			Type: &workitemtypes,
+			// Links: &app.GenericLinks{
+			// Related: &witRelatedURL,
+			// },
+		}
+	}
+	return res
 }
