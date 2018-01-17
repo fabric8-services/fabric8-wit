bbbd0b4dd1d3d1fd9266c87bda23c26d5a7d67a7
Pranav Gore <pranavgore09@gmail.com>
2018-01-17 17:47:16 +0530

    apply unique index ONLY for non-deleted records. updated tests accordingly

--- migration/sql-files/081-queries.sql
+++ migration/sql-files/081-queries.sql
@@ -6,8 +6,8 @@ CREATE TABLE queries (
     title text NOT NULL CHECK(title <> ''),
     fields jsonb NOT NULL,
     space_id uuid NOT NULL REFERENCES spaces (id) ON DELETE CASCADE,
-    creator uuid NOT NULL,
-    CONSTRAINT queries_title_space_id_creator_unique UNIQUE(title, space_id, creator, deleted_at)
+    creator uuid NOT NULL
 );
+CREATE UNIQUE INDEX queries_title_space_id_creator_unique ON queries (title, space_id, creator) WHERE deleted_at IS NULL;
 
 CREATE INDEX query_creator_idx ON queries USING btree (creator);
--- migration/sql-test-files/081-query-conflict.sql
+++ migration/sql-test-files/081-query-conflict.sql
@@ -3,20 +3,18 @@ INSERT INTO spaces (id, name) VALUES ('e2297c54-eb0c-4eb0-b1f9-d3212cda5e1f', 't
 
 DELETE FROM queries;
 INSERT INTO
-	queries(title, fields, space_id, creator, deleted_at)
+	queries(title, fields, space_id, creator)
 VALUES
    (
 	'assigned to me', '{"key": "value"}', 
     'e2297c54-eb0c-4eb0-b1f9-d3212cda5e1f',
-    '5f6d7daf-6be3-4171-a77b-857b327c4bac',
-    '2018-01-02 10:26:55.742134+00'
+    '5f6d7daf-6be3-4171-a77b-857b327c4bac'
    );
 INSERT INTO
-	queries(title, fields, space_id, creator, deleted_at)
+	queries(title, fields, space_id, creator)
 VALUES
    (
 	'assigned to me', '{"key": "value"}',
     'e2297c54-eb0c-4eb0-b1f9-d3212cda5e1f',
-    '5f6d7daf-6be3-4171-a77b-857b327c4bac',
-    '2018-01-02 10:26:55.742134+00'
+    '5f6d7daf-6be3-4171-a77b-857b327c4bac'
    );
