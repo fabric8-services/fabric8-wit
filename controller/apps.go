c56d526630af1798f387d9973c2fd4f93c2d81f1
Simon Tooke <stooke@redhat.com>
2018-02-06 12:03:46 -0500

    Make /apps API JSON-API conformant (Issue #1848) (#1870)
    Adds JSON-API compliant /deployments endpoints.
    
    Co-authored-by: Elliot Baron <ebaron@redhat.com>
    Co-authored-by: Christopher Koehler <chkoehle@redhat.com>
    
    This commit adds JSON-API conformance to the following endpoints (used on the Deployments page UI):
    
    /deployments/environments/{envName}
    /deployments/environments/{envName}/applications/{appName}/pods
    /deployments/spaces/{spaceID}
    /deployments/spaces/{spaceID}/environments
    /deployments/spaces/{spaceID}/applications/{appName}
    /deployments/spaces/{spaceID}/applications/{appName}/deployments/{deployName}
    /deployments/spaces/{spaceID}/applications/{appName}/deployments/{deployName} (PUT)
    /deployments/spaces/{spaceID}/applications/{appName}/deployments/{deployName}/stats
    /deployments/spaces/{spaceID}/applications/{appName}/deployments/{deployName}/statseries
    
    Fixes #1848

--- controller/apps.go
+++ controller/apps.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/url"
 	"os"
 	"time"
@@ -48,14 +49,6 @@ func NewAppsController(service *goa.Service, config *configuration.Registry) *Ap
 			config: config,
 		},
 	}
-}
-
-func tostring(item interface{}) string {
-	bytes, err := json.MarshalIndent(item, "", "  ")
-	if err != nil {
-		return err.Error()
-	}
-	return string(bytes)
 }
 
 func getAndCheckOSIOClientV1(ctx context.Context) *OSIOClientV1 {
@@ -124,7 +117,7 @@ func getUserV1(authClient authservice.Client, ctx context.Context) (*authservice
 	respBody, err := ioutil.ReadAll(resp.Body)
 
 	status := resp.StatusCode
-	if status < 200 || status > 300 {
+	if status != http.StatusOK {
 		return nil, fmt.Errorf("Failed to GET user due to status code %d", status)
 	}
 
@@ -148,7 +141,7 @@ func getTokenDataV1(authClient authservice.Client, ctx context.Context, forServi
 	respBody, err := ioutil.ReadAll(resp.Body)
 
 	status := resp.StatusCode
-	if status < 200 || status > 300 {
+	if status != http.StatusOK {
 		return nil, errors.New("Failed to GET user due to status code " + string(status))
 	}
 
@@ -285,10 +278,6 @@ func (c *AppsController) ShowDeploymentStatSeries(ctx *app.ShowDeploymentStatSer
 	}
 
 	return ctx.OK(res)
-}
-
-func convertToTime(unixMillis int64) time.Time {
-	return time.Unix(0, unixMillis*int64(time.Millisecond))
 }
 
 // ShowDeploymentStats runs the showDeploymentStats action.
