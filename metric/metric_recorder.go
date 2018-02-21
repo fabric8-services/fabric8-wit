a86447c351c22332415bef2d2b1c7e0b1574d1cf
nurali-techie <nurali.techie@gmail.com>
2018-02-21 17:34:19 +0530

    Test added for metric recorder with minor refactoring.

--- metric/metric_recorder.go
+++ metric/metric_recorder.go
@@ -10,14 +10,15 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var reqLabels = []string{"entity", "action"}
-
-// Recorder record metrics
-func Recorder() goa.Middleware {
-	reqCnt := prometheus.NewCounterVec(prometheus.CounterOpts{
+var (
+	reqLabels = []string{"entity", "action"}
+	reqCnt    = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "wit_requests_total",
 		Help: "Total number of WIT requests.",
 	}, reqLabels)
+)
+
+func init() {
 	err := prometheus.Register(reqCnt)
 	if err != nil {
 		if are, ok := err.(prometheus.AlreadyRegisteredError); ok {
@@ -29,25 +30,30 @@ func Recorder() goa.Middleware {
 			}, "failed to register the prometheus metric")
 		}
 	}
+}
 
+// Recorder record metrics
+func Recorder() goa.Middleware {
 	return func(h goa.Handler) goa.Handler {
 		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
 			err := h(ctx, rw, req)
-
-			action := goa.ContextAction(ctx)
-			ctrl := goa.ContextController(ctx)
-			entity := ""
-			if strings.HasSuffix(ctrl, "Controller") {
-				entity = strings.ToLower(strings.TrimSuffix(ctrl, "Controller"))
-			}
-			log.Debug(ctx, nil, "ctrl=%s, entity=%s, action=%s", ctrl, entity, action)
-
-			if entity != "" {
-				reqCnt.WithLabelValues(entity, action).Inc()
-			}
-
+			recordMetric(ctx)
 			return err
 		}
 	}
 
+}
+
+func recordMetric(ctx context.Context) {
+	action := goa.ContextAction(ctx)
+	ctrl := goa.ContextController(ctx)
+	entity := ""
+	if strings.HasSuffix(ctrl, "Controller") {
+		entity = strings.ToLower(strings.TrimSuffix(ctrl, "Controller"))
+	}
+	log.Debug(ctx, nil, "ctrl=%s, entity=%s, action=%s", ctrl, entity, action)
+
+	if entity != "" {
+		reqCnt.WithLabelValues(entity, action).Inc()
+	}
 }
