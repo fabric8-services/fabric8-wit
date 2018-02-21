11a3e92d6a025459c84a816f769e2548f6765ddc
nurali-techie <nurali.techie@gmail.com>
2018-02-21 12:31:23 +0530

    Review comments incorporated. First, added error handling when metric reg failed. Second, lower-case action value.

--- metric/metric_recorder.go
+++ metric/metric_recorder.go
@@ -18,8 +18,17 @@ func Recorder() goa.Middleware {
 		Name: "wit_requests_total",
 		Help: "Total number of WIT requests.",
 	}, reqLabels)
-	// TODO VN: Check err, need prometheus version change.
-	prometheus.Register(reqCnt)
+	err := prometheus.Register(reqCnt)
+	if err != nil {
+		if are, ok := err.(prometheus.AlreadyRegisteredError); ok {
+			reqCnt = are.ExistingCollector.(*prometheus.CounterVec)
+		} else {
+			log.Panic(nil, map[string]interface{}{
+				"metric_name": "wit_requests_total",
+				"err":         err,
+			}, "failed to register the prometheus metric")
+		}
+	}
 
 	return func(h goa.Handler) goa.Handler {
 		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
@@ -29,7 +38,7 @@ func Recorder() goa.Middleware {
 			ctrl := goa.ContextController(ctx)
 			entity := ""
 			if strings.HasSuffix(ctrl, "Controller") {
-				entity = strings.TrimSuffix(ctrl, "Controller")
+				entity = strings.ToLower(strings.TrimSuffix(ctrl, "Controller"))
 			}
 			log.Debug(ctx, nil, "ctrl=%s, entity=%s, action=%s", ctrl, entity, action)
 
