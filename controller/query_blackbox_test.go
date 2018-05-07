fabric8-services/fabric8-wit

commit 6fc47d8cf3c68fccb751a431d21f43106dd41d84

Query update feature (#2061)
Support updating saved queries

controller/query.go
@@ -86,6 +86,7 @@ func ConvertQuery(request *http.Request, q query.Query) *app.Query {
 			Title:     q.Title,
 			Fields:    q.Fields,
 			CreatedAt: &q.CreatedAt,
+			Version:   &q.Version,
 		},
 		Links: &app.GenericLinks{
 			Self:    &relatedURL,
@@ -181,6 +182,52 @@ func (c *QueryController) Show(ctx *app.ShowQueryContext) error {
 		Data: ConvertQuery(ctx.Request, *q),
 	}
 	return ctx.OK(res)
+}
+
+// Update runs the update action.
+func (c *QueryController) Update(ctx *app.UpdateQueryContext) error {
+	currentUser, err := login.ContextIdentity(ctx)
+	if err != nil {
+		return jsonapi.JSONErrorResponse(ctx, goa.ErrUnauthorized(err.Error()))
+	}
+	if ctx.Payload.Data.Attributes.Version == nil {
+		return jsonapi.JSONErrorResponse(ctx, errors.NewBadParameterError("data.attributes.version", nil).Expected("not nil"))
+	}
+	var q *query.Query
+	err = application.Transactional(c.db, func(appl application.Application) error {
+		var err error
+		q, err = appl.Queries().Load(ctx.Context, ctx.QueryID, ctx.SpaceID)
+		if err != nil {
+			return err
+		}
+		if q.Creator != *currentUser {
+			log.Warn(ctx, map[string]interface{}{
+				"query_id":     ctx.QueryID,
+				"creator":      q.Creator,
+				"current_user": *currentUser,
+			}, "user is not the query creator")
+			return errors.NewForbiddenError("user is not the query creator")
+		}
+		if q.Version != *ctx.Payload.Data.Attributes.Version {
+			return errors.NewVersionConflictError("version conflict")
+		}
+		if ctx.Payload.Data.Attributes.Title != "" {
+			q.Title = strings.TrimSpace(ctx.Payload.Data.Attributes.Title)
+		}
+		if strings.TrimSpace(ctx.Payload.Data.Attributes.Fields) != "" {
+			q.Fields = strings.TrimSpace(ctx.Payload.Data.Attributes.Fields)
+		}
+		q, err = appl.Queries().Save(ctx, *q)
+		return err
+	})
+	if err != nil {
+		return jsonapi.JSONErrorResponse(ctx, err)
+	}
+	result := &app.QuerySingle{
+		Data: ConvertQuery(ctx.Request, *q),
+	}
+	ctx.ResponseData.Header().Set("Location", rest.AbsoluteURL(ctx.Request, app.QueryHref(ctx.SpaceID, result.Data.ID)))
+	return ctx.OK(result)
 }
 
 // Delete runs the delete action.

controller/query_blackbox_test.go
@@ -2,6 +2,7 @@ package controller_test
 
 import (
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/fabric8-services/fabric8-auth/auth"
@@ -302,6 +303,201 @@ func (rest *TestQueryREST) TestShow() {
 	})
 }
 
+func (rest *TestQueryREST) TestUpdate() {
+
+	rest.T().Run("update query", func(t *testing.T) {
+		// 1 for each test, without conflict of changes during each test execution, isolated or not :)
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt.Identities[0])
+		newTitle := "Query New 1001"
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   newTitle,
+					Version: &testFxt.Queries[0].Version,
+					Fields:  fields,
+				},
+				ID:   &testFxt.Queries[0].ID,
+				Type: query.APIStringTypeQuery,
+			},
+		}
+		resp, updated := test.UpdateQueryOK(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		assert.Equal(t, newTitle, updated.Data.Attributes.Title)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "ok.query.golden.json"), updated)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "ok.headers.golden.json"), resp)
+
+		_, queries2 := test.ShowQueryOK(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil)
+		assertQueryLinking(t, queries2.Data)
+		require.NotEmpty(t, queries2.Data, "queries found")
+		assert.Equal(t, newTitle, queries2.Data.Attributes.Title)
+	})
+
+	rest.T().Run("update query with version conflict", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt.Identities[0])
+		newVersion := testFxt.Queries[0].Version + 2
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   testFxt.Queries[0].Title,
+					Version: &newVersion,
+					Fields:  fields,
+				},
+				ID:   &testFxt.Queries[0].ID,
+				Type: query.APIStringTypeQuery,
+			},
+		}
+		_, jerrs := test.UpdateQueryConflict(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		require.NotNil(t, jerrs)
+		require.Len(t, jerrs.Errors, 1)
+		require.Contains(t, jerrs.Errors[0].Detail, "version conflict")
+		ignoreString := "IGNORE_ME"
+		jerrs.Errors[0].ID = &ignoreString
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "conflict.errors.golden.json"), jerrs)
+	})
+
+	rest.T().Run("update query with bad parameter", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt.Identities[0])
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:  testFxt.Queries[0].Title,
+					Fields: fields,
+				},
+				Type: query.APIStringTypeQuery,
+			},
+		}
+
+		_, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		require.NotNil(t, jerrs)
+		require.Len(t, jerrs.Errors, 1)
+		require.Contains(t, jerrs.Errors[0].Detail, "Bad value for parameter 'data.attributes.version'")
+		ignoreString := "IGNORE_ME"
+		jerrs.Errors[0].ID = &ignoreString
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_version.errors.golden.json"), jerrs)
+	})
+
+	rest.T().Run("update query with bad parameter - title", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt.Identities[0])
+		newTitle := " 	   " // tab & spaces
+		newVersion := testFxt.Queries[0].Version
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   newTitle,
+					Version: &newVersion,
+					Fields:  fields,
+				},
+				ID:   &testFxt.Queries[0].ID,
+				Type: query.APIStringTypeQuery,
+			},
+		}
+
+		_, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		require.NotNil(t, jerrs)
+		require.Len(t, jerrs.Errors, 1)
+		require.Contains(t, jerrs.Errors[0].Detail, "Bad value for parameter 'query title cannot be empty string'")
+		ignoreString := "IGNORE_ME"
+		jerrs.Errors[0].ID = &ignoreString
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_name.errors.golden.json"), jerrs)
+	})
+
+	rest.T().Run("update query with unauthorized", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc := goa.New("Query-Service")
+		ctrl := NewQueryController(svc, rest.db, rest.Configuration)
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   testFxt.Queries[0].Title,
+					Version: &testFxt.Queries[0].Version,
+					Fields:  fields,
+				},
+				Type: query.APIStringTypeQuery,
+			},
+		}
+
+		_, jerrs := test.UpdateQueryUnauthorized(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		require.NotNil(t, jerrs)
+		require.Len(t, jerrs.Errors, 1)
+		require.Contains(t, jerrs.Errors[0].Detail, "Missing token manager")
+		ignoreString := "IGNORE_ME"
+		jerrs.Errors[0].ID = &ignoreString
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "unauthorized.errors.golden.json"), jerrs)
+	})
+
+	rest.T().Run("different user updating", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		testFxt2 := tf.NewTestFixture(t, rest.DB,
+			tf.Identities(1))
+
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt2.Identities[0])
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   testFxt.Queries[0].Title,
+					Version: &testFxt.Queries[0].Version,
+					Fields:  fields,
+				},
+				Type: query.APIStringTypeQuery,
+			},
+		}
+
+		_, jerrs := test.UpdateQueryForbidden(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		require.NotNil(t, jerrs)
+		require.Len(t, jerrs.Errors, 1)
+		require.Contains(t, jerrs.Errors[0].Detail, "user is not the query creator")
+		ignoreString := "IGNORE_ME"
+		jerrs.Errors[0].ID = &ignoreString
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "forbiden.errors.golden.json"), jerrs)
+	})
+
+	rest.T().Run("update query not found", func(t *testing.T) {
+		testFxt := tf.NewTestFixture(t, rest.DB,
+			tf.CreateWorkItemEnvironment(),
+			tf.Queries(1))
+		svc, ctrl := rest.SecuredControllerWithIdentity(testFxt.Identities[0])
+		newTitle := "Query New 1002"
+		newVersion := testFxt.Queries[0].Version + 1
+		fields := `{"$AND": [{"space": "1b0efd64-ba69-42a6-b7da-762264744223"}]}`
+		id := uuid.NewV4()
+		payload := app.UpdateQueryPayload{
+			Data: &app.Query{
+				Attributes: &app.QueryAttributes{
+					Title:   newTitle,
+					Version: &newVersion,
+					Fields:  fields,
+				},
+				ID:   &id,
+				Type: query.APIStringTypeQuery,
+			},
+		}
+		test.UpdateQueryNotFound(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, id, nil, nil, &payload)
+	})
+}
+
 func (rest *TestQueryREST) TestDelete() {
 
 	rest.T().Run("success", func(t *testing.T) {
@@ -350,4 +546,15 @@ func (rest *TestQueryREST) TestDelete() {
 			test.DeleteQueryNotFound(t, svc.Context, svc, ctrl, fxt.Spaces[0].ID, uuid.Nil)
 		})
 	})
+}
+
+func assertQueryLinking(t *testing.T, target *app.Query) {
+	assert.NotNil(t, target.ID)
+	assert.Equal(t, query.APIStringTypeQuery, target.Type)
+	assert.NotNil(t, target.Links.Self)
+	require.NotNil(t, target.Relationships)
+	require.NotNil(t, target.Relationships.Space)
+	require.NotNil(t, target.Relationships.Space.Links)
+	require.NotNil(t, target.Relationships.Space.Links.Self)
+	assert.True(t, strings.Contains(*target.Relationships.Space.Links.Self, "/api/spaces/"))
 }

query/query.go
@@ -2,8 +2,10 @@ package query
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/fabric8-services/fabric8-wit/application/repository"
@@ -27,6 +29,7 @@ type Query struct {
 	Creator uuid.UUID `sql:"type:uuid"`
 	Title   string
 	Fields  string
+	Version int
 }
 
 // QueryTableName constant that holds table name of Queries
@@ -50,6 +53,7 @@ type Repository interface {
 	List(ctx context.Context, spaceID uuid.UUID) ([]Query, error)
 	ListByCreator(ctx context.Context, spaceID uuid.UUID, creatorID uuid.UUID) ([]Query, error)
 	Load(ctx context.Context, queryID uuid.UUID, spaceID uuid.UUID) (*Query, error)
+	Save(ctx context.Context, q Query) (*Query, error)
 	Delete(ctx context.Context, ID uuid.UUID) error
 }
 
@@ -81,6 +85,10 @@ func (r *GormQueryRepository) Create(ctx context.Context, q *Query) error {
 	if q.Creator == uuid.Nil {
 		return errors.NewBadParameterError("creator cannot be nil", q.Creator).Expected("valid user ID")
 	}
+	var v map[string]interface{}
+	if err := json.Unmarshal([]byte(q.Fields), &v); err != nil {
+		return errors.NewBadParameterError("query field is invalid JSON syntax", q.Fields).Expected("valid JSON")
+	}
 	// Parse fields to make sure that query is valid
 	exp, _, err := search.ParseFilterString(ctx, q.Fields)
 	if err != nil || exp == nil {
@@ -108,6 +116,60 @@ func (r *GormQueryRepository) Create(ctx context.Context, q *Query) error {
 		return err
 	}
 	return nil
+}
+
+// Save update the given query
+func (r *GormQueryRepository) Save(ctx context.Context, q Query) (*Query, error) {
+	defer goa.MeasureSince([]string{"goa", "db", "query", "save"}, time.Now())
+	if strings.TrimSpace(q.Title) == "" {
+		return nil, errors.NewBadParameterError("query title cannot be empty string", q.Title).Expected("non empty string")
+	}
+	var v map[string]interface{}
+	if err := json.Unmarshal([]byte(q.Fields), &v); err != nil {
+		return nil, errors.NewBadParameterError("query field is invalid JSON syntax", q.Fields).Expected("valid JSON")
+	}
+	qry := Query{}
+	tx := r.db.Where("id = ?", q.ID).First(&qry)
+	oldVersion := q.Version
+	q.Version = qry.Version + 1
+	if tx.RecordNotFound() {
+		log.Error(ctx, map[string]interface{}{
+			"query_id": q.ID,
+		}, "query cannot be found")
+		return nil, errors.NewNotFoundError("query", q.ID.String())
+	}
+	if err := tx.Error; err != nil {
+		log.Error(ctx, map[string]interface{}{
+			"query_id": q.ID,
+			"err":      err,
+		}, "unknown error happened when searching the query")
+		return nil, errors.NewInternalError(ctx, err)
+	}
+	tx = tx.Where("Version = ?", oldVersion).Save(&q)
+	if err := tx.Error; err != nil {
+		// combination of name and space ID should be unique
+		if gormsupport.IsUniqueViolation(err, "queries_title_space_id_creator_unique") {
+			log.Error(ctx, map[string]interface{}{
+				"err":      err,
+				"title":    q.Title,
+				"space_id": q.SpaceID,
+			}, "unable to create query because a query with same title already exists in the space")
+			return nil, errors.NewDataConflictError(fmt.Sprintf("query already exists with title = %s , space_id = %s", q.Title, q.SpaceID.String()))
+		}
+		log.Error(ctx, map[string]interface{}{
+			"query_id": q.ID,
+			"err":      err,
+		}, "unable to save the query")
+		return nil, errors.NewInternalError(ctx, err)
+	}
+	if tx.RowsAffected == 0 {
+		return nil, errors.NewVersionConflictError("version conflict")
+	}
+	log.Debug(ctx, map[string]interface{}{
+		"query_id": q.ID,
+	}, "query updated successfully")
+
+	return &q, nil
 }
 
 // List all queries in a space

design/queries.go
@@ -30,6 +30,12 @@ var queryAttributes = a.Type("QueryAttributes", func() {
 	a.Attribute("created-at", d.DateTime, "When the query was created", func() {
 		a.Example("2016-11-29T23:18:14Z")
 	})
+	a.Attribute("updated-at", d.DateTime, "When the query was updated", func() {
+		a.Example("2016-11-29T23:18:14Z")
+	})
+	a.Attribute("version", d.Integer, "Version for optimistic concurrency control (optional during creating)", func() {
+		a.Example(23)
+	})
 	a.Attribute("fields", d.String, mandatoryOnCreate("Query fields"), func() {
 		a.Example(`"{ \"$AND\":[ { \"space\":\"a2d6ab7a-5d35-47b5-8fff-d4ce6285a158\" }, { \"assignee\":\"7ef78c14-f314-4a5a-8512-21640e3d2ef8\" } ] }"`)
 	})
@@ -103,6 +109,28 @@ var _ = a.Resource("query", func() {
 		a.Response(d.Forbidden, JSONAPIErrors)
 		a.Response(d.NotFound, JSONAPIErrors)
 		a.Response(d.Conflict, JSONAPIErrors)
+	})
+
+	a.Action("update", func() {
+		a.Security("jwt")
+		a.Routing(
+			a.PATCH("/:queryID"),
+		)
+		a.Description("Update the query for the given id.")
+		a.Params(func() {
+			a.Param("queryID", d.UUID, "ID of the query to update")
+		})
+		a.UseTrait("conditional")
+		a.Payload(querySingle)
+		a.Response(d.OK, func() {
+			a.Media(querySingle)
+		})
+		a.Response(d.BadRequest, JSONAPIErrors)
+		a.Response(d.Conflict, JSONAPIErrors)
+		a.Response(d.InternalServerError, JSONAPIErrors)
+		a.Response(d.NotFound, JSONAPIErrors)
+		a.Response(d.Unauthorized, JSONAPIErrors)
+		a.Response(d.Forbidden, JSONAPIErrors)
 	})
 
 	a.Action("delete", func() {

migration/migration.go
@@ -407,6 +407,9 @@ func GetMigrations() Migrations {
 		workitem.SystemPlannerItem.String(),
 	)})
 
+	// Version 90
+	m = append(m, steps{ExecuteSQLFile("090-queries-version.sql")})
+
 	// Version N
 	//
 	// In order to add an upgrade, simply append an array of MigrationFunc to the

migration/migration_blackbox_test.go
@@ -138,6 +138,7 @@ func TestMigrations(t *testing.T) {
 	t.Run("TestMigration87", testMigration87SpaceTemplates) // space templates
 	t.Run("TestMigration88", testMigration88TypeGroups)     // type groups
 	t.Run("TestMigration89", testMigration89FixupForSpaceTemplates)
+	t.Run("TestMigration90", testMigration90QueriesVersion)
 
 	// Perform the migration
 	err = migration.Migrate(sqlDB, databaseName)
@@ -796,6 +797,11 @@ func testMigration89FixupForSpaceTemplates(t *testing.T) {
 	err = stmt.QueryRow(spacetemplate.SystemBaseTemplateID).Scan(&cnt)
 	require.NoError(t, err)
 	require.Equal(t, 3, cnt)
+}
+
+func testMigration90QueriesVersion(t *testing.T) {
+	migrateToVersion(t, sqlDB, migrations[:91], 91)
+	assert.True(t, dialect.HasColumn("queries", "version"))
 }
 
 // runSQLscript loads the given filename from the packaged SQL test files and

migration/sql-files/090-queries-version.sql
@@ -1,0 +1,1 @@
+ALTER TABLE queries ADD COLUMN version INTEGER DEFAULT 0 NOT NULL;

commit 4c9ac8568a7f1b6a0936db8e6c1c7437be9be606

Print error stack & follow golden file naming
Some of these issues was introduced in #2061

controller/query.go
@@ -17,6 +17,7 @@ import (
 	"github.com/fabric8-services/fabric8-wit/rest"
 	"github.com/fabric8-services/fabric8-wit/space"
 	"github.com/goadesign/goa"
+	errs "github.com/pkg/errors"
 )
 
 // QueryController implements the query resource.
@@ -60,7 +61,7 @@ func (c *QueryController) Create(ctx *app.CreateQueryContext) error {
 			Creator: *currentUserIdentityID,
 		}
 		err = appl.Queries().Create(ctx, &q)
-		return err
+		return errs.WithStack(err)
 	})
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
@@ -136,10 +137,10 @@ func (c *QueryController) List(ctx *app.ListQueryContext) error {
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		err = appl.Spaces().CheckExists(ctx, ctx.SpaceID)
 		if err != nil {
-			return err
+			return errs.WithStack(err)
 		}
 		queries, err = appl.Queries().ListByCreator(ctx, ctx.SpaceID, *currentUserIdentityID)
-		return err
+		return errs.WithStack(err)
 	})
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
@@ -162,10 +163,10 @@ func (c *QueryController) Show(ctx *app.ShowQueryContext) error {
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		err := appl.Spaces().CheckExists(ctx, ctx.SpaceID)
 		if err != nil {
-			return err
+			return errs.WithStack(err)
 		}
 		q, err = appl.Queries().Load(ctx, ctx.QueryID, ctx.SpaceID)
-		return err
+		return errs.WithStack(err)
 	})
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
@@ -198,7 +199,7 @@ func (c *QueryController) Update(ctx *app.UpdateQueryContext) error {
 		var err error
 		q, err = appl.Queries().Load(ctx.Context, ctx.QueryID, ctx.SpaceID)
 		if err != nil {
-			return err
+			return errs.WithStack(err)
 		}
 		if q.Creator != *currentUser {
 			log.Warn(ctx, map[string]interface{}{
@@ -218,7 +219,7 @@ func (c *QueryController) Update(ctx *app.UpdateQueryContext) error {
 			q.Fields = strings.TrimSpace(ctx.Payload.Data.Attributes.Fields)
 		}
 		q, err = appl.Queries().Save(ctx, *q)
-		return err
+		return errs.WithStack(err)
 	})
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
@@ -239,7 +240,7 @@ func (c *QueryController) Delete(ctx *app.DeleteQueryContext) error {
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		q, err := appl.Queries().Load(ctx.Context, ctx.QueryID, ctx.SpaceID)
 		if err != nil {
-			return err
+			return errs.WithStack(err)
 		}
 		if q.Creator != *currentUser {
 			log.Warn(ctx, map[string]interface{}{
@@ -249,7 +250,8 @@ func (c *QueryController) Delete(ctx *app.DeleteQueryContext) error {
 			}, "user is not the query creator")
 			return errors.NewForbiddenError("user is not the query creator")
 		}
-		return appl.Queries().Delete(ctx.Context, ctx.QueryID)
+		err = appl.Queries().Delete(ctx.Context, ctx.QueryID)
+		return errs.WithStack(err)
 	})
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)

controller/query_blackbox_test.go
@@ -326,8 +326,8 @@ func (rest *TestQueryREST) TestUpdate() {
 		}
 		resp, updated := test.UpdateQueryOK(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		assert.Equal(t, newTitle, updated.Data.Attributes.Title)
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "ok.query.golden.json"), updated)
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "ok.headers.golden.json"), resp)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "update_query.res.payload.golden.json"), updated)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "update_query.res.headers.golden.json"), resp.Header())
 
 		_, queries2 := test.ShowQueryOK(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil)
 		assertQueryLinking(t, queries2.Data)
@@ -353,13 +353,14 @@ func (rest *TestQueryREST) TestUpdate() {
 				Type: query.APIStringTypeQuery,
 			},
 		}
-		_, jerrs := test.UpdateQueryConflict(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		resp, jerrs := test.UpdateQueryConflict(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		require.NotNil(t, jerrs)
 		require.Len(t, jerrs.Errors, 1)
 		require.Contains(t, jerrs.Errors[0].Detail, "version conflict")
 		ignoreString := "IGNORE_ME"
 		jerrs.Errors[0].ID = &ignoreString
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "conflict.errors.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "update_conflict.res.payload.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "update_conflict.res.headers.golden.json"), resp.Header())
 	})
 
 	rest.T().Run("update query with bad parameter", func(t *testing.T) {
@@ -378,13 +379,14 @@ func (rest *TestQueryREST) TestUpdate() {
 			},
 		}
 
-		_, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		resp, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		require.NotNil(t, jerrs)
 		require.Len(t, jerrs.Errors, 1)
 		require.Contains(t, jerrs.Errors[0].Detail, "Bad value for parameter 'data.attributes.version'")
 		ignoreString := "IGNORE_ME"
 		jerrs.Errors[0].ID = &ignoreString
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_version.errors.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_version.res.payload.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_version.res.headers.golden.json"), resp.Header())
 	})
 
 	rest.T().Run("update query with bad parameter - title", func(t *testing.T) {
@@ -407,13 +409,14 @@ func (rest *TestQueryREST) TestUpdate() {
 			},
 		}
 
-		_, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		resp, jerrs := test.UpdateQueryBadRequest(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		require.NotNil(t, jerrs)
 		require.Len(t, jerrs.Errors, 1)
 		require.Contains(t, jerrs.Errors[0].Detail, "Bad value for parameter 'query title cannot be empty string'")
 		ignoreString := "IGNORE_ME"
 		jerrs.Errors[0].ID = &ignoreString
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_name.errors.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_name.res.payload.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "badparam_name.res.headers.golden.json"), resp.Header())
 	})
 
 	rest.T().Run("update query with unauthorized", func(t *testing.T) {
@@ -435,13 +438,14 @@ func (rest *TestQueryREST) TestUpdate() {
 			},
 		}
 
-		_, jerrs := test.UpdateQueryUnauthorized(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		resp, jerrs := test.UpdateQueryUnauthorized(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		require.NotNil(t, jerrs)
 		require.Len(t, jerrs.Errors, 1)
 		require.Contains(t, jerrs.Errors[0].Detail, "Missing token manager")
 		ignoreString := "IGNORE_ME"
 		jerrs.Errors[0].ID = &ignoreString
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "unauthorized.errors.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "unauthorized.res.payload.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "unauthorized.res.headers.golden.json"), resp.Header())
 	})
 
 	rest.T().Run("different user updating", func(t *testing.T) {
@@ -465,13 +469,14 @@ func (rest *TestQueryREST) TestUpdate() {
 			},
 		}
 
-		_, jerrs := test.UpdateQueryForbidden(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
+		resp, jerrs := test.UpdateQueryForbidden(t, svc.Context, svc, ctrl, testFxt.Spaces[0].ID, testFxt.Queries[0].ID, nil, nil, &payload)
 		require.NotNil(t, jerrs)
 		require.Len(t, jerrs.Errors, 1)
 		require.Contains(t, jerrs.Errors[0].Detail, "user is not the query creator")
 		ignoreString := "IGNORE_ME"
 		jerrs.Errors[0].ID = &ignoreString
-		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "forbiden.errors.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "forbiden.res.payload.golden.json"), jerrs)
+		compareWithGoldenAgnostic(t, filepath.Join(rest.testDir, "update", "forbiden.res.headers.golden.json"), resp.Header())
 	})
 
 	rest.T().Run("update query not found", func(t *testing.T) {