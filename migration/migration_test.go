8f4e84c0eac1b75b872e9acac43dac314f3c64f4
Konrad Kleine <kkleine@redhat.com>
2018-06-29 13:02:53 +0200

    Prevent starting old cores on newer databases
    When a core starts up and find out that the database version is newer
    than the one it is aware of it stops the migration.
    
    NOTE: A core that is already running will not error out if the database
    is newer than what it is aware of. This only happens when a core starts
    up and the migration happens.
    
    See openshiftio/openshift.io#3880

--- migration/migration_test.go
+++ migration/migration_test.go
@@ -30,7 +30,7 @@ func TestConcurrentMigrations(t *testing.T) {
 			defer wg.Done()
 			db, err := sql.Open("postgres", configuration.GetPostgresConfigString())
 			if err != nil {
-				t.Fatalf("Cannot connect to DB: %s\n", err)
+				require.NoError(t, err, "cannot connect to DB")
 			}
 			err = Migrate(db, configuration.GetPostgresDatabase())
 			require.NoError(t, err, "%+v", err)
@@ -38,4 +38,37 @@ func TestConcurrentMigrations(t *testing.T) {
 
 	}
 	wg.Wait()
+}
+
+func TestMigrationWithExpectedVersion(t *testing.T) {
+	resource.Require(t, resource.Database)
+
+	configuration, err := config.Get()
+	if err != nil {
+		panic(fmt.Errorf("failed to setup the configuration: %s", err.Error()))
+	}
+
+	db, err := sql.Open("postgres", configuration.GetPostgresConfigString())
+	if err != nil {
+		require.NoError(t, err, "cannot connect to DB")
+	}
+
+	t.Run("ok", func(t *testing.T) {
+		err := Migrate(db, configuration.GetPostgresDatabase())
+		require.NoError(t, err, "%+v", err)
+	})
+	t.Run("fail due to newer db version", func(t *testing.T) {
+		// given
+		artificialDBVersion := len(GetMigrations()) + 1 + 100
+		_, err = db.Exec("INSERT INTO version(version) VALUES($1) ON CONFLICT DO NOTHING", artificialDBVersion)
+		require.NoError(t, err, "failed to insert artificial version: %d", artificialDBVersion)
+		defer func() {
+			_, err = db.Exec("DELETE FROM version WHERE version = $1", artificialDBVersion)
+			require.NoError(t, err, "failed to remove any potentially existing artificial version: %d", artificialDBVersion)
+		}()
+		// when
+		err = Migrate(db, configuration.GetPostgresDatabase())
+		// then
+		require.Error(t, err, "the migration should have failed because the db was artificially moved by 100 revisions to %d and core expects version %d: %+v", artificialDBVersion, len(GetMigrations())+1, err)
+	})
 }
