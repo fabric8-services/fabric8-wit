1648c40bedd38e25dcc3df25b40851ea820ad4f2
Konrad Kleine <kkleine@redhat.com>
2018-06-29 13:05:55 +0200

    Accidently overwrote "after"-part of test

--- migration/migration_blackbox_test.go
+++ migration/migration_blackbox_test.go
@@ -986,6 +986,20 @@ func testMigration95RemoveResolutionFieldFromImpediment(t *testing.T) {
 			expectWorkItemFieldsToBe(t, uuid.FromStringOrNil("6a870ee3-e57c-4f98-9c7a-3cdf2ef5c2ef"), `{"system.title": "Work item 2"}`)
 		})
 	})
+
+	// Migrate to the current version. That removes the "resolution" field from
+	// the "impediment" work items and their work item type. We then check that
+	// the "resolution" field has actually been removed from the work items and
+	// their type.
+	migrateToVersion(t, sqlDB, migrations[:96], 96)
+
+	t.Run("after", func(t *testing.T) {
+		t.Run("impediment", func(t *testing.T) {
+			expectWorkItemTypeFieldsToBe(t, uuid.FromStringOrNil("03b9bb64-4f65-4fa7-b165-494cd4f01401"), `{"system.area": {"type": {"kind": "area"}, "label": "Area", "required": false, "read_only": false, "description": "The area to which the work item belongs"}, "system.order": {"type": {"kind": "float"}, "label": "Execution Order", "required": false, "read_only": true, "description": "Execution Order of the workitem"}, "system.state": {"type": {"values": ["New", "Open", "In Progress", "Resolved", "Closed"], "base_type": {"kind": "string"}, "simple_type": {"kind": "enum"}, "rewritable_values": false}, "label": "State", "required": true, "read_only": false, "description": "The state of the impediment."}, "system.title": {"type": {"kind": "string"}, "label": "Title", "required": true, "read_only": false, "description": "The title text of the work item"}, "system.labels": {"type": {"simple_type": {"kind": "list"}, "component_type": {"kind": "label"}}, "label": "Labels", "required": false, "read_only": false, "description": "List of labels attached to the work item"}, "system.number": {"type": {"kind": "integer"}, "label": "Number", "required": false, "read_only": true, "description": "The unique number that was given to this workitem within its space."}, "system.creator": {"type": {"kind": "user"}, "label": "Creator", "required": true, "read_only": false, "description": "The user that created the work item"}, "system.codebase": {"type": {"kind": "codebase"}, "label": "Codebase", "required": false, "read_only": false, "description": "Contains codebase attributes to which this WI belongs to"}, "system.assignees": {"type": {"simple_type": {"kind": "list"}, "component_type": {"kind": "user"}}, "label": "Assignees", "required": false, "read_only": false, "description": "The users that are assigned to the work item"}, "system.iteration": {"type": {"kind": "iteration"}, "label": "Iteration", "required": false, "read_only": false, "description": "The iteration to which the work item belongs"}, "system.created_at": {"type": {"kind": "instant"}, "label": "Created at", "required": false, "read_only": true, "description": "The date and time when the work item was created"}, "system.updated_at": {"type": {"kind": "instant"}, "label": "Updated at", "required": false, "read_only": true, "description": "The date and time when the work item was last updated"}, "system.description": {"type": {"kind": "markup"}, "label": "Description", "required": false, "read_only": false, "description": "A descriptive text of the work item"}, "system.remote_item_id": {"type": {"kind": "string"}, "label": "Remote item", "required": false, "read_only": false, "description": "The ID of the remote work item"}}`)
+			expectWorkItemFieldsToBe(t, uuid.FromStringOrNil("24ed462d-0430-4ffe-ba4f-7b5725b6a48c"), `{"system.title": "Work item 1"}`)
+			expectWorkItemFieldsToBe(t, uuid.FromStringOrNil("6a870ee3-e57c-4f98-9c7a-3cdf2ef5c2ef"), `{"system.title": "Work item 2"}`)
+		})
+	})
 }
 
 func testMigration96ChangesToAgileTemplate(t *testing.T) {
