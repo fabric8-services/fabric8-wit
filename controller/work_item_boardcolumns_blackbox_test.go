d466eeb324ace4486338ae48b763bfe578af53b0
Konrad Kleine <kkleine@redhat.com>
2018-07-03 13:50:39 +0200

    some cleanup and sub tests

--- controller/work_item_boardcolumns_blackbox_test.go
+++ controller/work_item_boardcolumns_blackbox_test.go
@@ -10,8 +10,6 @@ import (
 	"github.com/fabric8-services/fabric8-wit/app"
 	"github.com/fabric8-services/fabric8-wit/app/test"
 	. "github.com/fabric8-services/fabric8-wit/controller"
-	"github.com/fabric8-services/fabric8-wit/gormapplication"
-	"github.com/fabric8-services/fabric8-wit/gormsupport/cleaner"
 	"github.com/fabric8-services/fabric8-wit/gormtestsupport"
 	"github.com/fabric8-services/fabric8-wit/ptr"
 	"github.com/fabric8-services/fabric8-wit/resource"
@@ -25,10 +23,7 @@ import (
 
 type TestWorkItemBoardcolumnREST struct {
 	gormtestsupport.DBTestSuite
-	db           *gormapplication.GormDB
-	ctx          context.Context
-	clean        func()
-	testIdentity account.Identity
+	ctx context.Context
 }
 
 func TestRunWorkItemBoardcolumnREST(t *testing.T) {
@@ -37,48 +32,38 @@ func TestRunWorkItemBoardcolumnREST(t *testing.T) {
 }
 
 func (l *TestWorkItemBoardcolumnREST) SetupTest() {
-	l.db = gormapplication.NewGormDB(l.DB)
-	l.clean = cleaner.DeleteCreatedEntities(l.DB)
 	req := &http.Request{Host: "localhost"}
 	params := url.Values{}
 	l.ctx = goa.NewContext(context.Background(), nil, req, params)
-	fixture := tf.NewTestFixture(l.T(), l.DB, tf.Identities(1))
-	require.NotNil(l.T(), fixture)
-	require.Len(l.T(), fixture.Identities, 1)
-	l.testIdentity = *fixture.Identities[0]
 }
 
-func (l *TestWorkItemBoardcolumnREST) TearDownTest() {
-	l.clean()
-}
-
-func (l *TestWorkItemBoardcolumnREST) SecuredController() (*goa.Service, app.WorkitemController) {
-	svc := testsupport.ServiceAsUser("WorkItemBoardcolumn-Service", l.testIdentity)
-	return svc, NewWorkitemController(svc, l.db, l.Configuration)
+func (l *TestWorkItemBoardcolumnREST) SecuredController(identity account.Identity) (*goa.Service, app.WorkitemController) {
+	svc := testsupport.ServiceAsUser("WorkItemBoardcolumn-Service", identity)
+	return svc, NewWorkitemController(svc, l.GormDB, l.Configuration)
 }
 
 func (l *TestWorkItemBoardcolumnREST) UnSecuredController() (*goa.Service, app.WorkitemController) {
 	svc := goa.New("WorkItemBoardcolumn-Service")
-	return svc, NewWorkitemController(svc, l.db, l.Configuration)
+	return svc, NewWorkitemController(svc, l.GormDB, l.Configuration)
 }
 
 func (l *TestWorkItemBoardcolumnREST) TestAddWItoBoardcolumn() {
 	wiCnt := 2
 	boardCnt := 2
-	fixtures := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
-	svc, ctrl := l.SecuredController()
+	fxt := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
+	svc, ctrl := l.SecuredController(*fxt.Identities[0])
 
 	// Fetch WI and verify boardcolumns Relationship
-	_, fetchedWI := test.ShowWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, nil, nil)
+	_, fetchedWI := test.ShowWorkitemOK(l.T(), svc.Context, svc, ctrl, fxt.WorkItems[0].ID, nil, nil)
 	require.NotNil(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns)
 	assert.Empty(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns.Data)
 
 	u := app.UpdateWorkitemPayload{
 		Data: &app.WorkItem{
-			ID:   &fixtures.WorkItems[0].ID,
+			ID:   &fxt.WorkItems[0].ID,
 			Type: APIStringTypeWorkItem,
 			Attributes: map[string]interface{}{
-				"version": fixtures.WorkItems[0].Version,
+				"version": fxt.WorkItems[0].Version,
 			},
 			Relationships: &app.WorkItemRelationships{},
 		},
@@ -87,16 +72,16 @@ func (l *TestWorkItemBoardcolumnREST) TestAddWItoBoardcolumn() {
 	u.Data.Relationships.SystemBoardcolumns = &app.RelationGenericList{
 		Data: []*app.GenericData{
 			{
-				ID:   ptr.String(fixtures.WorkItemBoards[0].Columns[0].ID.String()),
+				ID:   ptr.String(fxt.WorkItemBoards[0].Columns[0].ID.String()),
 				Type: ptr.String("boardcolumns"),
 			},
 			{
-				ID:   ptr.String(fixtures.WorkItemBoards[1].Columns[1].ID.String()),
+				ID:   ptr.String(fxt.WorkItemBoards[1].Columns[1].ID.String()),
 				Type: ptr.String("boardcolumns"),
 			},
 		},
 	}
-	_, updatedWI := test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, &u)
+	_, updatedWI := test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fxt.WorkItems[0].ID, &u)
 	assert.NotNil(l.T(), updatedWI)
 	assert.Len(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data, 2)
 	mustHave := map[string]struct{}{
@@ -112,127 +97,142 @@ func (l *TestWorkItemBoardcolumnREST) TestAddWItoBoardcolumn() {
 func (l *TestWorkItemBoardcolumnREST) TestAddWItoDistinctBoardcolumn() {
 	wiCnt := 2
 	boardCnt := 2
-	fixtures := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
-	svc, ctrl := l.SecuredController()
+	fxt := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
+	svc, ctrl := l.SecuredController(*fxt.Identities[0])
 
-	// Fetch WI and verify boardcolumns Relationship
-	_, fetchedWI := test.ShowWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, nil, nil)
-	require.NotNil(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns)
-	assert.Empty(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns.Data)
+	l.T().Run("Fetch WI and verify boardcolumns Relationship", func(t *testing.T) {
+		_, fetchedWI := test.ShowWorkitemOK(t, svc.Context, svc, ctrl, fxt.WorkItems[0].ID, nil, nil)
+		require.NotNil(t, fetchedWI.Data.Relationships.SystemBoardcolumns)
+		assert.Empty(t, fetchedWI.Data.Relationships.SystemBoardcolumns.Data)
+	})
 
-	u := app.UpdateWorkitemPayload{
-		Data: &app.WorkItem{
-			ID:   &fixtures.WorkItems[0].ID,
-			Type: APIStringTypeWorkItem,
-			Attributes: map[string]interface{}{
-				"version": fixtures.WorkItems[0].Version,
+	l.T().Run("add a column reference duplicate", func(*testing.T) {
+		// given
+		u := app.UpdateWorkitemPayload{
+			Data: &app.WorkItem{
+				ID:   &fxt.WorkItems[0].ID,
+				Type: APIStringTypeWorkItem,
+				Attributes: map[string]interface{}{
+					"version": fxt.WorkItems[0].Version,
+				},
+				Relationships: &app.WorkItemRelationships{
+					SystemBoardcolumns: &app.RelationGenericList{
+						Data: []*app.GenericData{
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[0].Columns[0].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[0].Columns[0].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[1].Columns[1].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[1].Columns[1].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+						},
+					},
+				},
 			},
-			Relationships: &app.WorkItemRelationships{},
-		},
-	}
-	// add a column reference duplicate
-	u.Data.Relationships.SystemBoardcolumns = &app.RelationGenericList{
-		Data: []*app.GenericData{
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[0].Columns[0].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[0].Columns[0].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[1].Columns[1].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[1].Columns[1].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-		},
-	}
-	_, updatedWI := test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, &u)
-	assert.NotNil(l.T(), updatedWI)
-	assert.Len(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data, 2)
-	mustHave := map[string]struct{}{
-		*u.Data.Relationships.SystemBoardcolumns.Data[0].ID: {},
-		*u.Data.Relationships.SystemBoardcolumns.Data[2].ID: {},
-	}
-	for _, lblData := range updatedWI.Data.Relationships.SystemBoardcolumns.Data {
-		delete(mustHave, *lblData.ID)
-	}
-	require.Empty(l.T(), mustHave)
+		}
+		// when
+		_, updatedWI := test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fxt.WorkItems[0].ID, &u)
+		// then
+		assert.NotNil(l.T(), updatedWI)
+		assert.Len(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data, 2)
+		mustHave := map[string]struct{}{
+			*u.Data.Relationships.SystemBoardcolumns.Data[0].ID: {},
+			*u.Data.Relationships.SystemBoardcolumns.Data[2].ID: {},
+		}
+		for _, lblData := range updatedWI.Data.Relationships.SystemBoardcolumns.Data {
+			delete(mustHave, *lblData.ID)
+		}
+		require.Empty(l.T(), mustHave)
+	})
 }
 
 func (l *TestWorkItemBoardcolumnREST) TestRemoveAllBoardcolumns() {
 	wiCnt := 2
 	boardCnt := 2
-	fixtures := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
-	svc, ctrl := l.SecuredController()
+	fxt := tf.NewTestFixture(l.T(), l.DB, tf.CreateWorkItemEnvironment(), tf.WorkItems(wiCnt), tf.WorkItemBoards(boardCnt))
+	svc, ctrl := l.SecuredController(*fxt.Identities[0])
 
-	// Fetch WI and verify boardcolumns Relationship
-	_, fetchedWI := test.ShowWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, nil, nil)
-	require.NotNil(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns)
-	assert.Empty(l.T(), fetchedWI.Data.Relationships.SystemBoardcolumns.Data)
+	l.T().Run("Fetch WI and verify boardcolumns Relationship", func(t *testing.T) {
+		_, fetchedWI := test.ShowWorkitemOK(t, svc.Context, svc, ctrl, fxt.WorkItems[0].ID, nil, nil)
+		require.NotNil(t, fetchedWI.Data.Relationships.SystemBoardcolumns)
+		assert.Empty(t, fetchedWI.Data.Relationships.SystemBoardcolumns.Data)
+	})
 
-	u := app.UpdateWorkitemPayload{
-		Data: &app.WorkItem{
-			ID:   &fixtures.WorkItems[0].ID,
-			Type: APIStringTypeWorkItem,
-			Attributes: map[string]interface{}{
-				"version": fixtures.WorkItems[0].Version,
+	l.T().Run("add a column reference", func(t *testing.T) {
+		// given
+		u := app.UpdateWorkitemPayload{
+			Data: &app.WorkItem{
+				ID:   &fxt.WorkItems[0].ID,
+				Type: APIStringTypeWorkItem,
+				Attributes: map[string]interface{}{
+					"version": fxt.WorkItems[0].Version,
+				},
+				Relationships: &app.WorkItemRelationships{
+					SystemBoardcolumns: &app.RelationGenericList{
+						Data: []*app.GenericData{
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[0].Columns[0].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+							{
+								ID:   ptr.String(fxt.WorkItemBoards[1].Columns[1].ID.String()),
+								Type: ptr.String("boardcolumns"),
+							},
+						},
+					},
+				},
 			},
-			Relationships: &app.WorkItemRelationships{},
-		},
-	}
-	// add a column reference
-	u.Data.Relationships.SystemBoardcolumns = &app.RelationGenericList{
-		Data: []*app.GenericData{
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[0].Columns[0].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-			{
-				ID:   ptr.String(fixtures.WorkItemBoards[1].Columns[1].ID.String()),
-				Type: ptr.String("boardcolumns"),
-			},
-		},
-	}
-	_, updatedWI := test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, &u)
-	assert.NotNil(l.T(), updatedWI)
-	assert.Len(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data, 2)
-	mustHave := map[string]struct{}{
-		*u.Data.Relationships.SystemBoardcolumns.Data[0].ID: {},
-		*u.Data.Relationships.SystemBoardcolumns.Data[1].ID: {},
-	}
-	for _, lblData := range updatedWI.Data.Relationships.SystemBoardcolumns.Data {
-		delete(mustHave, *lblData.ID)
-	}
-	require.Empty(l.T(), mustHave)
+		}
+		// when
+		_, updatedWI := test.UpdateWorkitemOK(t, svc.Context, svc, ctrl, fxt.WorkItems[0].ID, &u)
+		// then
+		assert.NotNil(t, updatedWI)
+		assert.Len(t, updatedWI.Data.Relationships.SystemBoardcolumns.Data, 2)
+		mustHave := map[string]struct{}{
+			*u.Data.Relationships.SystemBoardcolumns.Data[0].ID: {},
+			*u.Data.Relationships.SystemBoardcolumns.Data[1].ID: {},
+		}
+		for _, lblData := range updatedWI.Data.Relationships.SystemBoardcolumns.Data {
+			delete(mustHave, *lblData.ID)
+		}
+		require.Empty(t, mustHave)
 
-	// now remove all columns
-	u.Data.Attributes["version"] = updatedWI.Data.Attributes["version"]
-	u.Data.Relationships.SystemBoardcolumns = &app.RelationGenericList{
-		Data: []*app.GenericData{},
-	}
-	_, updatedWI = test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fixtures.WorkItems[0].ID, &u)
-	assert.NotNil(l.T(), updatedWI)
-	assert.Empty(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data)
-
+		t.Run("now remove all columns", func(t *testing.T) {
+			// given
+			u.Data.Attributes["version"] = updatedWI.Data.Attributes["version"]
+			u.Data.Relationships.SystemBoardcolumns = &app.RelationGenericList{
+				Data: []*app.GenericData{},
+			}
+			// when
+			_, updatedWI = test.UpdateWorkitemOK(l.T(), svc.Context, svc, ctrl, fxt.WorkItems[0].ID, &u)
+			// then
+			assert.NotNil(l.T(), updatedWI)
+			assert.Empty(l.T(), updatedWI.Data.Relationships.SystemBoardcolumns.Data)
+		})
+	})
 }
 
 /* FIXME(michaelkleinhenz): Add tests as soon as isValid is added to workitem.go
 func (l *TestWorkItemBoardcolumnREST) TestFailInvalidLabel() {
-	fixtures := tf.NewTestFixture(l.T(), l.DB, tf.Spaces(1), tf.Iterations(1), tf.Areas(1), tf.WorkItems(1))
+	fxt := tf.NewTestFixture(l.T(), l.DB, tf.Spaces(1), tf.Iterations(1), tf.Areas(1), tf.WorkItems(1))
 	svc, ctrl := l.SecuredController()
 	apiLabelType := label.APIStringTypeLabels
 	invalidLabelID := uuid.NewV4().String()
 	u := app.UpdateWorkitemPayload{
 		Data: &app.WorkItem{
-			ID:   &fixtures.WorkItems[0].ID,
+			ID:   &fxt.WorkItems[0].ID,
 			Type: APIStringTypeWorkItem,
 			Attributes: map[string]interface{}{
-				"version": fixtures.WorkItems[0].Version,
+				"version": fxt.WorkItems[0].Version,
 			},
 			Relationships: &app.WorkItemRelationships{
 				Labels: &app.RelationGenericList{
@@ -246,6 +246,6 @@ func (l *TestWorkItemBoardcolumnREST) TestFailInvalidLabel() {
 			},
 		},
 	}
-	test.UpdateWorkitemBadRequest(l.T(), svc.Context, svc, ctrl, fixtures.Spaces[0].ID, &u)
+	test.UpdateWorkitemBadRequest(l.T(), svc.Context, svc, ctrl, fxt.Spaces[0].ID, &u)
 }
 */
