f43e46b49f73fee4676e521d10b00cb2c30501d5
tinakurian <tina.t.s.kurian@gmail.com>
2018-07-16 07:58:55 -0400

    fixing example and quotes

--- design/root.go
+++ design/root.go
@@ -8,15 +8,11 @@ import (
 
 var root = a.Type("Root", func() {
 
-	a.Description(`JSONAPI store for the data of a Root. See also http://jsonapi.org/format/#document-resource-object`)
-	a.Attribute("relationships", a.HashOf(d.String, d.Any), "User context information of any type as a json", func() {
-		a.Example(map[string]interface{}{"codebases_che_state": "exmaple"})
-	})
+	a.Description("JSONAPI store for the data of a Root. See also http://jsonapi.org/format/#document-resource-object")
+	a.Attribute("relationships", a.HashOf(d.String, d.Any), "User context information of any type as a json")
 	a.Attribute("basePath", d.String)
 	a.Attribute("attributes", d.Any)
-	a.Attribute("id", d.UUID, "ID of root", func() {
-		a.Example("40bbdd3d-8b5d-4fd6-ac90-7236b669af04")
-	})
+	a.Attribute("id", d.UUID, "ID of root")
 	a.Attribute("links", genericLinksForRoot)
 
 })
