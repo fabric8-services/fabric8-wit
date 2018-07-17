6857c09dafd4935736d0244f05fb3ef17b3ca2ed
Konrad Kleine <kkleine@redhat.com>
2018-07-17 05:36:01 +0200

    Test that migration sends the correct signal

--- gormsupport/listener_test.go
+++ gormsupport/listener_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fabric8-services/fabric8-wit/gormsupport"
 	"github.com/fabric8-services/fabric8-wit/gormtestsupport"
+	"github.com/fabric8-services/fabric8-wit/migration"
 	"github.com/fabric8-services/fabric8-wit/resource"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -26,7 +27,7 @@ func (s *TestListenerSuite) TestSetupDatabaseListener() {
 		channelName := "f8_custom_event_channel"
 		payload := "some additional info about the event"
 		wg := sync.WaitGroup{}
-		wg.Add(1)
+		wg.Add(2)
 
 		gormsupport.SetupDatabaseListener(*s.Configuration, map[string]gormsupport.SubscriberFunc{
 			// This is the channel we send to from this test
@@ -39,17 +40,21 @@ func (s *TestListenerSuite) TestSetupDatabaseListener() {
 			// This is the channel that we send to from
 			// migration.PopulateCommonTypes() which is called by
 			// gormtestsupport.DBTestSuite internally.
-			// gormsupport.ChanSpaceTemplateUpdates: func(channel, extra string) {
-			// 	t.Logf("received notification on channel %s: %s", channel, extra)
-			// 	require.Equal(t, gormsupport.ChanSpaceTemplateUpdates, channel)
-			// 	require.Equal(t, "", extra)
-			// 	wg.Done()
-			// },
+			gormsupport.ChanSpaceTemplateUpdates: func(channel, extra string) {
+				t.Logf("received notification on channel %s: %s", channel, extra)
+				require.Equal(t, gormsupport.ChanSpaceTemplateUpdates, channel)
+				require.Equal(t, "", extra)
+				wg.Done()
+			},
 		})
 
 		// Send a notification from a completely different connection than the
 		// one we established to listen to channels.
 		s.DB.Debug().Exec("SELECT pg_notify($1, $2)", channelName, payload)
+
+		// This will send a notification on the
+		// gormsupport.ChanSpaceTemplateUpdates channel
+		migration.PopulateCommonTypes(nil, s.DB)
 
 		// wait until notification was received
 		wg.Wait()
