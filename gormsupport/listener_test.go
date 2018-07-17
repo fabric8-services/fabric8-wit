b227b9f3a81fe17894feb1c7b1bd4305f18bc8a9
Konrad Kleine <kkleine@redhat.com>
2018-07-17 06:02:52 +0200

    Fixup

--- gormsupport/listener_test.go
+++ gormsupport/listener_test.go
@@ -28,6 +28,7 @@ func (s *TestListenerSuite) TestSetupDatabaseListener() {
 		payload := "some additional info about the event"
 		wg := sync.WaitGroup{}
 		wg.Add(2)
+		var executedMigration bool
 
 		err := gormsupport.SetupDatabaseListener(*s.Configuration, map[string]gormsupport.SubscriberFunc{
 			// This is the channel we send to from this test
@@ -41,21 +42,28 @@ func (s *TestListenerSuite) TestSetupDatabaseListener() {
 			// migration.PopulateCommonTypes() which is called by
 			// gormtestsupport.DBTestSuite internally.
 			gormsupport.ChanSpaceTemplateUpdates: func(channel, extra string) {
-				t.Logf("received notification on channel %s: %s", channel, extra)
-				require.Equal(t, gormsupport.ChanSpaceTemplateUpdates, channel)
-				require.Equal(t, "", extra)
-				wg.Done()
+				// potentially the migration is executed twice but we're only
+				// interested in one event.
+				if !executedMigration {
+					executedMigration = true
+					t.Logf("received notification on channel %s: %s", channel, extra)
+					require.Equal(t, gormsupport.ChanSpaceTemplateUpdates, channel)
+					require.Equal(t, "", extra)
+					wg.Done()
+				}
 			},
 		})
 		require.NoError(t, err)
 
 		// Send a notification from a completely different connection than the
 		// one we established to listen to channels.
-		s.DB.Debug().Exec("SELECT pg_notify($1, $2)", channelName, payload)
+		db := s.DB.Debug().Exec("SELECT pg_notify($1, $2)", channelName, payload)
+		require.NoError(t, db.Error)
 
 		// This will send a notification on the
 		// gormsupport.ChanSpaceTemplateUpdates channel
-		migration.PopulateCommonTypes(nil, s.DB)
+		err = migration.PopulateCommonTypes(nil, s.DB)
+		require.NoError(t, err)
 
 		// wait until notification was received
 		wg.Wait()
