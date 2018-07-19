5ae863dc21c7f802f6abf506bc6ef0a9198d328b
Konrad Kleine <kkleine@redhat.com>
2018-07-19 07:19:12 +0200

    Adapt endpoints controller
    * reverted 8f3ecdb74b1958468a00cc2d7123f36ab1f1070f
    * use jsonapi.FormatMemberName
    * adapt to new endpoints design with no optional fields
    * fix comments to begin with function names (see
    https://blog.golang.org/godoc-documenting-go-code)

--- controller/root.go
+++ controller/root.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"encoding/json"
+	"strings"
+
 	"github.com/fabric8-services/fabric8-wit/app"
 	"github.com/fabric8-services/fabric8-wit/errors"
 	"github.com/fabric8-services/fabric8-wit/jsonapi"
@@ -12,7 +14,6 @@ import (
 	"github.com/goadesign/goa"
 	errs "github.com/pkg/errors"
 	"github.com/satori/go.uuid"
-	"strings"
 )
 
 const embeddedSwaggerSpecFile = "swagger.json"
@@ -27,15 +28,17 @@ type RootController struct {
 	FileHandler asseter
 }
 
+type workingFileFetcher struct{}
+
+func (s workingFileFetcher) Asset(fileName string) ([]byte, error) {
+	return swagger.Asset(fileName)
+}
+
+var _ asseter = workingFileFetcher{}
+var _ asseter = (*workingFileFetcher)(nil)
+
 // NewRootController creates a root controller.
 func NewRootController(service *goa.Service) *RootController {
-	type workingFileFetcher struct{}	
-	func (s workingFileFetcher) Asset(fileName string) ([]byte, error) {
-		return swagger.Asset(fileName)
-	}	
-	var _ asseter = workingFileFetcher{}
-	var _ asseter = (*workingFileFetcher)(nil)
-	
 	return &RootController{
 		Controller:  service.NewController("RootController"),
 		FileHandler: workingFileFetcher{},
@@ -66,15 +69,13 @@ func getRoot(ctx *app.ListRootContext, fileHandler asseter) (*app.Root, error) {
 		return nil, err
 	}
 
+	// the path map stores paths as key and URLs as values
 	namedPaths := make(map[string]interface{})
 	for path, swaggerPath := range swaggerPaths {
 
 		// Currently not supporting endpoints that contain parameters.
 		if !strings.Contains(path, "{") {
-
-			// Use the segments in the path to construct a meaningful name to use for the path.
-			key := strings.Replace(path, "/", "_", -1)
-			key = strings.Replace(key, "_", "", 1)
+			key := path
 
 			pathsObj, ok := swaggerPath.(map[string]interface{})
 			if !ok {
@@ -91,6 +92,9 @@ func getRoot(ctx *app.ListRootContext, fileHandler asseter) (*app.Root, error) {
 				key = xtag
 			}
 
+			// cleanup the key to conform to JSONAPI member names
+			key = jsonapi.FormatMemberName(key)
+
 			// Set the related field and link objects for each name.
 			namedPaths[key] = map[string]interface{}{
 				"links": map[string]string{
@@ -105,18 +109,20 @@ func getRoot(ctx *app.ListRootContext, fileHandler asseter) (*app.Root, error) {
 		return nil, err
 	}
 
-	basePath = rest.AbsoluteURL(ctx.Request, basePath)
-	links := &app.GenericLinksForRoot{
-		Self: &basePath,
-	}
-	types := "endpoints"
-	return &app.Root{Relationships: namedPaths, ID: ptr.UUID(uuid.NewV4()), Links: links, Type: ptr.String(types)}, nil
+	return &app.Root{
+		Type:          "endpoints",
+		ID:            uuid.NewV4(),
+		Relationships: namedPaths,
+		Links: app.GenericLinksForRoot{
+			Self: ptr.String(rest.AbsoluteURL(ctx.Request, basePath)),
+		},
+	}, nil
 }
 
-// Gets the swagger specification binary and attempts to unmarshal it.
-// Returns the unmarshed specification, or error. Error is returned if
-// the specification could not be found, or the specification was not
-// able to be unmarshalled.
+// getUnmarshalledSwagger gets the swagger specification binary and attempts to
+// unmarshal it. Returns the unmarshed specification, or error. Error is
+// returned if the specification could not be found, or the specification was
+// not able to be unmarshalled.
 func getUnmarshalledSwagger(ctx *app.ListRootContext, fileHandler asseter) (map[string]interface{}, error) {
 	swaggerJSON, err := fileHandler.Asset(embeddedSwaggerSpecFile)
 	if err != nil {
@@ -139,9 +145,9 @@ func getUnmarshalledSwagger(ctx *app.ListRootContext, fileHandler asseter) (map[
 	return result, nil
 }
 
-// Gets a field from te swagger specification and asserts the type to
-// string. Returns errors if the key does not exist in the swagger
-// specification, or the field cannot be type asserted to a string.
+// getSwaggerFieldAsString gets a field from te swagger specification and
+// asserts the type to string. Returns errors if the key does not exist in the
+// swagger specification, or the field cannot be type asserted to a string.
 func getSwaggerFieldAsString(ctx *app.ListRootContext, field string, json map[string]interface{}, isXtag bool) (string, error) {
 	value, ok := json[field]
 	if !ok {
