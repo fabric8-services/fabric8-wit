5136927a09d3381cae2978ba7b90ae1267c13e4d
Elliott Baron <ebaron@redhat.com>
2018-07-27 17:25:00 -0400

    Implement authz check for DeleteDeployment

--- kubernetes/deployments_access_blackbox_test.go
+++ kubernetes/deployments_access_blackbox_test.go
@@ -7,6 +7,47 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestCanDeleteDeployment(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		envName        string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			envName:        "run",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanDeleteDeployment(testCase.envName)
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
 func TestCanGetEnvironments(t *testing.T) {
 	testCases := []struct {
 		testName       string
