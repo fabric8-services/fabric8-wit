a99ccb52e393dc9b715aae41758005c2b784b553
Elliott Baron <ebaron@redhat.com>
2018-07-31 18:29:01 -0400

    Implement remaining methods, add tests for access control failure on builds

--- kubernetes/deployments_access_blackbox_test.go
+++ kubernetes/deployments_access_blackbox_test.go
@@ -7,6 +7,188 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestCanGetSpace(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanGetSpace()
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
+func TestCanGetApplication(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanGetApplication()
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
+func TestCanGetDeployment(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		envName        string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			envName:        "run",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			envName:        "run",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanGetDeployment(testCase.envName)
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
+func TestCanScaleDeployment(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		envName        string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			envName:        "run",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			envName:        "run",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanScaleDeployment(testCase.envName)
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
 func TestCanDeleteDeployment(t *testing.T) {
 	testCases := []struct {
 		testName       string
@@ -21,6 +203,12 @@ func TestCanDeleteDeployment(t *testing.T) {
 			envName:        "run",
 			cassetteName:   "can-i",
 			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			envName:        "run",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
 		},
 	}
 
@@ -73,6 +261,100 @@ func TestCanGetEnvironments(t *testing.T) {
 			kc := getDefaultKubeClient(fixture, r.Transport, t)
 
 			result, err := kc.CanGetEnvironments()
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
+func TestCanGetDeploymentStats(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		envName        string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			envName:        "run",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			envName:        "run",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanGetDeploymentStats(testCase.envName)
+			if testCase.shouldFail {
+				require.Error(t, err, "Expected an error")
+				if testCase.errorChecker != nil {
+					matches, _ := testCase.errorChecker(err)
+					require.True(t, matches, "Error or cause must be the expected type")
+				}
+			} else {
+				require.NoError(t, err, "Unexpected error occurred")
+				require.Equal(t, testCase.expectedResult, result, "Expected different authorization result")
+			}
+		})
+	}
+}
+
+func TestCanGetDeploymentStatSeries(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		cassetteName   string
+		envName        string
+		expectedResult bool
+		shouldFail     bool
+		errorChecker   func(error) (bool, error)
+	}{
+		{
+			testName:       "Basic",
+			envName:        "run",
+			cassetteName:   "can-i",
+			expectedResult: true,
+		},
+		{
+			testName:       "No Builds",
+			envName:        "run",
+			cassetteName:   "can-i-no-builds",
+			expectedResult: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			r, err := recorder.New(pathToTestJSON + testCase.cassetteName)
+			require.NoError(t, err, "Failed to open cassette")
+			defer r.Stop()
+
+			fixture := &testFixture{}
+			kc := getDefaultKubeClient(fixture, r.Transport, t)
+
+			result, err := kc.CanGetDeploymentStatSeries(testCase.envName)
 			if testCase.shouldFail {
 				require.Error(t, err, "Expected an error")
 				if testCase.errorChecker != nil {
