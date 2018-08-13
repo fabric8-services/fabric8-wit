07a9a1568a777dfb13c8c86b0263d34fd2c08843
tinakurian <tkurian@redhat.com>
2018-08-12 23:40:32 -0400

    fix data

---
+++ migration/sql-test-files/101-insert-test-root-iteration.sql
@@ -0,0 +1,8 @@
+
+-- create space template
+INSERT INTO space_templates (id,name,description) VALUES('0961ada9-afca-439f-865b-d5a8c6e3e9a2', 'test space template 2', 'test template');
+
+-- create space
+insert into spaces (id,name,space_template_id) values ('652f8798-ef89-49ac-81f9-4bb937a5d175', 'test space 2', '0961ada9-afca-439f-865b-d5a8c6e3e9a2');
+
+insert into iterations (id, name, path, space_id) values ('abd93233-75c9-4419-a2e8-3c328736c443', 'test space 3', '', '652f8798-ef89-49ac-81f9-4bb937a5d175');
