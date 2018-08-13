9741ac7431680f142bf217a9e8e8784482af7005
tinakurian <tkurian@redhat.com>
2018-08-13 00:01:41 -0400

    update comments

--- migration/sql-files/102-update-root-area-and-iteration-path-field.sql
+++ migration/sql-files/102-update-root-area-and-iteration-path-field.sql
@@ -1,5 +1,7 @@
+-- update root iteration and area paths to use converted ids
 UPDATE iterations SET path=text2ltree(replace(cast(id as text), '-', '_')) WHERE path='' OR PATH IS NULL;
 UPDATE areas SET path=text2ltree(replace(cast(id as text), '-', '_')) WHERE path='' OR PATH IS NULL;
 
+-- alter iteration and area path column to not accept NULL values
 ALTER TABLE iterations ALTER COLUMN path SET NOT NULL;
 ALTER TABLE areas ALTER COLUMN path SET NOT NULL;
