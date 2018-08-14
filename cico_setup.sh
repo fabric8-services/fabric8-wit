4d805dbce2deb646412d8e0ab644325318fcac35
Konrad Kleine <193408+kwk@users.noreply.github.com>
2018-08-14 11:28:08 +0200

    Make sure e2e tests are cleaned always (even on error)

--- cico_setup.sh
+++ cico_setup.sh
@@ -84,8 +84,8 @@ function run_tests_without_coverage() {
 }
 
 function run_e2e_tests() {
-  make docker-test-e2e
-  make docker-clean-e2e
+  trap "make docker-clean-e2e" EXIT
+  make docker-test-e2e  
   echo "CICO: ran e2e-tests"
 }
 
