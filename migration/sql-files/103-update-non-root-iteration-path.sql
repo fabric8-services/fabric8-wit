2b2e446f1e40ce969cf259ab90d36f60b2785d85
tinakurian <tkurian@redhat.com>
2018-08-13 21:31:14 -0400

    adding files for migration

---
+++ migration/sql-files/103-update-non-root-iteration-path.sql
@@ -0,0 +1,3 @@
+-- update root iteration and area paths to use converted ids
+UPDATE iterations SET path=text2ltree(concat(path, concat('_',replace(cast(id as text), '-', '_')))) WHERE path!='' OR NOT NULL; 
+UPDATE areas SET path=text2ltree(concat(path, concat('_',replace(cast(id as text), '-', '_')))) WHERE path!='' OR NOT NULL; 
