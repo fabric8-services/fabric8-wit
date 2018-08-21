69ce1ac6f94d02f2497de12d9d3086cc0be78a3e
Konrad Kleine <193408+kwk@users.noreply.github.com>
2018-08-21 12:44:24 +0200

    Default value (#2247)
    ## Defaults for fields
    
    This change introduces the ability for any field type to OPTIONALLY specify a custom default value. See also https://openshift.io/openshiftio/Openshift_io/plan/detail/35 and https://github.com/openshiftio/openshift.io/issues/3832.
    
    For lists and simple types, the only restriction is that the default value is of the same kind as the list or simple type (e.g. integer, float, string, user, label, ...). For enum fields the custom default value needs to be in the list of allowed values.
    
    ### Example usage of the feature
    
    As an example, we've set the default for severity and priority fields to a medium value instead of the first on in the list, which would be a high severity/priority. (See also https://github.com/openshiftio/openshift.io/issues/3832)
    
    ## Improvements to tests
    
    ### Space template import
    When a space template is imported it creates or overrides the work item types. With this change I've introduced a cascading validation that checks 
    
    1. the work item types, 
    2. the fields inside each work item type
    3. the field type of each field.
    
    This ensures that people don't accidentally specify a float default (e.g. `33.45`) for an integer field.
    
    Before this change, most of the input and output validation happened at runtime when a value was converted into another representation. Now, the validation happens at import time and also when a default value for a field is calculated.

--- workitem/enum_type_blackbox_test.go
+++ workitem/enum_type_blackbox_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/fabric8-services/fabric8-wit/convert"
 	"github.com/fabric8-services/fabric8-wit/resource"
-	"github.com/fabric8-services/fabric8-wit/workitem"
+	w "github.com/fabric8-services/fabric8-wit/workitem"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,26 +13,32 @@ func TestEnumType_Equal(t *testing.T) {
 	t.Parallel()
 	resource.Require(t, resource.UnitTest)
 
-	a := workitem.EnumType{
-		SimpleType:       workitem.SimpleType{Kind: workitem.KindEnum},
-		BaseType:         workitem.SimpleType{Kind: workitem.KindString},
+	a := w.EnumType{
+		SimpleType:       w.SimpleType{Kind: w.KindEnum},
+		BaseType:         w.SimpleType{Kind: w.KindString},
 		Values:           []interface{}{"foo", "bar"},
 		RewritableValues: false,
+		DefaultValue:     "fooooooobar",
 	}
-
 	t.Run("type inequality", func(t *testing.T) {
 		require.False(t, a.Equal(convert.DummyEqualer{}))
 	})
 
 	t.Run("simple type difference", func(t *testing.T) {
 		b := a
-		b.SimpleType = workitem.SimpleType{Kind: workitem.KindArea}
+		b.SimpleType = w.SimpleType{Kind: w.KindArea}
 		require.False(t, a.Equal(b))
 	})
 
 	t.Run("base type difference", func(t *testing.T) {
 		b := a
-		b.BaseType = workitem.SimpleType{Kind: workitem.KindInteger}
+		b.BaseType = w.SimpleType{Kind: w.KindInteger}
+		require.False(t, a.Equal(b))
+	})
+
+	t.Run("default value difference", func(t *testing.T) {
+		b := a
+		b.DefaultValue = "foo"
 		require.False(t, a.Equal(b))
 	})
 
@@ -68,26 +74,218 @@ func TestEnumType_Equal(t *testing.T) {
 	})
 }
 
+func TestEnumType_GetDefaultValue(t *testing.T) {
+	t.Parallel()
+	resource.Require(t, resource.UnitTest)
+
+	tests := []struct {
+		name           string
+		enum           w.EnumType
+		expectedOutput interface{}
+	}{
+		{"return first value of enum when default is nil", w.EnumType{
+			SimpleType: w.SimpleType{Kind: w.KindEnum},
+			BaseType:   w.SimpleType{Kind: w.KindString},
+			Values:     []interface{}{"first", "second", "third"},
+		}, "first"},
+		{"return custom default when a default is set", w.EnumType{
+			SimpleType:   w.SimpleType{Kind: w.KindEnum},
+			BaseType:     w.SimpleType{Kind: w.KindInteger},
+			Values:       []interface{}{111, 222, 333},
+			DefaultValue: 222,
+		}, 222},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tt.expectedOutput, tt.enum.GetDefaultValue())
+		})
+	}
+}
+
+func TestEnumType_SetDefaultValue(t *testing.T) {
+	t.Parallel()
+	resource.Require(t, resource.UnitTest)
+
+	tests := []struct {
+		name           string
+		enum           w.EnumType
+		defVal         interface{}
+		expectedOutput w.FieldType
+		wantErr        bool
+	}{
+		{"set default to allowed value",
+			w.EnumType{
+				SimpleType: w.SimpleType{Kind: w.KindEnum},
+				BaseType:   w.SimpleType{Kind: w.KindString},
+				Values:     []interface{}{"first", "second", "third"},
+			},
+			"second",
+			&w.EnumType{
+				SimpleType:   w.SimpleType{Kind: w.KindEnum},
+				BaseType:     w.SimpleType{Kind: w.KindString},
+				Values:       []interface{}{"first", "second", "third"},
+				DefaultValue: "second",
+			},
+			false},
+		{"set default to nil value",
+			w.EnumType{
+				SimpleType: w.SimpleType{Kind: w.KindEnum},
+				BaseType:   w.SimpleType{Kind: w.KindString},
+				Values:     []interface{}{"first", "second", "third"},
+			},
+			nil,
+			&w.EnumType{
+				SimpleType:   w.SimpleType{Kind: w.KindEnum},
+				BaseType:     w.SimpleType{Kind: w.KindString},
+				Values:       []interface{}{"first", "second", "third"},
+				DefaultValue: nil,
+			},
+			false},
+		{"set default to not-allowed value (wrong base type)",
+			w.EnumType{
+				SimpleType: w.SimpleType{Kind: w.KindEnum},
+				BaseType:   w.SimpleType{Kind: w.KindString},
+				Values:     []interface{}{"first", "second", "third"},
+			},
+			123,
+			nil,
+			true},
+		{"set default to not-allowed value (not in list)",
+			w.EnumType{
+				SimpleType: w.SimpleType{Kind: w.KindEnum},
+				BaseType:   w.SimpleType{Kind: w.KindString},
+				Values:     []interface{}{"first", "second", "third"},
+			},
+			"foobar",
+			nil,
+			true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			output, err := tt.enum.SetDefaultValue(tt.defVal)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectedOutput, output)
+			}
+		})
+	}
+}
+
+func TestEnumType_Validate(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		obj     w.EnumType
+		wantErr bool
+	}{
+		{"ok", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindString},
+			Values:           []interface{}{"who", "let", "the", "dogs", "out"},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, false},
+		{"error - empty values", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindString},
+			Values:           []interface{}{},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"error - nil values", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindString},
+			Values:           nil,
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid type", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindString},
+			BaseType:         w.SimpleType{Kind: w.KindString},
+			Values:           []interface{}{"who", "let", "the", "dogs", "out"},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid base type (list)", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindList},
+			Values:           []interface{}{"who", "let", "the", "dogs", "out"},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid base type (enum)", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindEnum},
+			Values:           []interface{}{"who", "let", "the", "dogs", "out"},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid string values", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindString},
+			Values:           []interface{}{"who", 1, "the", "dogs", "out"},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid integer values", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindInteger},
+			Values:           []interface{}{1, 2, "the", 4, 5},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid default value (wrong type)", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindInteger},
+			Values:           []interface{}{1, 2, 3, 4, 5},
+			RewritableValues: false,
+			DefaultValue:     "the",
+		}, true},
+		{"invalid default value (not in allowed values)", w.EnumType{
+			SimpleType:       w.SimpleType{Kind: w.KindEnum},
+			BaseType:         w.SimpleType{Kind: w.KindInteger},
+			Values:           []interface{}{1, 2, 3, 4, 5},
+			RewritableValues: false,
+			DefaultValue:     42,
+		}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			err := tt.obj.Validate()
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
 func TestEnumType_EqualEnclosing(t *testing.T) {
 	t.Parallel()
 	resource.Require(t, resource.UnitTest)
 
-	a := workitem.EnumType{
-		SimpleType:       workitem.SimpleType{Kind: workitem.KindEnum},
-		BaseType:         workitem.SimpleType{Kind: workitem.KindString},
+	a := w.EnumType{
+		SimpleType:       w.SimpleType{Kind: w.KindEnum},
+		BaseType:         w.SimpleType{Kind: w.KindString},
 		Values:           []interface{}{"foo", "bar", "baz"},
 		RewritableValues: false,
 	}
 
 	t.Run("simple type difference", func(t *testing.T) {
 		b := a
-		b.SimpleType = workitem.SimpleType{Kind: workitem.KindArea}
+		b.SimpleType = w.SimpleType{Kind: w.KindArea}
 		require.False(t, a.EqualEnclosing(b))
 	})
 
 	t.Run("base type difference", func(t *testing.T) {
 		b := a
-		b.BaseType = workitem.SimpleType{Kind: workitem.KindInteger}
+		b.BaseType = w.SimpleType{Kind: w.KindInteger}
 		require.False(t, a.EqualEnclosing(b))
 	})
 
