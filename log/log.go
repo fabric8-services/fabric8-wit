daaa4861b28c92d1b109ab2a0fa18c828f04a786
Konrad Kleine <193408+kwk@users.noreply.github.com>
2018-08-24 12:38:29 +0200

    removed log.NewCustomizedLogger because not used anywhere (#2257)

--- log/log.go
+++ log/log.go
@@ -55,43 +55,6 @@ func InitializeLogger(logJSON bool, lvl string) {
 	logger.Out = os.Stdout
 }
 
-// NewCustomizedLogger creates a custom logger specifying the desired log level
-// and the log format flag. Returns the logger object and the error.
-func NewCustomizedLogger(level string, logJSON bool) (*log.Logger, error) {
-	logger := log.New()
-
-	lv, err := log.ParseLevel(level)
-	if err != nil {
-		return nil, err
-	}
-	logger.Level = lv
-
-	if logJSON {
-		customFormatter := new(log.JSONFormatter)
-		customFormatter.TimestampFormat = "2006-01-02 15:04:05"
-
-		log.SetFormatter(customFormatter)
-		customFormatter.DisableTimestamp = false
-
-		log.SetLevel(log.InfoLevel)
-		logger.Level = lv
-		logger.Formatter = customFormatter
-	} else {
-		customFormatter := new(log.TextFormatter)
-		customFormatter.FullTimestamp = true
-		customFormatter.TimestampFormat = "2006-01-02 15:04:05"
-		log.SetFormatter(customFormatter)
-
-		log.SetLevel(log.DebugLevel)
-		logger.Level = lv
-		logger.Formatter = customFormatter
-	}
-
-	logger.Out = os.Stdout
-
-	return logger, nil
-}
-
 // Logger returns the current logger object.
 func Logger() *log.Logger {
 	return logger
