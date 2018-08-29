eb866af76b464cc1d71b1bcaa7ce8a1ad0ec8ef2
Konrad Kleine <kkleine@redhat.com>
2018-08-29 14:09:41 +0200

    Add test for BeforeUpdate and BeforeCreate

--- gormsupport/versioning.go
+++ gormsupport/versioning.go
@@ -6,16 +6,26 @@ import (
 )
 
 // Versioning can be embedded into model structs that want to have a Version
-// column which will automatically be incremented before each update.
+// column which will automatically be incremented before each update. For the
+// first creation of a model the initial version will always be overwritten with
+// 0 nomatter what the user specified in the model itself. The model itself is
+// not changed in any cases, just the DB query for INSERT and UPDATE is touched.
 type Versioning struct {
 	Version int `json:"version"`
 }
 
-// BeforeSave will is a GORM callback (see http://doc.gorm.io/callbacks.html)
-// that will be called before updating the model. We use it to automatically
+// BeforeUpdate is a GORM callback (see http://doc.gorm.io/callbacks.html) that
+// will be called before updating the model. We use it to automatically
 // increment the version number before saving the model.
-func (v *Versioning) BeforeSave(scope *gorm.Scope) (err error) {
+func (v *Versioning) BeforeUpdate(scope *gorm.Scope) (err error) {
 	return scope.SetColumn("version", v.Version+1)
+}
+
+// BeforeCreate is a GORM callback (see http://doc.gorm.io/callbacks.html) that
+// will be called before creating the model. We use it to automatically
+// have the first version of the model set to 0.
+func (v *Versioning) BeforeCreate(scope *gorm.Scope) (err error) {
+	return scope.SetColumn("version", 0)
 }
 
 // Ensure Versioning implements the Equaler interface
--- gormsupport/versioning_blackbox_test.go
+++ gormsupport/versioning_blackbox_test.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/fabric8-services/fabric8-wit/convert"
 	"github.com/fabric8-services/fabric8-wit/gormsupport"
+	"github.com/fabric8-services/fabric8-wit/gormtestsupport"
 	"github.com/fabric8-services/fabric8-wit/resource"
+	"github.com/fabric8-services/fabric8-wit/workitem/link"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestVersioning_Equal(t *testing.T) {
@@ -32,5 +35,43 @@ func TestVersioning_Equal(t *testing.T) {
 			Version: 123,
 		}
 		require.False(t, a.Equal(b))
+	})
+}
+
+type VersioningSuite struct {
+	gormtestsupport.DBTestSuite
+}
+
+func TestVersioning(t *testing.T) {
+	suite.Run(t, &VersioningSuite{DBTestSuite: gormtestsupport.NewDBTestSuite()})
+}
+
+func (s *VersioningSuite) TestCallbacks() {
+	// given a work item link category that embeds the Versioning struct is a
+	// good way to demonstrate the way the callbacks work.
+	cat := link.WorkItemLinkCategory{
+		Name: "foo",
+	}
+	// set the version to something else than 0 the BeforeCreate callback will
+	// automatically set this to 0 before entering it in the DB.
+	cat.Version = 123
+
+	s.T().Run("before create", func(t *testing.T) {
+		// when
+		err := s.DB.Create(&cat).Error
+		// then
+		require.NoError(t, err)
+		require.Equal(t, 0, cat.Version, "initial version of entity must be 0 nomatter what the given version was")
+	})
+	s.T().Run("before update", func(t *testing.T) {
+		// given
+		newName := "new name"
+		cat.Name = newName
+		// when
+		err := s.DB.Save(&cat).Error
+		// then
+		require.NoError(t, err)
+		require.Equal(t, 1, cat.Version, "followup version of entity must be 1")
+		require.Equal(t, newName, cat.Name)
 	})
 }
