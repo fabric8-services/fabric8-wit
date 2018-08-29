26e27af6d84cbcb0c6a669ab6667c928240612c5
Konrad Kleine <kkleine@redhat.com>
2018-08-29 14:45:20 +0200

    fixed TestWorkItemLinkCategory_ConvertLinkCategoryFromModel

--- controller/work_item_link_category_whitebox_test.go
+++ controller/work_item_link_category_whitebox_test.go
@@ -19,7 +19,6 @@ func TestWorkItemLinkCategory_ConvertLinkCategoryFromModel(t *testing.T) {
 		ID:          uuid.FromStringOrNil("0e671e36-871b-43a6-9166-0c4bd573e231"),
 		Name:        "Example work item link category",
 		Description: &description,
-		Version:     0,
 	}
 
 	expected := app.WorkItemLinkCategorySingle{
