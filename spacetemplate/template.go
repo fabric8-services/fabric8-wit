ff4d2881f7d127edc683a395615f121796dcd43b
Konrad Kleine <kkleine@redhat.com>
2018-09-11 12:23:50 +0200

    Don't use gormsupport.Versioning in space templates

--- spacetemplate/template.go
+++ spacetemplate/template.go
@@ -23,11 +23,11 @@ var (
 // ImportHelper to learn more about how we import space templates using YAML.
 type SpaceTemplate struct {
 	gormsupport.Lifecycle `json:"lifecycle"`
-	gormsupport.Versioning
-	ID           uuid.UUID `sql:"type:uuid default uuid_generate_v4()" gorm:"primary_key" json:"id"`
-	Name         string    `json:"name"`
-	Description  *string   `json:"description,omitempty"`
-	CanConstruct bool      `gorm:"can_construct" json:"can_construct"`
+	ID                    uuid.UUID `sql:"type:uuid default uuid_generate_v4()" gorm:"primary_key" json:"id"`
+	Version               int       `json:"version"`
+	Name                  string    `json:"name"`
+	Description           *string   `json:"description,omitempty"`
+	CanConstruct          bool      `gorm:"can_construct" json:"can_construct"`
 }
 
 // Validate ensures that all inner-document references of the given space
@@ -76,7 +76,7 @@ func (s SpaceTemplate) Equal(u convert.Equaler) bool {
 	if s.Name != other.Name {
 		return false
 	}
-	if !s.Versioning.Equal(other.Versioning) {
+	if s.Version != other.Version {
 		return false
 	}
 	if s.CanConstruct != other.CanConstruct {
