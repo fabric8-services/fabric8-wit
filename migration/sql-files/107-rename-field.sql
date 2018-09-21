823610c5c1ca0f579232834cb46396dfe43c103b
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2018-09-21 17:00:28 +0530

    rename workitemtype jsonb field keys

--- migration/sql-files/107-rename-field.sql
+++ migration/sql-files/107-rename-field.sql
@@ -2,6 +2,12 @@ CREATE OR REPLACE FUNCTION rename_wi_fields() RETURNS void as $$
     DECLARE
         k text;
     BEGIN
+		-- update json field keys of work_item_types
+                FOR k in SELECT jsonb_object_keys(Fields) FROM work_item_types LOOP
+			UPDATE work_item_types SET fields = fields - k || jsonb_build_object(regexp_replace(k, '\.', '_'), fields->k); 
+		END LOOP;
+
+		-- update json field keys of work_items
                 FOR k in SELECT jsonb_object_keys(Fields) FROM work_items LOOP
 			UPDATE work_items SET fields = fields - k || jsonb_build_object(regexp_replace(k, '\.', '_'), fields->k); 
 		END LOOP;
