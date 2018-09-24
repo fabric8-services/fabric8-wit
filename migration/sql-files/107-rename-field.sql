83cc0e8f3dd46e236c657fa855a3e66bf83fb609
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2018-09-25 02:10:29 +0530

    update the migration file

--- migration/sql-files/107-rename-field.sql
+++ migration/sql-files/107-rename-field.sql
@@ -1,20 +1,16 @@
-CREATE OR REPLACE FUNCTION rename_wi_fields() RETURNS void as $$
-    DECLARE
-        k text;
-    BEGIN
+DO 
+	$do$
+	declare k text;
+	BEGIN
 		-- update json field keys of work_item_types
                 FOR k in SELECT jsonb_object_keys(Fields) FROM work_item_types LOOP
-			UPDATE work_item_types SET fields = fields - k || jsonb_build_object(regexp_replace(k, '\.', '_'), fields->k); 
+			UPDATE work_item_types SET fields = replace(fields::TEXT, k, regexp_replace(k, '\.', '_'))::jsonb; 
 		END LOOP;
 
 		-- update json field keys of work_items
                 FOR k in SELECT jsonb_object_keys(Fields) FROM work_items LOOP
-			UPDATE work_items SET fields = fields - k || jsonb_build_object(regexp_replace(k, '\.', '_'), fields->k); 
+			UPDATE work_items SET fields = replace(fields::TEXT, k, regexp_replace(k, '\.', '_'))::jsonb; 
 		END LOOP;
-END $$ LANGUAGE plpgsql;
-
-DO $$ BEGIN
-    PERFORM rename_wi_fields();
-    DROP FUNCTION rename_wi_fields();
-END $$;
+	END
+	$do$;
 
