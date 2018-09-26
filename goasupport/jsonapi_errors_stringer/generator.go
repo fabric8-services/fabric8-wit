c30e173b5059cadb64f2d856ac92069dc8839174
Konrad Kleine <kkleine@redhat.com>
2018-09-26 12:15:03 +0200

    errcheck: Error return value of set.Parse is not checked

--- goasupport/jsonapi_errors_stringer/generator.go
+++ goasupport/jsonapi_errors_stringer/generator.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/goadesign/goa/design"
 	"github.com/goadesign/goa/goagen/codegen"
+	errs "github.com/pkg/errors"
 )
 
 // Generate adds method to support conditional queries
@@ -20,7 +21,9 @@ func Generate() ([]string, error) {
 	set.String("design", "", "") // Consume design argument so Parse doesn't complain
 	set.StringVar(&ver, "version", "", "")
 	set.StringVar(&outDir, "out", "", "")
-	set.Parse(os.Args[2:])
+	if err := set.Parse(os.Args[2:]); err != nil {
+		return nil, errs.WithStack(err)
+	}
 	// First check compatibility
 	if err := codegen.CheckVersion(ver); err != nil {
 		return nil, err
