b996cf63ff8f0f79ab59e37d36844144411738f5
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2018-10-23 12:22:36 +0530

    update error response

--- controller/workitem.go
+++ controller/workitem.go
@@ -244,7 +244,7 @@ func (c *WorkitemController) Delete(ctx *app.DeleteWorkitemContext) error {
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		wi, err = appl.WorkItems().LoadByID(ctx, ctx.WiID)
 		if err != nil {
-			return jsonapi.JSONErrorResponse(ctx, errs.Wrap("failed to delete workitem", err))
+			return errs.Wrap(err, fmt.Sprintf("Fail to load work item with id %v", ctx.WiID))
 		}
 		return nil
 	})
