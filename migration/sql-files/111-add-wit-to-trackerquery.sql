97f22475fc28565ee1a7538885c44c83953941cf
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2018-12-04 16:12:34 +0530

    deletes old data from tracker_queries; adds wit column in tracker_queries table

--- migration/sql-files/111-add-wit-to-trackerquery.sql
+++ migration/sql-files/111-add-wit-to-trackerquery.sql
@@ -1,26 +1,2 @@
-CREATE OR REPLACE FUNCTION adds_wit() RETURNS void as $$
--- adds_wit() function adds work_item_type to existing tracker_queries in database
-	DECLARE 
-		r RECORD;
-		c CURSOR FOR SELECT id, space_id, work_item_type_id from tracker_queries;
-	BEGIN
-		open c;
-			FOR r in FETCH ALL FROM c LOOP
-					UPDATE tracker_queries as tq
-						SET work_item_type_id = wit.id
-						FROM work_item_types as wit, spaces as sp
-						WHERE
-							tq.space_id = sp.id
-						        AND sp.space_template_id = wit.space_template_id
-						        AND wit.can_construct = true;
-			END LOOP;
-		close c;
-END $$ LANGUAGE plpgsql;
-
-DO $$ BEGIN
-	ALTER TABLE tracker_queries ADD COLUMN work_item_type_id uuid REFERENCES work_item_types(id) ON DELETE CASCADE;
-	PERFORM adds_wit();
-	DROP FUNCTION adds_wit();
-	ALTER TABLE tracker_queries ALTER COLUMN work_item_type_id set not null;
-END $$;
-
+DELETE FROM tracker_queries; -- tracker_queries was not fully supported and not in working condition before this
+ALTER TABLE tracker_queries ADD COLUMN work_item_type_id uuid not null REFERENCES work_item_types(id) ON DELETE CASCADE;
