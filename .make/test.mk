7dfed099bafd9a976a0a51e72c40d22a0292cc04
Konrad Kleine <193408+kwk@users.noreply.github.com>
2019-01-29 14:38:17 +0100

    Update .make/test.mk
    Co-Authored-By: pmacik <pavel.macik@gmail.com>

--- .make/test.mk
+++ .make/test.mk
@@ -209,7 +209,7 @@ CLEAN_TARGETS += clean-contract-tests
 ## Cleans generated pacts and logs from contract tests
 clean-contract-tests:
 	$(call log-info,"Cleaning generated pacts and logs from contract tests")
-	$(eval PACT_DIR=$(PWD)/test/contracts/pacts)
+	$(eval PACT_DIR=$(TMP_PATH)/test/contracts/pacts)
 	$(eval LOGS_TO_BE_CLEANED:=$(shell find $(TMP_PATH)/test/contracts -name '*logs'))
 	rm -rvf $(PACT_DIR) $(LOGS_TO_BE_CLEANED)
 
