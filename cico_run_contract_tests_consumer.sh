62dba45cf197b3b078776ea01a058d2a1f3877b5
PaveL Macík <pavel.macik@gmail.com>
2019-02-06 14:26:51 +0100

    contract-tests: Ensure Pact CLI is installed in the CICO script.

--- cico_run_contract_tests_consumer.sh
+++ cico_run_contract_tests_consumer.sh
@@ -4,7 +4,20 @@
 
 cico_setup;
 
-set -e
+make deps
+
+export TMP_DIR="$(readlink -f tmp)"
+
+# Add Pact CLI to PATH
+export PATH="$TMP_DIR/pact/bin:$PATH"
+
+# Ensure Pact CLI is installed
+pact-mock-service version &> /dev/null
+test_pact_exit=$?
+if [ $test_pact_exit -ne 0 ]; then
+    curl -L -s https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.63.0/pact-1.63.0-linux-x86_64.tar.gz -o "$TMP_DIR/pact-cli.tar.gz"
+    tar -xf "$TMP_DIR/pact-cli.tar.gz" --directory "$TMP_DIR"
+fi
 
 # Run the contract tests
 make test-contracts-consumer-no-coverage
