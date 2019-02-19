cd4c62e583b44b87fd53421d19018b891bda07df
Pavel Macík <pavel.macik@gmail.com>
2019-02-19 12:47:56 +0100

    contract-tests: Workaround 'set -e' for pact-mock-service check.

--- cico_run_contract_tests_consumer.sh
+++ cico_run_contract_tests_consumer.sh
@@ -15,8 +15,7 @@ export TMP_DIR="$(readlink -f tmp)"
 export PATH="$TMP_DIR/pact/bin:$PATH"
 
 # Ensure Pact CLI is installed
-pact-mock-service version &> /dev/null
-test_pact_exit=$?
+test_pact_exit=$(pact-mock-service version &> /dev/null; echo $?)
 if [ $test_pact_exit -ne 0 ]; then
     curl -L -s https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.63.0/pact-1.63.0-linux-x86_64.tar.gz -o "$TMP_DIR/pact-cli.tar.gz"
     tar -xf "$TMP_DIR/pact-cli.tar.gz" --directory "$TMP_DIR"
