885396219207d85dec2889b9d6389ed62209b4ea
Pavel Macík <pavel.macik@gmail.com>
2019-02-20 08:14:36 +0100

    contract-tests: Fix docker container name in cico script.

--- cico_run_contract_tests_consumer.sh
+++ cico_run_contract_tests_consumer.sh
@@ -10,16 +10,18 @@ fi
 make docker-start
 make docker-build
 
+DOCKER_CONTAINER_NAME="${BUILD_TAG:-fabric8-wit-local-build}"
+
 #Ensure Pact CLI is installed
 cmd="curl -L -s https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.63.0/pact-1.63.0-linux-x86_64.tar.gz -o /tmp/pact-cli.tar.gz \
     && mkdir -p /tmp/pact \
     && tar -xf /tmp/pact-cli.tar.gz --directory /tmp \
     && rm -vf /tmp/pact-cli.tar.gz"
-docker exec -t "fabric8-wit-local-build" /bin/bash -ec "$cmd"
+docker exec -t "$DOCKER_CONTAINER_NAME" /bin/bash -ec "$cmd"
 
 # Run the contract tests
 cmd="PATH=\$PATH:/tmp/pact/bin make test-contracts-consumer-no-coverage"
-docker exec -t "fabric8-wit-local-build" /bin/bash -ec "$cmd"
+docker exec -t "$DOCKER_CONTAINER_NAME" /bin/bash -ec "$cmd"
 
 # Publish the generated Pact files to Pact broker.
 cmd="PATH=\$PATH:/tmp/pact/bin make publish-contract-testing-pacts-to-broker"
@@ -29,4 +31,4 @@ docker exec -t \
     -e PACT_BROKER_PASSWORD=$PACT_BROKER_PASSWORD \
     -e PACT_VERSION=${PACT_VERSION:-PR-commit} \
     -e PACT_TAGS=${PACT_TAGS:-PR-testing} \
-    "fabric8-wit-local-build" /bin/bash -ec "$cmd"
+    "$DOCKER_CONTAINER_NAME" /bin/bash -ec "$cmd"
